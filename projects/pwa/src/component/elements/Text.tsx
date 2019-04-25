fdadf83f5a51ffd46bf9a1c8e8d4979a8d46f694
Simon Adcock <simonadcock2@gmail.com>
2019-04-25 17:26:48 +0100

    use metrics

--- projects/pwa/src/component/elements/Text.tsx
+++ projects/pwa/src/component/elements/Text.tsx
@@ -3,13 +3,15 @@ import { css } from 'emotion';
 import { palette } from '@guardian/pasteup/palette';
 import { body } from '@guardian/pasteup/typography';
 
+import { metrics } from '../../helper/styles';
+
 // tslint:disable:react-no-dangerous-html
 export const textStyle = css`
     strong {
         font-weight: 700;
     }
     p {
-        padding: 0 0 12px;
+        padding: 0 0 ${metrics.baseline}px;
         ${body(2)};
         font-weight: 300;
         word-wrap: break-word;
