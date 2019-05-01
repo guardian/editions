6a0737dae6e5202fe281a5d92c723daa23ebc7b4
Alex Ware <alex.ware@guardian.co.uk>
2019-05-01 16:52:13 +0100

    rn commit:

--- projects/rntest/AwesomeProject/App.js
+++ projects/rntest/AwesomeProject/App.js
@@ -9,20 +9,27 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+import ReactDOMServer from 'react-dom/server';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
 });
 
 type Props = {};
+
+
+
+
+const s = ReactDOMServer.renderToString(<h1>this escalated quickly</h1>)
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, disable your antivirus</Text>
+        <Text style={styles.welcome}>{s}</Text>
+        <Text style={styles.instructions}>🙈🐶</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
