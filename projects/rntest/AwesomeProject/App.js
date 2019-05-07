b3b2e266b153e51037a905257c8c3ac73d179db9
Alex Ware <alex.ware@guardian.co.uk>
2019-05-07 09:50:07 +0100

    more test things

--- projects/rntest/AwesomeProject/App.js
+++ projects/rntest/AwesomeProject/App.js
@@ -8,8 +8,11 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { WebView } from 'react-native-webview';
+import { Button } from 'react-native';
 
 import ReactDOMServer from 'react-dom/server';
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -26,11 +29,32 @@ type Props = {};
 const s = ReactDOMServer.renderToString(<h1>this escalated quickly</h1>)
 export default class App extends Component<Props> {
   render() {
+    console.log('hello')
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>{s}</Text>
-        <Text style={styles.instructions}>🙈🐶</Text>
+        <Text style={styles.instructions}>🙈🌍🐶</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>🐳🏝</Text>
+        <Button
+  onPress={()=>{
+    console.log("👌🏻")
+    const hi = 'AAA'
+    debugger;
+    console.log("👌🏻")
+
+  }}
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
+        <WebView
+        originWhitelist={['*']}
+        source={{ html: '<h1>Hello world</h1>' }}
+        style={{flex:1, height: 80, width: 80}}
+      />
+        <Text style={styles.instructions}>HEY</Text>
+
       </View>
     );
   }
