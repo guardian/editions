362f52017ca449c34e004b0289d3782ab943f563
Laura buns <lawr@lauragonzalez.cc>
2019-05-07 13:48:58 +0100

    no pwa

--- projects/pwa/src/@types/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@guardian/pasteup/palette';
-declare module '@guardian/pasteup/typography';
-declare module 'emotion-reset';
-
-interface CAPIElement {
-    _type: string;
-    html: string;
-}
