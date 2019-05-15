20fe785075f5a48c1d90f89eda97f8014ce7a065
Simon Adcock <simonadcock2@gmail.com>
2019-05-15 09:06:07 +0100

    fix linting errors

--- projects/backend/index.ts
+++ projects/backend/index.ts
@@ -1,81 +1,81 @@
-require("dotenv").config();
+require('dotenv').config()
 
-import express = require("express");
-import fetch from "node-fetch";
+import express = require('express')
+import fetch from 'node-fetch'
 import {
-  BufferedTransport,
-  CompactProtocol
-} from "@creditkarma/thrift-server-core";
-import { ItemResponseCodec } from "@guardian/capi-ts";
-import { s3fetch } from './s3';
-import { getIssue, getCollectionsForFront, getCollection } from './fronts';
-const app = express();
+    BufferedTransport,
+    CompactProtocol,
+} from '@creditkarma/thrift-server-core'
+import { ItemResponseCodec } from '@guardian/capi-ts'
+import { s3fetch } from './s3'
+import { getIssue, getCollectionsForFront, getCollection } from './fronts'
+const app = express()
 // app.get('/', (req, res) => res.send('Hello World!'))
 
 // app.listen(3000, () => console.log(`Example app listening on port 👌🏻!`))
-console.log(process.env.CAPIKEY);
-const port = 3131;
+console.log(process.env.CAPIKEY)
+const port = 3131
 const url = (path: string) =>
-  `https://content.guardianapis.com/${path}?format=thrift&api-key=${
-    process.env.CAPIKEY
-  }&show-elements=all&show-atoms=all&show-rights=all&show-fields=all&show-tags=all&show-blocks=all&show-references=all`;
+    `https://content.guardianapis.com/${path}?format=thrift&api-key=${
+        process.env.CAPIKEY
+    }&show-elements=all&show-atoms=all&show-rights=all&show-fields=all&show-tags=all&show-blocks=all&show-references=all`
 
 const getArticle = async (path: string) => {
-  const resp = await fetch(url(path));
-  const buffer = await resp.arrayBuffer();
+    const resp = await fetch(url(path))
+    const buffer = await resp.arrayBuffer()
 
-  const receiver: BufferedTransport = BufferedTransport.receiver(
-    new Buffer(buffer)
-  );
-  const input = new CompactProtocol(receiver);
+    const receiver: BufferedTransport = BufferedTransport.receiver(
+        new Buffer(buffer),
+    )
+    const input = new CompactProtocol(receiver)
 
-  const data = ItemResponseCodec.decode(input);
-  const title = data && data.content && data.content.webTitle;
-  const body =
-    data &&
-    data.content &&
-    data.content.blocks &&
-    data.content.blocks.body &&
-    data.content.blocks.body.map(_ => _.elements);
-  return [title, body];
-};
-app.get("/edition/:editionId", (req,res)=>{
+    const data = ItemResponseCodec.decode(input)
+    const title = data && data.content && data.content.webTitle
+    const body =
+        data &&
+        data.content &&
+        data.content.blocks &&
+        data.content.blocks.body &&
+        data.content.blocks.body.map(_ => _.elements)
+    return [title, body]
+}
+app.get('/edition/:editionId', (req, res) => {
     const id: string = req.params.editionId
     getIssue(id).then(data => {
-        res.setHeader("Content-Type", "application/json");
-        res.send(JSON.stringify(data));
-      })
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(data))
+    })
 })
 
-app.get("/front/:frontId", (req,res)=>{
+app.get('/front/:frontId', (req, res) => {
     const id: string = req.params.frontId
     getCollectionsForFront(id).then(data => {
-        res.setHeader("Content-Type", "application/json");
-        res.send(JSON.stringify(data));
-      })
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(data))
+    })
 })
 
-app.get("/collection/:collectionId", (req,res)=>{
+app.get('/collection/:collectionId', (req, res) => {
     const id: string = req.params.collectionId
     getCollection(id).then(data => {
-        res.setHeader("Content-Type", "application/json");
-        res.send(JSON.stringify(data));
-      })
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(data))
+    })
 })
 
-app.get("/content/*?",(req,res)=>{
+app.get('/content/*?', (req, res) => {
     console.log(req.params)
     const path: string = req.params[0]
     console.log(path)
     getArticle(path).then(data => {
-        res.setHeader("Content-Type", "application/json");
-        res.send(JSON.stringify(data));
-      })
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(data))
+    })
 })
 
-app.get("/", (req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    res.send(JSON.stringify({'client':'🦆'}));
-});
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify({ client: '🦆' }))
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- projects/backend/s3.ts
+++ projects/backend/s3.ts
@@ -1,19 +1,21 @@
-import {S3 } from 'aws-sdk'
+import { S3 } from 'aws-sdk'
 const s3 = new S3()
 const stage = 'CODE'
 
 export const s3fetch = async (key: string) => {
     const k = `${stage}/${key}`
     console.log(k)
-    const result = await s3.getObject({
-        Key: k,
-        Bucket: 'facia-tool-store'
-    }).promise()
+    const result = await s3
+        .getObject({
+            Key: k,
+            Bucket: 'facia-tool-store',
+        })
+        .promise()
     const body = result.Body
-    if(body == null) throw new Error("Not found.")
+    if (body == null) throw new Error('Not found.')
     return {
-        text: async ()=>body.toString(),
-        json: async ()=>JSON.parse(body.toString()),
-        lastModified: result.LastModified
+        text: async () => body.toString(),
+        json: async () => JSON.parse(body.toString()),
+        lastModified: result.LastModified,
     }
 }
