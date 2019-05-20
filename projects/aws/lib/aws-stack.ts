1b304f752300fa9ba9af40bff7c997df99c7956d
Alex Ware <alex.ware@guardian.co.uk>
2019-05-20 15:20:54 +0100

    maybe add vpb

--- projects/aws/lib/aws-stack.ts
+++ projects/aws/lib/aws-stack.ts
@@ -2,6 +2,7 @@ import cdk = require('@aws-cdk/cdk')
 import apigateway = require('@aws-cdk/aws-apigateway')
 import lambda = require('@aws-cdk/aws-lambda')
 import s3 = require('@aws-cdk/aws-s3')
+import ec2 = require('@aws-cdk/aws-ec2')
 
 import { Code } from '@aws-cdk/aws-lambda'
 // import { Bucket } from '@aws-cdk/aws-s3';
@@ -22,6 +23,29 @@ export class EditionsStack extends cdk.Stack {
             description: 'Stage',
         })
 
+        // const vpcId = new cdk.CfnParameter(this, 'vpc', {
+        //     type: 'AWS::EC2::VPC::Id',
+        //     description: 'vpc goes here',
+        // })
+
+        // const vpc = ec2.VpcNetwork.import(this, 'vpc', {
+        //     vpcId: vpcId.stringValue,
+        //     availabilityZones: ['eu-west-1'],
+        // })
+
+        // const subnetId = new cdk.CfnParameter(this, 'subnet', {
+        //     type: 'AWS::EC2::Subnet::Id',
+        //     description: 'subnet goes here',
+        // })
+
+        // const subnet = ec2.VpcSubnet.import(this, 'subnets', {
+        //     availabilityZone: 'eu-west-1',
+        //     subnetId: subnetId.stringValue,
+        // })
+        // const vpc = new ec2.VpcNetwork(this, 'editions-vpc', {})
+        //locating vpc
+        //const vpcSubnet = new ec2.VpcPrivateSubnet(this, 'editions-subnet', {})
+
         const deploy = new s3.Bucket(this, 'editions-dist')
 
         const backend = new lambda.Function(this, 'EditionsBackend', {
@@ -33,6 +57,11 @@ export class EditionsStack extends cdk.Stack {
                 }/${appParameter.value}.zip`,
             ),
             handler: 'handler',
+            environment: {
+                hello: 'key',
+            },
+            // vpc: vpc,
+            // vpcSubnets: subnet,
         })
 
         new apigateway.LambdaRestApi(
