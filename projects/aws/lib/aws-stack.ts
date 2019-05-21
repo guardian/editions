dd489a519e49603043bf4afa8b404f87560a12b6
Alex Ware <alex.ware@guardian.co.uk>
2019-05-21 14:20:02 +0100

    Fix cloudformation

--- projects/aws/lib/aws-stack.ts
+++ projects/aws/lib/aws-stack.ts
@@ -1,9 +1,8 @@
 import cdk = require('@aws-cdk/cdk')
 import apigateway = require('@aws-cdk/aws-apigateway')
 import lambda = require('@aws-cdk/aws-lambda')
-import s3 = require('@aws-cdk/aws-s3')
-
 import { Code } from '@aws-cdk/aws-lambda'
+import s3 = require('@aws-cdk/aws-s3')
 
 export class EditionsStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -26,15 +25,25 @@ export class EditionsStack extends cdk.Stack {
             description: 'Capi key',
         })
 
-        const deploy = new s3.Bucket(this, 'editions-dist')
-
+        const deploy = s3.Bucket.fromBucketName(
+            this,
+            'editions-dist',
+            'editions-dist',
+        )
         const backend = new lambda.Function(this, 'EditionsBackend', {
             runtime: lambda.Runtime.NodeJS810,
+            // code: Code.inline(
+            //     `module.handler = () => console.log('hi ${
+            //         stackParameter.value
+            //     }/${stageParameter.value}/${appParameter.value}/${
+            //         appParameter.value
+            //     }.zip')`,
+            // ),
             code: Code.bucket(
                 deploy,
-                `${stackParameter.value}/${stageParameter.value}/${
-                    appParameter.value
-                }/${appParameter.value}.zip`,
+                `${stackParameter.stringValue}/${
+                    stageParameter.stringValue
+                }/backend/backend.zip`,
             ),
             handler: 'handler',
             environment: {
