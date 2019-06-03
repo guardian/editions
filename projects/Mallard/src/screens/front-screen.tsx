fd26ab00614dcf0ad9095d936397ebb0f0d19a3d
Laura buns <lawr@lauragonzalez.cc>
2019-06-03 12:10:19 +0200

    Update projects/Mallard/src/screens/front-screen.tsx

--- projects/Mallard/src/screens/front-screen.tsx
+++ projects/Mallard/src/screens/front-screen.tsx
@@ -49,7 +49,7 @@ const FrontRow: React.FC<{
                 </View>
                 <View style={{ width }}>
                     <Grid
-                        onPress={item => navigation.navigate('Article', item)}
+                        onPress={(item: string) => navigation.navigate('Article', item)}
                         data={frontsData.map(
                             ([title]: any[], index: number) => ({
                                 issue,
