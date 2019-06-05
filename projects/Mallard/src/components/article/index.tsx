5f9845436a455324c03082c02dccf01909693de3
Simon Adcock <simonadcock2@gmail.com>
2019-06-05 11:03:38 +0100

    more linting

--- projects/Mallard/src/components/article/index.tsx
+++ projects/Mallard/src/components/article/index.tsx
@@ -44,9 +44,9 @@ const render = article => {
     <head></head>
     <body>
       ${article
-            .filter(el => el.type === 0)
-            .map(el => el.textTypeData.html)
-            .join('')}
+          .filter(el => el.type === 0)
+          .map(el => el.textTypeData.html)
+          .join('')}
       <script>
         window.requestAnimationFrame(function() {
             window.ReactNativeWebView.postMessage(document.body.getBoundingClientRect().height)
@@ -90,8 +90,8 @@ const Article = ({
                 {appearanceName === 'longread' ? (
                     <LongReadHeader {...{ headline, image, kicker }} />
                 ) : (
-                        <NewsHeader {...{ headline, image, kicker }} />
-                    )}
+                    <NewsHeader {...{ headline, image, kicker }} />
+                )}
                 <Standfirst {...{ byline, standfirst }} />
 
                 <View style={{ backgroundColor: color.background, flex: 1 }}>
@@ -101,7 +101,7 @@ const Article = ({
                         onMessage={event => {
                             setHeight(
                                 parseInt(event.nativeEvent.data) /
-                                PixelRatio.get(),
+                                    PixelRatio.get(),
                             )
                         }}
                         style={{ flex: 1, height: height }}
