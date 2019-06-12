2ed47b278e52e8a93b379d9d2578eda9415cb52e
Alex Ware <alex.ware@guardian.co.uk>
2019-06-12 15:08:09 +0100

    Add permision

--- projects/backend/capi/atoms.ts
+++ projects/backend/capi/atoms.ts
@@ -6,16 +6,15 @@ import {
 import { awsToString } from '../parser'
 import { AtomElement } from '../common'
 
+const creds = process.env.arn
+    ? {}
+    : {
+          credentials: new SharedIniFileCredentials({ profile: 'frontend' }),
+      }
+
 const lambda = new Lambda({
     region: 'eu-west-1',
-    credentials: process.env.arn
-        ? new ChainableTemporaryCredentials({
-              params: {
-                  RoleArn: process.env.arn as string,
-                  RoleSessionName: 'front-assume-role-access',
-              },
-          })
-        : new SharedIniFileCredentials({ profile: 'frontend' }),
+    ...creds,
 })
 const stage = process.env.stage || 'CODE'
 export const renderAtom = async (
@@ -24,19 +23,14 @@ export const renderAtom = async (
 ): Promise<{ html?: string; css: string[]; js: string[] } | null> => {
     let resp = await lambda
         .invoke({
-            FunctionName: `editions-atom-renderer-${stage}`,
+            FunctionName: process.env.atomArn || `editions-atom-renderer-CODE`,
             Payload: JSON.stringify({
                 atomType: atomType,
                 id: atomId,
             }),
         })
         .promise()
-    console.log(
-        JSON.stringify({
-            atomType: atomType,
-            id: atomId,
-        }),
-    )
+
     const payload = awsToString(resp.Payload)
     if (!payload) return null
     const result = JSON.parse(JSON.parse(payload)) //FIXME: this is not good.
