3f0caccc63568dbb6ad9be9cbe48e3e85801664f
Laura buns <lawr@lauragonzalez.cc>
2019-06-25 12:51:28 +0100

    comment this for future laura

--- projects/Mallard/src/navigation/interpolators.ts
+++ projects/Mallard/src/navigation/interpolators.ts
@@ -5,19 +5,19 @@ import { metrics } from 'src/theme/spacing'
 
 const issueScreenInterpolator = (sceneProps: NavigationTransitionProps) => {
     const { position, scene } = sceneProps
-    const thisSceneIndex = scene.index
+    const sceneIndex = scene.index
 
     const scale = position.interpolate({
-        inputRange: [thisSceneIndex + 0.1, thisSceneIndex + 1],
+        inputRange: [sceneIndex + 0.1, sceneIndex + 1],
         outputRange: [1, 0.75],
     })
     const borderRadius = position.interpolate({
-        inputRange: [thisSceneIndex, thisSceneIndex + 1],
+        inputRange: [sceneIndex, sceneIndex + 1],
         extrapolate: 'clamp',
         outputRange: [0, 20],
     })
     const opacity = position.interpolate({
-        inputRange: [thisSceneIndex, thisSceneIndex + 0.1, thisSceneIndex + 1],
+        inputRange: [sceneIndex, sceneIndex + 0.1, sceneIndex + 1],
         extrapolate: 'clamp',
         outputRange: [1, 0.5, 0.2],
     })
@@ -36,42 +36,59 @@ const issueScreenInterpolator = (sceneProps: NavigationTransitionProps) => {
 
 const articleScreenInterpolator = (sceneProps: NavigationTransitionProps) => {
     const { layout, position, scene } = sceneProps
-    const thisSceneIndex = scene.index
+    const sceneIndex = scene.index
 
+    /* 
+    we are assuming our final article 
+    will cover the entire screen here
+    */
     const { width: windowWidth, height: windowHeight } = Dimensions.get(
         'window',
     )
-
     const { x, y, width } = getScreenPositionOfItem(
         scene.route.params &&
             scene.route.params.path &&
             scene.route.params.path.article,
     )
 
-    const scaler = width / (layout.initWidth || windowWidth)
-    const scale = position.interpolate({
-        inputRange: [thisSceneIndex - 1, thisSceneIndex],
-        outputRange: [scaler, 1],
-    })
-
-    const yscaleDiff = (windowHeight - windowHeight * scaler) / 2
-
+    /*
+    subtly blend the actual article page 
+    and its card so it's a bit less jarring
+    */
     const opacity = position.interpolate({
-        inputRange: [thisSceneIndex - 1, thisSceneIndex - 0.8, thisSceneIndex],
+        inputRange: [sceneIndex - 1, sceneIndex - 0.8, sceneIndex],
         outputRange: [0, 0.9, 1],
     })
 
-    const translater = y - yscaleDiff
+    /*
+    we need to scale the article's X to
+    whatever its card width is
+    */
+    const scaler = width / (layout.initWidth || windowWidth)
+    const scale = position.interpolate({
+        inputRange: [sceneIndex - 1, sceneIndex],
+        outputRange: [scaler, 1],
+    })
+
+    /* 
+    yScaleDiff calculates how many pixels we need to offset 
+    our y translate to account for the scale
+    */
+    const yScaleDiff = (windowHeight - windowHeight * scaler) / 2
+    const translater = y - yScaleDiff
     const translateY = position.interpolate({
-        inputRange: [thisSceneIndex - 1, thisSceneIndex],
+        inputRange: [sceneIndex - 1, sceneIndex],
         outputRange: [translater, metrics.slideCardSpacing],
     })
 
-    const fromCentre = width / 2 + x - windowWidth / 2
-
+    /*
+    If the card is half width we 
+    need to move it to its side at the start
+    */
+    const distanceFromCentre = width / 2 + x - windowWidth / 2
     const translateX = position.interpolate({
-        inputRange: [thisSceneIndex - 1, thisSceneIndex],
-        outputRange: [fromCentre, 0],
+        inputRange: [sceneIndex - 1, sceneIndex],
+        outputRange: [distanceFromCentre, 0],
     })
 
     return {
