3434a4f1df7d321020aeffdcfb99e74d6c9e3665
Laura buns <lawr@lauragonzalez.cc>
2019-06-25 16:21:35 +0100

    clarify this

--- projects/Mallard/src/helpers/positions.ts
+++ projects/Mallard/src/helpers/positions.ts
@@ -4,7 +4,16 @@ import { Article } from 'src/common'
 /*
 This stores the screen positions of all items so 
 that when you try to go and open them the transitioner
-knows where to place the screen
+knows where to place the screen.
+
+Ideally we'd use state for something like this but
+a) it's unclear how to retrieve react state 
+   from navigation/index :(
+b) animations in react in general are always a
+   bunch of imperative escape hatches put together
+   for performance reasons. I'm not sure we wanna
+   bother with proper state every time a tile goes
+   on screen?
 */
 
 const positions: {
