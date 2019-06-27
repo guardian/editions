b71453e0fc03b66e9ac24c1368941959340c99a0
Laura buns <lawr@lauragonzalez.cc>
2019-06-27 15:23:25 +0100

    tweaks

--- projects/Mallard/src/components/onboarding/onboarding-card.tsx
+++ projects/Mallard/src/components/onboarding/onboarding-card.tsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
         paddingVertical: metrics.vertical,
     },
     subtitle: {
-        marginBottom: metrics.vertical,
+        marginBottom: metrics.vertical * 2,
     },
 })
 
