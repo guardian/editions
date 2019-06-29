f3aed001a940b208f1e454fd5e03d9cebd753792
Luke Taylor <luke.taylor@guardian.co.uk>
2019-06-29 21:17:44 +0100

    Add endpoint to retrieve weather forecast by IP address.

---
+++ projects/backend/controllers/weather.ts
@@ -0,0 +1,61 @@
+import { Request, Response as ExpressResponse } from 'express'
+import fetch, { Response } from 'node-fetch'
+
+const accuWeatherBaseUrl = 'https://weather.guardianapis.com'
+
+const accuWeatherApiKey: string | undefined =
+    process.env.ACCUWEATHER_API_KEY || undefined
+
+const getLocationKey = (ipAddress: string): Promise<Response> => {
+    return fetch(
+        `${accuWeatherBaseUrl}/locations/v1/cities/ipaddress?apikey=${accuWeatherApiKey}&details=true&metric=true&language=en-gb&q=${ipAddress}`,
+    )
+}
+
+const getWeatherForecastForLocation = (
+    locationKey: string,
+): Promise<Response> => {
+    return fetch(
+        `${accuWeatherBaseUrl}/forecasts/v1/hourly/12hour/${locationKey}.json?apikey=${accuWeatherApiKey}&details=true&metric=true&language=en-gb`,
+    )
+}
+
+export const weatherController = (req: Request, res: ExpressResponse) => {
+    const ipAddress: string = req.params.ipAddress
+
+    getLocationKey(ipAddress)
+        .then(async resp => {
+            if (resp.status != 200) {
+                res.status(resp.status)
+                res.send('Cannot get location key')
+                return
+            }
+
+            const json = await resp.json()
+
+            const locationKey: string = json.Key
+
+            if (!locationKey) {
+                res.status(500).send(
+                    'Error: could not get location key for the given IP address.',
+                )
+                return
+            }
+
+            return getWeatherForecastForLocation(locationKey).then(
+                async resp => {
+                    if (resp.status != 200) {
+                        res.status(resp.status)
+                        res.send('Cannot get location key')
+                        return
+                    }
+
+                    const json = await resp.json()
+
+                    res.setHeader('Content-Type', 'application/json')
+                    res.send(JSON.stringify(json))
+                },
+            )
+        })
+        .catch(e => console.error(e))
+}
