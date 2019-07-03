5dfca1e76880d3ba18d37f06c7b0a983f081bba0
Laura buns <lawr@lauragonzalez.cc>
2019-07-03 20:11:23 +0100

    Update use-api.ts

--- projects/Mallard/src/hooks/use-api.ts
+++ projects/Mallard/src/hooks/use-api.ts
@@ -5,11 +5,15 @@ import { withResponse } from 'src/helpers/response'
 import {
     createCachedOrPromise,
     asPromise,
+    isNotCached,
 } from 'src/helpers/fetch/cached-or-promise'
 import { getIssueResponse } from './use-issue'
 
 export const getIssueSummary = () =>
-    fetchFromApi<IssueSummary[]>(issueSummaryPath(), { cached: true })
+    fetchFromApi<IssueSummary[]>(issueSummaryPath(), {
+        cached: true,
+        validator: res => res.length > 0,
+    })
 
 export const useIssueSummary = () =>
     withResponse<IssueSummary[]>(useCachedOrPromise(getIssueSummary()))
@@ -17,18 +21,25 @@ export const useIssueSummary = () =>
 export const getLatestIssue = () => {
     return createCachedOrPromise<Issue>(
         [
-            null,
-            async () => {
-                const resp = await getIssueSummary().getValue()
-                if (resp.length < 1) {
-                    throw 'Missing issues'
+            (() => {
+                const summary = getIssueSummary()
+                if (!isNotCached(summary)) {
+                    const issue = getIssueResponse(
+                        summary.value[summary.value.length - 1].key,
+                    )
+                    if (!isNotCached(issue)) {
+                        return issue.value
+                    }
                 }
+            })(),
+            async () => {
+                const resp = await asPromise(getIssueSummary())
                 const issue = getIssueResponse(resp[resp.length - 1].key)
                 return asPromise(issue)
             },
         ],
         {
-            savePromiseResultToValue: value => null,
+            savePromiseResultToValue: () => null,
         },
     )
 }
