967baeab0c1d0aa427f83cf5df964cb491bd1918
David Blishen <david.blishen@guardian.co.uk>
2019-07-05 10:24:52 +0100

    Swap to using the headline rather than the webTitle

--- projects/backend/capi/articles.ts
+++ projects/backend/capi/articles.ts
@@ -61,7 +61,10 @@ const parseArticleResult = async (
     if (internalid == null)
         throw new Error(`internalid was undefined in ${path}!`)
 
-    const title = result && result.webTitle
+    const title = 
+          result && 
+          result.fields &&
+          result.fields.headline
 
     const standfirst =
         result &&
