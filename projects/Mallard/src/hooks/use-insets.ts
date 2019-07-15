f70be6acf7e16ea6e33ebd4826a0189130a578e5
Laura buns <lawr@lauragonzalez.cc>
2019-07-15 09:40:34 +0100

    fix statusbar

--- projects/Mallard/src/hooks/use-insets.ts
+++ projects/Mallard/src/hooks/use-insets.ts
@@ -1,15 +1,21 @@
 import { useState, useEffect } from 'react'
 import { currentInsets } from '@delightfulstudio/react-native-safe-area-insets'
+import { getStatusBarHeight } from 'react-native-status-bar-height'
 
 const useInsets = () => {
     const [insets, setInsets] = useState({
         left: 0,
-        top: 0,
+        top: getStatusBarHeight(),
         bottom: 0,
         right: 0,
     })
     useEffect(() => {
-        currentInsets().then(setInsets)
+        currentInsets().then(insets => {
+            setInsets({
+                ...insets,
+                top: insets.top ? insets.top : getStatusBarHeight(),
+            })
+        })
     }, [])
     return insets
 }
