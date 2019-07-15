77b2b36a72b4336ec59ba88a7a2a66ea66739d1f
Laura buns <lawr@lauragonzalez.cc>
2019-07-15 09:46:07 +0100

    Update use-insets.ts

--- projects/Mallard/src/hooks/use-insets.ts
+++ projects/Mallard/src/hooks/use-insets.ts
@@ -1,20 +1,27 @@
 import { useState, useEffect } from 'react'
 import { currentInsets } from '@delightfulstudio/react-native-safe-area-insets'
 import { getStatusBarHeight } from 'react-native-status-bar-height'
+import { Dimensions } from 'react-native'
 
 const useInsets = () => {
     const [insets, setInsets] = useState({
         left: 0,
-        top: getStatusBarHeight(),
+        top: getStatusBarHeight(true),
         bottom: 0,
         right: 0,
     })
     useEffect(() => {
-        currentInsets().then(insets => {
-            setInsets({
-                ...insets,
-                top: insets.top ? insets.top : getStatusBarHeight(),
+        const updateInsets = () => {
+            currentInsets().then(insets => {
+                setInsets({
+                    ...insets,
+                    top: insets.top ? insets.top : getStatusBarHeight(true),
+                })
             })
+        }
+        updateInsets()
+        Dimensions.addEventListener('change', () => {
+            updateInsets()
         })
     }, [])
     return insets
