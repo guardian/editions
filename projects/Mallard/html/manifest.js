72d447df7daa1aeaf66ba4a4741e2e9661091a33
Laura buns <lawr@lauragonzalez.cc>
2019-07-18 14:51:04 +0300

    docs

--- projects/Mallard/html/manifest.js
+++ projects/Mallard/html/manifest.js
@@ -1,3 +1,20 @@
+/*
+{
+    key:
+        name of the bundle. a-z, no spaces
+    project:
+        project folder in this monorepo
+    watchPort:
+        port the local dev server is running on
+    watchScript:
+        script that will start watching this project and spin up the dev server
+    buildScript:
+        script that will build the app down to static html and assets
+    buildPath:
+        directory where the built app goes, the build script will move it from there to the bundle
+}
+*/
+
 module.exports = {
     crosswords: {
         key: 'crosswords',
