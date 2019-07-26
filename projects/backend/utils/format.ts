6db020fe6340399bed9149a295c599a4077cbee2
Laura buns <lawr@lauragonzalez.cc>
2019-07-26 12:34:57 +0100

    add credits

--- projects/backend/utils/format.ts
+++ projects/backend/utils/format.ts
@@ -1,6 +1,9 @@
 const capitalizeFirstLetter = (string: string) =>
     string.charAt(0).toUpperCase() + string.slice(1)
 
+/*
+from https://blog.abelotech.com/posts/number-currency-formatting-javascript/
+*/
 const addCommas = (num: number): string =>
     num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 
