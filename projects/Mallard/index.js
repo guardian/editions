196116986a58ac32677c07105800ee0e05d7f2ba
Luke Taylor <luke.taylor@guardian.co.uk>
2019-08-02 14:44:51 +0100

    Integrate with Sentry.

--- projects/Mallard/index.js
+++ projects/Mallard/index.js
@@ -5,5 +5,14 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
+import { Sentry, SentrySeverity } from 'react-native-sentry'
+import Config from 'react-native-config'
+
+const { SENTRY_DSN_URL } = Config
+
+// See: https://docs.sentry.io/clients/react-native/config/
+if (!__DEV__) {
+Sentry.config(SENTRY_DSN_URL).install();
+}
 
 AppRegistry.registerComponent(appName, () => App)
