6dc3acbb4bc6ab5c631e3982203bdfc273f39582
RichieAHB <richard.beddington@guardian.co.uk>
2019-08-02 14:53:03 +0100

    Add in builders for sentry config files

--- projects/Mallard/scripts/helpers/helpers.js
+++ projects/Mallard/scripts/helpers/helpers.js
@@ -4,22 +4,43 @@ const fs = require('fs')
 const NEWLINE = '\n'
 const removeWhiteSpace = s => s.trim()
 const isNonEmpty = s => s
-const getValueFromKey = map => key => {
-    const value = map[key]
-    if (!value) throw new Error(`Could not find ${key}`)
-    return `${key}=${value}`
-}
-const writeToEnvFileinCWD = data =>
-    fs.writeFileSync(path.join(process.cwd(), '.env'), data)
 
-const writeRequiredEnvVars = strings =>
-    writeToEnvFileinCWD(
+/**
+ *
+ * `key` is expected to be in the format:
+ * `MY_ENV_VAR`
+ * where `value` will be read from the environment variable `process.env.MY_ENV_VAR` and then
+ * set as `MY_ENV_VAR=value` in the file
+ *
+ * or it can be in the format:
+ * `test.var=MY_ENV_VAR`
+ * where the `value` will be read from `process.env.MY_ENV_VAR` and set as `test.var=value`
+ * in the file
+ *
+ * This is useful for when config files require chars that cannot be used in environment variables
+ * such as periods. Or when the same env var needs to be written to two different properties
+ */
+
+const getValueFromEnvironment = key => {
+    let [writeKey, readKey = writeKey] = key.split('=')
+    const value = process.env[readKey]
+    if (!value) throw new Error(`Could not find ${readKey} in environment`)
+    return `${writeKey}=${value}`
+}
+const writeToFile = (relativePaths, data) =>
+    relativePaths.forEach(relativePath =>
+        fs.writeFileSync(path.join(process.cwd(), relativePath), data),
+    )
+
+const writeEnvVarsToFiles = (...paths) => strings =>
+    writeToFile(
+        paths,
         strings[0]
             .split(NEWLINE)
             .map(removeWhiteSpace)
             .filter(isNonEmpty)
-            .map(getValueFromKey(process.env))
+            .map(getValueFromEnvironment)
             .join(NEWLINE),
     )
 
-module.exports = { writeRequiredEnvVars }
+module.exports = { writeEnvVarsToFiles }
