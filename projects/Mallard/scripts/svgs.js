924c3da5a3864e3e89319027907e9da4db4c5948
Alex Ware <alex.ware@guardian.co.uk>
2019-08-12 14:35:34 +0100

    Don't explicitly depend on convert-svg-to-png

--- projects/Mallard/scripts/svgs.js
+++ projects/Mallard/scripts/svgs.js
@@ -1,19 +1,40 @@
 const path = require('path')
 const fs = require('fs')
 const chalk = require('chalk')
-const { convertFile } = require('convert-svg-to-png')
 
 const svgFolder = path.resolve(__dirname, '..', 'src', 'assets', 'svgs')
 const bitmapFolder = path.resolve(__dirname, '..', 'src', 'assets', 'images')
 
-const convertSvg = (file, scale = 1) =>
-    convertFile(path.resolve(svgFolder, file), {
+// We can't guarantee these have been installed before running this script,
+// e.g. if it's the first run.
+// So if they have not been installed already, quickly install them locally.
+// They should be specified as a depenency, so this will almost never be a problem.
+const installIfNecessary = (...packages) =>
+    new Promise(resolve => {
+        try {
+            resolve(packages.map(require))
+        } catch (e) {
+            childProcess
+                .spawn('yarn', ['global add', ...packages], {
+                    stdio: 'inherit',
+                })
+                .on('close', code => {
+                    if (code !== 0) process.exit(code)
+                    resolve(packages.map(require))
+                })
+        }
+    })
+
+const convertSvg = async (file, scale = 1) => {
+    const [{ convertFile }] = await installIfNecessary('convert-svg-to-png')
+    return convertFile(path.resolve(svgFolder, file), {
         scale,
         outputFilePath: path.resolve(
             bitmapFolder,
             path.parse(file).name + (scale !== 1 ? `@${scale}x` : '') + '.png',
         ),
     })
+}
 
 fs.readdir(svgFolder, async (err, files) => {
     const svgs = files.filter(file => path.parse(file).ext === '.svg')
