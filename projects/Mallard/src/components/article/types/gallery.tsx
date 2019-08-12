64c6e3a58db21fcd93e9793d79f7b61b5ad5d679
Laura buns <lawr@lauragonzalez.cc>
2019-08-12 13:05:26 +0100

    Update gallery.tsx

--- projects/Mallard/src/components/article/types/gallery.tsx
+++ projects/Mallard/src/components/article/types/gallery.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { GalleryArticle } from 'src/common'
+import { GalleryArticle, ImageElement, Image as ImageType } from 'src/common'
 import {
     View,
     Text,
@@ -7,25 +7,34 @@ import {
     StyleProp,
     StyleSheet,
     ImageStyle,
+    TextStyle,
 } from 'react-native'
 import { color } from 'src/theme/color'
 import { metrics } from 'src/theme/spacing'
 import { UiBodyCopy } from 'src/components/styled-text'
 import { APIPaths } from 'src/paths'
+import { WithBreakpoints } from 'src/components/layout/ui/with-breakpoints'
+import { Breakpoints } from 'src/theme/breakpoints'
 
 const galleryImageStyles = StyleSheet.create({
     root: { backgroundColor: color.skeleton },
 })
 const GalleryImage = ({
-    uri,
+    src,
     accessibilityLabel,
     style,
 }: {
-    uri: string
+    src: ImageType
     accessibilityLabel?: string
     style: StyleProp<ImageStyle>
 }) => {
     const [aspectRatio, setRatio] = useState(1)
+    const uri = `${APIPaths.mediaBackend}${APIPaths.media(
+        'issue',
+        'phone',
+        src.source,
+        src.path,
+    )}`
 
     useEffect(() => {
         Image.getSize(
@@ -52,6 +61,24 @@ const GalleryImage = ({
     )
 }
 
+const captionStyles = StyleSheet.create({
+    root: {
+        color: color.textOverPhotoBackground,
+    },
+})
+const Caption = ({
+    element,
+    style,
+}: {
+    element: ImageElement
+    style?: StyleProp<TextStyle>
+}) =>
+    element.caption || element.copyright ? (
+        <UiBodyCopy style={[style, captionStyles.root]}>
+            {[element.caption, element.copyright].filter(Boolean).join(' - ')}
+        </UiBodyCopy>
+    ) : null
+
 const styles = StyleSheet.create({
     background: {
         backgroundColor: color.photoBackground,
@@ -59,43 +86,62 @@ const styles = StyleSheet.create({
         paddingTop: metrics.vertical / 2,
     },
     image: {
-        marginBottom: metrics.vertical * 2,
         width: '100%',
     },
     caption: {
+        marginTop: metrics.vertical / 2,
+    },
+    spacer: {
         marginBottom: metrics.vertical * 2,
-        marginTop: metrics.vertical * -1.5,
-        color: color.textOverPhotoBackground,
     },
 })
+
+const GalleryItem = ({ element }: { element: ImageElement }) => {
+    return (
+        <WithBreakpoints>
+            {{
+                0: () => (
+                    <View style={styles.spacer}>
+                        <GalleryImage
+                            accessibilityLabel={element.alt}
+                            src={element.src}
+                            style={styles.image}
+                        />
+                        <Caption element={element} style={styles.caption} />
+                    </View>
+                ),
+                [Breakpoints.tabletVertical]: () => (
+                    <View style={[styles.spacer, { flexDirection: 'row' }]}>
+                        <GalleryImage
+                            accessibilityLabel={element.alt}
+                            src={element.src}
+                            style={[styles.image, { width: '75%' }]}
+                        />
+                        <View
+                            style={[
+                                {
+                                    flexShrink: 1,
+                                    marginLeft: metrics.articleSides * 2,
+                                    marginRight: metrics.articleSides * 2,
+                                },
+                            ]}
+                        >
+                            <Caption element={element} />
+                        </View>
+                    </View>
+                ),
+            }}
+        </WithBreakpoints>
+    )
+}
+
 const Gallery = ({ gallery }: { gallery: GalleryArticle }) => {
     console.log(gallery)
     return (
         <View style={styles.background}>
             {gallery.elements.map((element, index) => {
                 if (element.id === 'image') {
-                    return (
-                        <>
-                            <GalleryImage
-                                key={index}
-                                accessibilityLabel={element.alt}
-                                uri={`${APIPaths.mediaBackend}${APIPaths.media(
-                                    'issue',
-                                    'phone',
-                                    element.src.source,
-                                    element.src.path,
-                                )}`}
-                                style={styles.image}
-                            />
-                            {element.caption || element.copyright ? (
-                                <UiBodyCopy style={styles.caption}>
-                                    {[element.caption, element.copyright]
-                                        .filter(Boolean)
-                                        .join(' - ')}
-                                </UiBodyCopy>
-                            ) : null}
-                        </>
-                    )
+                    return <GalleryItem element={element} />
                 }
                 return <Text key={index}>{element.id}</Text>
             })}
