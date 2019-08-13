51497254b28408dbcdde2661b1db07bd4ca110cf
Alex Ware <alex.ware@guardian.co.uk>
2019-08-13 16:42:45 +0100

    update to very latest sentry

--- projects/Mallard/index.js
+++ projects/Mallard/index.js
@@ -5,14 +5,14 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
-import { Sentry, SentrySeverity } from 'react-native-sentry'
+import { Sentry } from 'react-native-sentry'
 import Config from 'react-native-config'
 
 const { SENTRY_DSN_URL } = Config
 
 // See: https://docs.sentry.io/clients/react-native/config/
 if (!__DEV__) {
-Sentry.config(SENTRY_DSN_URL).install();
+    Sentry.init({ dsn: SENTRY_DSN_URL })
 }
 
 AppRegistry.registerComponent(appName, () => App)
