242ce275566122a451da2ff8ac9fb9ad82a1759e
Alex Ware <alex.ware@guardian.co.uk>
2019-08-16 12:06:30 +0100

    Fix the android beta build
    bump types too
    
    magical change to fix the build (until everything is on androidx)
    
    fun with xml
    
    remove when all deps on android x
    
    correct string
    
    upgrade deps?
    
    set vars for new libs
    
    Android Manifest stuff
    
    Upgrade more things.
    
    update dep?
    
    version darts
    
    FIX LOCKILE
    
    try overriding
    
    restore supply

--- projects/Mallard/android/app/src/main/java/com/guardian/editions/MainApplication.java
+++ projects/Mallard/android/app/src/main/java/com/guardian/editions/MainApplication.java
@@ -2,7 +2,10 @@ package com.guardian.editions;
 
 import android.app.Application;
 
+import android.util.Log;
 import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -14,49 +17,49 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            @SuppressWarnings("UnnecessaryLocalVariable")
+            List<ReactPackage> packages = new PackageList(this).getPackages();
+
+            // packages.add(new MainReactPackage());
+            // packages.add(new RNDeviceInfo());
+            // packages.add(new NetInfoPackage());
+            // packages.add(new RNSentryPackage());
+            // packages.add(new ReactNativePushNotificationPackage());
+            // packages.add(new ReactNativeConfigPackage());
+            // packages.add(new KeychainPackage());
+            // packages.add(new RNCMaskedViewPackage());
+            // packages.add(new RNCWebViewPackage());
+            // packages.add(new RNZipArchivePackage());
+            // packages.add(new SvgPackage());
+            // packages.add(new AsyncStoragePackage());
+            // packages.add(new RNFetchBlobPackage());
+            // packages.add(new RNScreensPackage());
+            // packages.add(new RNGestureHandlerPackage());
+            return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-        @SuppressWarnings("UnnecessaryLocalVariable")
-        List<ReactPackage> packages = new PackageList(this).getPackages();
-
-        //     packages.add(new MainReactPackage());
-        //     packages.add(new RNDeviceInfo());
-        //     packages.add(new NetInfoPackage());
-        //     packages.add(new RNSentryPackage());
-        //     packages.add(new ReactNativePushNotificationPackage());
-        //     packages.add(new ReactNativeConfigPackage());
-        //     packages.add(new KeychainPackage());
-        //     packages.add(new RNCMaskedViewPackage());
-        //     packages.add(new RNCWebViewPackage());
-        //     packages.add(new RNZipArchivePackage());
-        //     packages.add(new SvgPackage());
-        //     packages.add(new AsyncStoragePackage());
-        //     packages.add(new RNFetchBlobPackage());
-        //     packages.add(new RNScreensPackage());
-        //     packages.add(new RNGestureHandlerPackage());
-        return packages;
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
     }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
