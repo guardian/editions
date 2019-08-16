ff395d7b037d108d0044b3d884427adab032a4f1
Luke Taylor <luke.taylor@guardian.co.uk>
2019-08-16 17:48:27 +0100

    Add cutout images to opinion header.

--- projects/Mallard/src/components/article/article-header/opinion-header.tsx
+++ projects/Mallard/src/components/article/article-header/opinion-header.tsx
@@ -21,17 +21,40 @@ const styles = StyleSheet.create({
     byline: {
         fontFamily: getFont('titlepiece', 1).fontFamily,
         width: '100%',
+        paddingRight: 0,
+    },
+    cutout: {
+        aspectRatio: 1.5,
+        transform: [{ scaleX: 2.5 }, { scaleY: 2.5 }],
+        marginBottom: metrics.vertical * 2.5,
+        marginLeft: -metrics.vertical * 2.5,
+    },
+    cutoutContainer: {
+        flexDirection: 'column',
+        alignSelf: 'flex-end',
+        flex: 1,
+    },
+    headlineContainer: {
+        flexDirection: 'column',
+        flex: 4,
+        flexWrap: 'wrap',
+    },
+    flexRow: {
+        display: 'flex',
+        flexDirection: 'row',
     },
 })
 
 const OpinionHeader = ({
     byline,
+    bylineImages,
     headline,
     image,
     standfirst,
 }: ArticleHeaderProps) => {
     const [articleColor] = useArticle()
     const font = getFont('headline', 1.5, 'light')
+
     return (
         <MultilineWrap
             style={styles.innerWrap}
@@ -56,23 +79,48 @@ const OpinionHeader = ({
                 </ArticleFader>
             ) : null}
             <ArticleFader>
-                <ArticleHeadline
-                    icon={{
-                        width: 80,
-                        height: font.lineHeight,
-                        element: () => (
-                            <Quote scale={1.2} fill={articleColor.main} />
-                        ),
-                    }}
-                    weight="light"
-                    textStyle={[{ marginBottom: metrics.vertical }, font]}
-                >
-                    {headline}
-                    <Text style={[{ color: articleColor.main }, styles.byline]}>
-                        {'\n'}
-                        {byline}
-                    </Text>
-                </ArticleHeadline>
+                <View style={styles.flexRow}>
+                    <View style={styles.headlineContainer}>
+                        <ArticleHeadline
+                            icon={{
+                                width: 80,
+                                height: font.lineHeight,
+                                element: () => (
+                                    <Quote
+                                        scale={1.2}
+                                        fill={articleColor.main}
+                                    />
+                                ),
+                            }}
+                            weight="light"
+                            textStyle={[
+                                { marginBottom: metrics.vertical },
+                                font,
+                            ]}
+                        >
+                            {headline}
+                            <Text
+                                style={[
+                                    { color: articleColor.main },
+                                    styles.byline,
+                                ]}
+                            >
+                                {'\n'}
+                                {byline}
+                            </Text>
+                        </ArticleHeadline>
+                    </View>
+                    {bylineImages && bylineImages.cutout ? (
+                        <View style={styles.cutoutContainer}>
+                            <ArticleFader>
+                                <ArticleImage
+                                    style={styles.cutout}
+                                    image={bylineImages.cutout}
+                                />
+                            </ArticleFader>
+                        </View>
+                    ) : null}
+                </View>
             </ArticleFader>
         </MultilineWrap>
     )
