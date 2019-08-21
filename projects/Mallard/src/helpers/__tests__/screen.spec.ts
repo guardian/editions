557b6265a995689289dd98bde9143328b81432ea
James Miller <james.miller.freelancer@guardian.co.uk>
2019-08-21 10:19:14 +0100

    feat: Provides the image description based on screen size

--- projects/Mallard/src/helpers/__tests__/screen.spec.ts
+++ projects/Mallard/src/helpers/__tests__/screen.spec.ts
@@ -1,4 +1,4 @@
-import { maxScreenSize, minScreenSize } from '../screen'
+import { convertImageSizeToImageDescription, maxScreenSize, minScreenSize, screenSizeToImageSize, imageForScreenSize } from '../screen';
 
 const mock = jest.fn()
 
@@ -39,6 +39,45 @@ describe('helpers/screen', () => {
         it('should return 800 as the screen width and height are equal', () => {
             mock.mockReturnValueOnce({ width: 800, height: 800 })
             expect(minScreenSize()).toBe(800)
+        })
+    })
+
+    describe('screenSizeToImageSize', () => {
+        it("should provide the minimum value if the screen size is less than that value", () => {
+            expect(screenSizeToImageSize(320)).toEqual(375)
+        })
+
+        it("should provide the minimum value if the screen size is equal to that value", () => {
+            expect(screenSizeToImageSize(375)).toEqual(375)
+        })
+
+        it('should return the max possible value if the screen size is greater than that value', () => {
+            expect(screenSizeToImageSize(1366)).toEqual(1140)
+        })
+
+        it('should return the max possible value if it equals that value', () => {
+            expect(screenSizeToImageSize(1140)).toEqual(1140)
+        })
+
+        it('should return a middle value that is the bracket greater than the screen size provided', () => {
+            expect(screenSizeToImageSize(900)).toEqual(980)
+        })
+    })
+
+    describe('convertImageSizeToImageDescription', () => {
+        it('should return "phone" if no screen size is provided', () => {
+            expect(convertImageSizeToImageDescription(NaN)).toEqual("phone")
+        })
+        it('should return "phone" if an incorrect screen size is provided', () => {
+            expect(convertImageSizeToImageDescription(-100)).toEqual("phone")
+        })
+
+        it('should return "phone" if a middling value is provided', () => {
+            expect(convertImageSizeToImageDescription(1000)).toEqual("phone")
+        })
+        
+        it('should return a correct size based on the value', () => {
+            expect(convertImageSizeToImageDescription(980)).toEqual("tabletL")
         })
     })
 })
--- projects/Mallard/src/helpers/screen.ts
+++ projects/Mallard/src/helpers/screen.ts
@@ -1,13 +1,45 @@
 import { Dimensions } from 'react-native'
+import DeviceInfo from "react-native-device-info";
+import { sizes, ImageSize } from "../constants/image-sizes";
 
-const maxScreenSize = () => {
+const maxScreenSize = (): number => {
     const { width, height } = Dimensions.get('window')
     return Math.max(width, height)
 }
 
-const minScreenSize = () => {
+const minScreenSize = (): number => {
     const { width, height } = Dimensions.get('window')
     return Math.min(width, height)
 }
 
-export { maxScreenSize, minScreenSize }
+const screenSizeToImageSize = (screenSize: number): number => {
+    const allSizes = Object.values(sizes);
+    const minPossibleSize = Math.min(...allSizes);
+    if(screenSize <= minPossibleSize){
+        return minPossibleSize
+    }
+    
+    const maxPossibleSize = Math.max(...allSizes);
+    if(screenSize >= maxPossibleSize){
+        return maxPossibleSize
+    }
+
+    const availableSizes: number[] = Object.values(sizes).filter(size => screenSize <= size);
+    return Math.min(...availableSizes)
+}
+
+const convertImageSizeToImageDescription = (screenSize: number): ImageSize => {
+    if(!screenSize){
+        return 'phone'
+    }
+    const imageSize = Object.keys(sizes).find(size => sizes[size as ImageSize] === screenSize)
+    return imageSize as ImageSize || 'phone'
+}
+
+const imageForScreenSize = () => {    
+    const screenSize = DeviceInfo.isTablet() ? maxScreenSize() : minScreenSize()
+    return convertImageSizeToImageDescription(screenSizeToImageSize(screenSize))
+
+}
+
+export { maxScreenSize, minScreenSize, screenSizeToImageSize, convertImageSizeToImageDescription, imageForScreenSize }
