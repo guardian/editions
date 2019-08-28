1d9ad0d577ee8051a23424cde7271e217868e807
James Miller <james.miller.freelancer@guardian.co.uk>
2019-08-28 16:04:00 +0100

    feat: Importing Ophan methods into the app

--- projects/Mallard/android/app/src/main/java/com/guardian/editions/ophan/RNOphanModule.java
+++ projects/Mallard/android/app/src/main/java/com/guardian/editions/ophan/RNOphanModule.java
@@ -4,11 +4,16 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import ophan.OphanKt;
 
 import javax.annotation.Nonnull;
 
+import ophan.OphanApi;
+import ophan.OphanKt;
+
 class RNOphanModule extends ReactContextBaseJavaModule {
+
+    private OphanApi ophanApi = new OphanApi("0.0.1", "Android", "Unknown", "Unknown", "testDeviceId", "testUserId",
+            new LogcatLogger(), "ophan");
 
     public RNOphanModule(@Nonnull ReactApplicationContext reactContext) {
         super(reactContext);
--- projects/Mallard/ophan/src/commonMain/kotlin/ophan/Ophan.kt
+++ projects/Mallard/ophan/src/commonMain/kotlin/ophan/Ophan.kt
@@ -1,3 +1,57 @@
 package ophan
 
-fun hello(): String = "Hello from JAMES AND MAX" 
+import com.gu.ophan.Logger
+import com.gu.ophan.OphanDispatcher
+import com.gu.ophan.FileRecordStore
+import ophan.thrift.componentEvent.Action
+import ophan.thrift.componentEvent.ComponentEvent
+import ophan.thrift.componentEvent.ComponentType
+import ophan.thrift.componentEvent.ComponentV2
+import ophan.thrift.nativeapp.*
+
+fun hello(): String = "Hello from MAX AND JAMES!"
+
+class OphanApi(
+        private val dispatcher: OphanDispatcher
+) {
+
+    constructor(
+            appVersion: String,
+            appOs: String,
+            deviceName: String,
+            deviceManufacturer: String,
+            deviceId: String,
+            userId: String,
+            logger: Logger,
+            recordStorePath: String
+    ) : this(OphanDispatcher(
+            App(appVersion, "TestEditions", appOs, Edition.UK),
+            Device(deviceName, deviceManufacturer),
+            deviceId,
+            userId,
+            logger,
+            FileRecordStore(recordStorePath)
+    ))
+
+    fun sendTestAppScreenEvent(screenName: String, eventId: String) {
+        val event = Event.Builder()
+                .eventId(eventId)
+                .eventType(EventType.COMPONENT_EVENT)
+                .viewId(null) /* TODO */
+                .componentEvent(ComponentEvent.Builder()
+                        .component(ComponentV2.Builder()
+                                .componentType(ComponentType.APP_SCREEN)
+                                .id(screenName)
+                                .products(emptySet())
+                                .campaignCode(null)
+                                .labels(emptySet())
+                                .build()
+                        )
+                        .action(Action.VIEW)
+                        .value("yes")
+                        .build()
+                )
+                .build()
+        dispatcher.dispatchEvent(event)
+    }
+}
