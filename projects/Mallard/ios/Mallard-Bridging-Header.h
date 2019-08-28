44d809f3b172149460ede127e304ae2ac12bc616
RichieAHB <richard.beddington@guardian.co.uk>
2019-08-28 11:24:40 +0100

    Add swift implementation for ReleaseStream

--- projects/Mallard/ios/Mallard-Bridging-Header.h
+++ projects/Mallard/ios/Mallard-Bridging-Header.h
@@ -1,6 +1,6 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
-// / React /
+//
 
 #import "React/RCTBridgeModule.h"
 #import "React/RCTEventDispatcher.h"
---
+++ projects/Mallard/ios/ReleaseStream.m
@@ -0,0 +1,14 @@
+//
+//  ReleaseStream.m
+//  Mallard
+//
+//  Created by Richard Beddington on 28/08/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "React/RCTBridgeModule.h"
+
+@interface RCT_EXTERN_REMAP_MODULE(RNReleaseStream, ReleaseStream, NSObject)
+@end
+
