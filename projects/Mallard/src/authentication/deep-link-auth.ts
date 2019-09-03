95b62b9322f63b6d9bac5951a907b7feb0d947ea
RichieAHB <richard.beddington@guardian.co.uk>
2019-09-03 12:21:53 +0100

    Add react-native-inappbrowser-reborn for social

--- projects/Mallard/src/authentication/deep-link-auth.ts
+++ projects/Mallard/src/authentication/deep-link-auth.ts
@@ -1,9 +1,34 @@
 import { Linking, AppState } from 'react-native'
+import InAppBrowser from 'react-native-inappbrowser-reborn'
 
 interface Emitter<T> {
     addEventListener(type: string, cb: (e: T) => void): void
     removeEventListener(type: string, cb: Function): void
 }
+
+type IAppState = Emitter<string>
+
+const createAppChangeListener = (
+    fn: (state: string) => void,
+    appStateEmitter: IAppState,
+) => {
+    appStateEmitter.addEventListener('change', fn)
+    return () => appStateEmitter.removeEventListener('change', fn)
+}
+
+type ILinking = Emitter<{ url: string }> & {
+    openURL: (url: string) => void
+}
+
+const createLinkListener = (
+    fn: (event: { url: string }) => void,
+    linkingImpl: ILinking,
+) => {
+    linkingImpl.addEventListener('url', fn)
+    return () => linkingImpl.removeEventListener('url', fn)
+}
+
+type IInAppBrowser = Pick<typeof InAppBrowser, 'close' | 'open' | 'isAvailable'>
 
 /**
  * This function will open an auth url and wait for the first navigation back to the app
@@ -12,42 +37,72 @@ interface Emitter<T> {
  * in the case where someone redirects to the app without a token and then attempts the login
  * flow again (which would have created two listeners)
  */
-const authWithDeepRedirect = (
+const authWithDeepRedirect = async (
     authUrl: string,
     extractTokenAndValidateState: (url: string) => Promise<string>,
     /* mocks for testing */
-    linkingEmitter: Emitter<{ url: string }> & {
-        openURL: (url: string) => void
-    } = Linking,
-    appStateEmitter: Emitter<string> = AppState,
+    linkingImpl: ILinking = Linking,
+    appStateImpl: IAppState = AppState,
+    inAppBrowserImpl: IInAppBrowser = InAppBrowser,
 ): Promise<string> => {
-    Linking.openURL(authUrl)
     return new Promise(async (res, rej) => {
-        const linkHandler = async ({ url }: { url: string }) => {
-            linkingEmitter.removeEventListener('url', linkHandler)
-            // eslint-disable-next-line
-            appStateEmitter.removeEventListener('change', appChangeHandler)
+        let unlisteners: (() => void)[] = []
 
-            try {
-                res(await extractTokenAndValidateState(url))
-            } catch (e) {
-                rej(e)
+        const onFinish = async (url?: string) => {
+            inAppBrowserImpl.close()
+
+            let unlistener
+            while ((unlistener = unlisteners.pop())) {
+                unlistener()
             }
-        }
 
-        const appChangeHandler = (currentState: string) => {
-            if (currentState === 'active') {
-                // make sure the link handler is removed whenever we come back to the app
-                // url is called first in the happy path so the promise will have resolved by then
-                // otherwise, if they navigate back without authenticating, remove the listener and cancel the login
-                linkingEmitter.removeEventListener('url', linkHandler)
-                AppState.removeEventListener('change', appChangeHandler)
+            if (!url) {
                 rej('Sign-in cancelled')
+            } else {
+                res(await extractTokenAndValidateState(url))
             }
         }
 
-        linkingEmitter.addEventListener('url', linkHandler)
-        appStateEmitter.addEventListener('change', appChangeHandler)
+        const unlistenLink = createLinkListener((event: { url: string }) => {
+            // eslint-disable-next-line
+            onFinish(event.url)
+        }, linkingImpl)
+
+        unlisteners.push(unlistenLink)
+
+        if (await inAppBrowserImpl.isAvailable()) {
+            await inAppBrowserImpl.open(authUrl, {
+                // iOS Properties
+                dismissButtonStyle: 'cancel',
+                // Android Properties
+                showTitle: false,
+                enableUrlBarHiding: true,
+                enableDefaultShare: true,
+            })
+            // The deep link handler is called before the `InAppBrowser.open` promise resolves
+            // so we can tidy up happily here, in case there was a cancelled event
+            // this call may end up rejecting the promise _again_ (after it's already been
+            // resolved / rejected) but this is basically a noop
+            onFinish()
+        } else {
+            const unlistenAppState = createAppChangeListener(
+                (currentState: string) => {
+                    if (currentState === 'active') {
+                        // This is being run when
+                        // make sure the link handler is removed whenever we come back to the app
+                        // url is called first in the happy path so the promise will have resolved by then
+                        // otherwise, if they navigate back without authenticating, remove the listener and cancel the login
+
+                        // eslint-disable-next-line
+                        onFinish()
+                    }
+                },
+                appStateImpl,
+            )
+            unlisteners.push(unlistenAppState)
+            // open in the browser if in app browsers are not supported
+            linkingImpl.openURL(authUrl)
+        }
     })
 }
 
