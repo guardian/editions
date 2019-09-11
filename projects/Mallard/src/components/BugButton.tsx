e4e7da2854ad679239e2374275c43b47fb074ee9
RichieAHB <richard.beddington@guardian.co.uk>
2019-09-11 10:40:33 +0100

    Refactor to stylesheet

--- projects/Mallard/src/components/BugButton.tsx
+++ projects/Mallard/src/components/BugButton.tsx
@@ -4,16 +4,21 @@ import { AuthContext } from 'src/authentication/auth-context'
 import { createMailtoHandler } from 'src/helpers/diagnostics'
 import { Button } from './button/button'
 import { isInBeta } from 'src/helpers/release-stream'
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    button: {
+        position: 'absolute',
+        bottom: 20,
+        right: 20,
+    },
+})
 
 const BugButton = () => {
     const { status } = useContext(AuthContext)
     return isInBeta() ? (
         <Button
-            style={{
-                position: 'absolute',
-                bottom: 20,
-                right: 20,
-            }}
+            style={styles.button}
             onPress={createMailtoHandler('Report a bug', '', status)}
             alt="Report a bug"
             icon=""
