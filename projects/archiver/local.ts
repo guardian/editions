42e93ffe69422ce8a874ca382d1d5f4bb55bd9fc
Alex Ware <alex.ware@guardian.co.uk>
2019-09-18 16:55:55 +0100

    fix old code

--- projects/archiver/local.ts
+++ projects/archiver/local.ts
@@ -1,7 +1,7 @@
 import MemWatch from 'node-memwatch'
 import * as main from './main'
 import { Handler, Context } from 'aws-lambda'
-import { IssueId, IssueParams } from './src/issueTask'
+import { IssueParams } from './src/issueTask'
 //When run from terminal
 MemWatch.on('stats', stats => {
     console.log('GC ran', JSON.stringify(stats))
@@ -14,17 +14,11 @@ MemWatch.on('leak', ev => {
 })
 console.log('Node process running on', process.pid)
 const task = process.argv[2]
-const id = process.argv[3]
-const source = process.argv[4]
+const json = process.argv[3]
 
 if (!(task in main)) {
     console.error(`No task called ${task} present`)
     process.exit(1)
-}
-
-const issueId: IssueId = {
-    source,
-    id,
 }
 
 const handlers = (main as unknown) as {
@@ -32,7 +26,7 @@ const handlers = (main as unknown) as {
 }
 const handler = handlers[task]
 
-const run = handler({ issueId }, {} as Context, () => {}) as Promise<any>
+const run = handler(JSON.parse(json), {} as Context, () => {}) as Promise<any>
 
 run.then(x => {
     console.log(x)
