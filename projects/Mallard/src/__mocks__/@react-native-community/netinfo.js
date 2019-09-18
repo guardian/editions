16e8da0b3c3cdebeabf851b86e869dd4a67917a8
RichieAHB <richard.beddington@guardian.co.uk>
2019-09-18 11:28:16 +0100

    Fix mock

--- projects/Mallard/src/__mocks__/@react-native-community/netinfo.js
+++ projects/Mallard/src/__mocks__/@react-native-community/netinfo.js
@@ -2,4 +2,7 @@ module.exports = {
     getCurrentState: jest.fn(() => Promise.resolve()),
     addListener: jest.fn(),
     removeListeners: jest.fn(),
+    NetInfoStateType: {
+        unknown: 'unknown',
+    },
 }
