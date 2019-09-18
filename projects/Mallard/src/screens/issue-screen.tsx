3efd3638bcd811c0d479e7cb372af1965cf4da5b
RichieAHB <richard.beddington@guardian.co.uk>
2019-09-18 16:45:45 +0100

    Render clipped subviews

--- projects/Mallard/src/screens/issue-screen.tsx
+++ projects/Mallard/src/screens/issue-screen.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect } from 'react'
 import { Animated, StyleProp, StyleSheet, View, ViewStyle } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import {
@@ -120,9 +120,9 @@ const IssueFronts = ({
     const { container } = useIssueScreenSize()
 
     return (
-        <ScrollView style={style} removeClippedSubviews>
+        <ScrollView style={style}>
             {ListHeaderComponent}
-            {issue.fronts.map((key, index) => (
+            {issue.fronts.map(key => (
                 <Front
                     localIssueId={issue.localId}
                     publishedIssueId={issue.publishedId}
