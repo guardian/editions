2c77b59eed2155e2b287ef2802deeb5d43fcc343
Laura buns <lawr@lauragonzalez.cc>
2019-09-25 14:52:43 +0100

    split new/old webviews

--- projects/Mallard/src/components/article/types/article/webview.tsx
+++ projects/Mallard/src/components/article/types/article/webview.tsx
@@ -1,14 +1,13 @@
 import { useNetInfo } from '@react-native-community/netinfo'
 import React, { useMemo } from 'react'
-import { Animated, Linking, Platform } from 'react-native'
 import { WebView, WebViewProps } from 'react-native-webview'
-import { ArticleFeatures, BlockElement } from 'src/common'
+import { BlockElement } from 'src/common'
 import { useArticle } from 'src/hooks/use-article'
 import { useIssueCompositeKey } from 'src/hooks/use-issue-id'
-import { EMBED_DOMAIN, render } from '../../html/render'
-import { WrapLayout } from '../../wrap/wrap'
 import { ArticleHeaderProps } from '../../article-header/types'
-import { onShouldStartLoadWithRequest, features } from './helpers'
+import { render } from '../../html/render'
+import { WrapLayout } from '../../wrap/wrap'
+import { features, onShouldStartLoadWithRequest } from './helpers'
 
 const WebviewWithArticle = ({
     article,
