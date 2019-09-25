f949f2b28ce975dbdcc77fe56f42646b970de329
Laura buns <lawr@lauragonzalez.cc>
2019-09-25 13:08:53 +0100

    Update article.tsx

--- projects/Mallard/src/components/article/types/article.tsx
+++ projects/Mallard/src/components/article/types/article.tsx
@@ -1,20 +1,14 @@
-import React, { ReactNode, useState, useEffect } from 'react'
-import { Animated, Dimensions, StyleSheet, View, Platform } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler'
+import React, { ReactNode, useEffect, useState } from 'react'
+import { StyleSheet, View } from 'react-native'
 import { BlockElement } from 'src/common'
-import { safeInterpolation } from 'src/helpers/math'
 import { useArticle } from 'src/hooks/use-article'
+import { OnTopPositionChangeFn } from 'src/screens/article/helpers'
 import { metrics } from 'src/theme/spacing'
 import { Fader } from '../../layout/animators/fader'
-import { ArticleHeader } from '../article-header'
 import { ArticleHeaderProps } from '../article-header/types'
 import { PropTypes as StandfirstPropTypes } from '../article-standfirst'
 import { Wrap, WrapLayout } from '../wrap/wrap'
 import { WebviewWithArticle } from './article/webview'
-import {
-    wireScrollBarToDismiss,
-    OnTopPositionChangeFn,
-} from 'src/screens/article/helpers'
 
 const styles = StyleSheet.create({
     block: {
@@ -26,9 +20,6 @@ const styles = StyleSheet.create({
         ...StyleSheet.absoluteFillObject,
     },
     webview: {
-        backgroundColor: 'yellow',
-        width: '100%',
-        height: 100,
         flex: 1,
         /*
         The below line fixes crashes on Android
@@ -49,39 +40,19 @@ const ArticleWebView = ({
     onTopPositionChange: OnTopPositionChangeFn
     wrapLayout: WrapLayout
 }) => {
-    const [height, setHeight] = useState(Dimensions.get('window').height)
-    useEffect(() => {
-        onTopPositionChange(false)
-    }, [])
     return (
-        <View style={[styles.webview]}>
-            <WebviewWithArticle
-                {...webviewProps}
-                scrollEnabled={true}
-                useWebKit={false}
-                style={[styles.webview]}
-            />
-        </View>
+        <WebviewWithArticle
+            {...webviewProps}
+            scrollEnabled={true}
+            useWebKit={false}
+            style={[styles.webview]}
+        />
     )
 }
 
-const androidStyles = StyleSheet.create({
-    header: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        width: '100%',
-        zIndex: 999999,
-    },
-    wrapper: {
-        height: '100%',
-    },
-})
-
 const Article = ({
-    article,
     onTopPositionChange,
+    article,
     ...headerProps
 }: {
     article: BlockElement[]
@@ -89,24 +60,24 @@ const Article = ({
 } & ArticleHeaderProps &
     StandfirstPropTypes) => {
     const [wrapLayout, setWrapLayout] = useState<WrapLayout | null>(null)
-    const [, { type }] = useArticle()
+    useEffect(() => {
+        onTopPositionChange(false)
+    }, [])
 
     return (
-        <>
-            <Fader>
-                {wrapLayout && (
-                    <ArticleWebView
-                        header={<View />}
-                        headerProps={headerProps}
-                        article={article}
-                        onTopPositionChange={onTopPositionChange}
-                        wrapLayout={wrapLayout}
-                    />
-                )}
-
-                <Wrap onWrapLayout={setWrapLayout}></Wrap>
-            </Fader>
-        </>
+        <Fader>
+            {wrapLayout && (
+                <WebviewWithArticle
+                    headerProps={headerProps}
+                    article={article}
+                    scrollEnabled={true}
+                    useWebKit={false}
+                    style={[styles.webview]}
+                    wrapLayout={wrapLayout}
+                />
+            )}
+            <Wrap onWrapLayout={setWrapLayout}></Wrap>
+        </Fader>
     )
 }
 
