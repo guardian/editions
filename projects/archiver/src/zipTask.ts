070bbd8964e7eb494318267f33c0a98294f38958
Martin Kuzdowicz <martin.kuzdowicz@gmail.com>
2019-09-26 17:11:30 +0100

    skeleton for AppStore Notification + more logs

--- projects/archiver/src/zipTask.ts
+++ projects/archiver/src/zipTask.ts
@@ -4,10 +4,16 @@ import { imageSizes, issueDir } from '../common'
 import { zip } from '../zipper'
 import { UploadTaskOutput } from './issueUploadTask'
 import { putStatus } from './status'
+import { logInput, logOutput } from './log-utils'
+
 export const handler: Handler<UploadTaskOutput, UploadTaskOutput> = async ({
     issuePublication,
     issue,
 }) => {
+    logInput({
+        issuePublication,
+        issue,
+    })
     const { issueDate, version } = issuePublication
     const { publishedId } = issue
     const name = issueDir(publishedId)
@@ -28,9 +34,11 @@ export const handler: Handler<UploadTaskOutput, UploadTaskOutput> = async ({
     )
     console.log('Media zips uploaded.')
     await putStatus(issuePublication, 'published')
-    return {
+    const out: UploadTaskOutput = {
         issuePublication,
         issue,
         message: `Issue ${issueDate} zipped`,
     }
+    logOutput(out)
+    return out
 }
