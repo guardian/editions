29a9f26419f5ff5b20ba6a0b2de1170b64d07ae3
Simon Hildrew <simon@hildrew.net>
2019-09-26 12:10:26 +0100

    Ensure input isn't ordered

--- projects/archiver/src/indexer/_tests_/getIssues.spec.ts
+++ projects/archiver/src/indexer/_tests_/getIssues.spec.ts
@@ -4,9 +4,9 @@ describe('issueWindow', () => {
     it('should sort issues', () => {
         const sorted = issueWindow(
             [
+                { edition: 'daily-edition', issueDate: '2019-09-27' },
                 { edition: 'daily-edition', issueDate: '2019-09-30' },
                 { edition: 'daily-edition', issueDate: '2019-09-29' },
-                { edition: 'daily-edition', issueDate: '2019-09-27' },
             ],
             3,
         )
