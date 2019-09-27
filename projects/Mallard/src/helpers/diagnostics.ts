cad3152e9a2c7ecc523d962409a46e20dffa30c6
James Miller <james.miller.freelancer@guardian.co.uk>
2019-09-27 11:41:08 +0100

    chore: Pop the email back in

--- projects/Mallard/src/helpers/diagnostics.ts
+++ projects/Mallard/src/helpers/diagnostics.ts
@@ -111,8 +111,7 @@ const createMailtoHandler = (
             text: 'Include',
             onPress: async () => {
                 const diagnostics = await getDiagnosticInfo(authStatus)
-                console.log(diagnostics)
-                // openSupportMailto(text, releaseURL, diagnostics)
+                openSupportMailto(text, releaseURL, diagnostics)
             },
         },
         {
