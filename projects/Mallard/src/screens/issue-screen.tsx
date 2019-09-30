136f170276a39b57107dcb102906ec2a4200c8dd
Laura buns <lawr@lauragonzalez.cc>
2019-09-30 11:30:37 +0100

    Update issue-screen.tsx

--- projects/Mallard/src/screens/issue-screen.tsx
+++ projects/Mallard/src/screens/issue-screen.tsx
@@ -129,16 +129,14 @@ const IssueFronts = ({
     return (
         <ScrollView style={style}>
             {ListHeaderComponent}
-            {issue.fronts
-                .filter(f => f === 'Journal')
-                .map(key => (
-                    <Front
-                        localIssueId={issue.localId}
-                        publishedIssueId={issue.publishedId}
-                        front={key}
-                        key={key}
-                    />
-                ))}
+            {issue.fronts.map(key => (
+                <Front
+                    localIssueId={issue.localId}
+                    publishedIssueId={issue.publishedId}
+                    front={key}
+                    key={key}
+                />
+            ))}
             <View style={{ height: container.height / 2 }} />
         </ScrollView>
     )
