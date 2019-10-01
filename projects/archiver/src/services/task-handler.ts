91a10ff35da5b82978b7c01c2b8fa1103d69cf74
Simon Hildrew <simon@hildrew.net>
2019-10-01 18:32:59 +0100

    WIP for extracting dependencies

--- projects/archiver/src/services/task-handler.ts
+++ projects/archiver/src/services/task-handler.ts
@@ -4,9 +4,14 @@ import moment from 'moment'
 import {
     sendPublishStatusToTopic,
     createPublishEvent,
+    PublishEvent,
 } from './pub-status-notifier'
 import { Handler } from 'aws-lambda'
 import { logInput, logOutput } from '../utils/log'
+import { Attempt } from '../../../backend/utils/try'
+import { PromiseResult } from 'aws-sdk/lib/request'
+import SNS from 'aws-sdk/clients/sns'
+import { AWSError } from 'aws-sdk'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const errorToString = (err: any): string => {
@@ -18,9 +23,20 @@ const errorToString = (err: any): string => {
 
 type InputWithIdentifier = { issuePublication: IssuePublicationIdentifier }
 
+type HandlerDependencies = {
+    putStatus: (
+        issuePublication: IssuePublicationIdentifier,
+        status: Status,
+    ) => Promise<{ etag: string }>
+    sendPublishStatusToTopic: (
+        event: PublishEvent,
+    ) => Promise<Attempt<PromiseResult<SNS.PublishResponse, AWSError>>>
+}
+
 function handleAndNotifyInternal<I extends InputWithIdentifier, O>(
     statusOnSuccess: Status | undefined,
     handler: (input: I) => Promise<O>,
+    dependencies: HandlerDependencies,
 ): Handler<I, O> {
     return async (input: I) => {
         try {
@@ -28,19 +44,22 @@ function handleAndNotifyInternal<I extends InputWithIdentifier, O>(
             const result = await handler(input)
             logOutput(result)
             if (statusOnSuccess) {
-                await putStatus(input.issuePublication, statusOnSuccess)
+                await dependencies.putStatus(
+                    input.issuePublication,
+                    statusOnSuccess,
+                )
                 const now = moment()
                 const event = createPublishEvent(
                     input.issuePublication,
                     statusOnSuccess,
                     now,
                 )
-                await sendPublishStatusToTopic(event)
+                await dependencies.sendPublishStatusToTopic(event)
             }
             return result
         } catch (err) {
             // send failure notification
-            await sendPublishStatusToTopic({
+            await dependencies.sendPublishStatusToTopic({
                 ...input.issuePublication,
                 status: 'Failed',
                 message: errorToString(err),
@@ -52,6 +71,8 @@ function handleAndNotifyInternal<I extends InputWithIdentifier, O>(
     }
 }
 
+const runtimeHandlerDependencies = { putStatus, sendPublishStatusToTopic }
+
 /* This is a general handler that handles logging of input and output objects
  * and also notifications to the tooling topic on both success and failure
  */
@@ -59,7 +80,11 @@ export function handleAndNotify<I extends InputWithIdentifier, O>(
     statusOnSuccess: Status,
     handler: (input: I) => Promise<O>,
 ): Handler<I, O> {
-    return handleAndNotifyInternal(statusOnSuccess, handler)
+    return handleAndNotifyInternal(
+        statusOnSuccess,
+        handler,
+        runtimeHandlerDependencies,
+    )
 }
 
 /* This is a general handler that handles logging of input and output objects
@@ -68,5 +93,9 @@ export function handleAndNotify<I extends InputWithIdentifier, O>(
 export function handleAndNotifyOnError<I extends InputWithIdentifier, O>(
     handler: (input: I) => Promise<O>,
 ): Handler<I, O> {
-    return handleAndNotifyInternal(undefined, handler)
+    return handleAndNotifyInternal(
+        undefined,
+        handler,
+        runtimeHandlerDependencies,
+    )
 }
