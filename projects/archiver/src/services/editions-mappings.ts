c2125ca21c6f4c5a15ff5ce6cd9168bf8d67eacc
Martin Kuzdowicz <martin.kuzdowicz.contractor@guardian.co.uk>
2019-10-09 16:13:29 +0100

    Support multiple editions in archiver tasks (indexer, notifier) (#670)
    * Support multiple editions in archiver tasks
    
    * add Scheduled Or Skipped status to notifier output
    
    * simplifying ScheduleDeviceNotification payload creation
    
    * refining NotificationStatus and simplifications

---
+++ projects/archiver/src/services/editions-mappings.ts
@@ -0,0 +1,18 @@
+const createEditionToDisplayNameMap = () => {
+    const map = new Map<string, string>()
+    map.set('daily-edition', 'Daily Edition')
+    map.set('american-edition', 'American Edition')
+    map.set('australian-edition', 'Australian Edition')
+    map.set('training-edition', 'Training Edition')
+    return map
+}
+
+const editionToName = createEditionToDisplayNameMap()
+
+export const getEditionDisplayName = (edition: string): string => {
+    if (!editionToName.has(edition)) {
+        throw new Error(`${edition} missing in editionToName mapping`)
+    }
+    // it will never be empty string because of above check, || '' is made to make compiler happy
+    return editionToName.get(edition) || ''
+}
