56c0fd8d20e7415dde7accaca4a4326a916fcee0
Laura buns <lawr@lauragonzalez.cc>
2019-10-17 14:43:50 +0100

    fix lint

--- projects/Mallard/src/components/article/index.tsx
+++ projects/Mallard/src/components/article/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react'
+import { ScrollView } from 'react-native'
 import { CAPIArticle } from 'src/common'
 import { FlexErrorMessage } from 'src/components/layout/ui/errors/flex-error-message'
-import { color } from 'src/theme/color'
-import { Article as OldArticle, ArticleTheme } from './types/article'
-import { Article } from './types/article'
-import { Crossword } from './types/crossword'
-import { Gallery } from './types/gallery'
-import { Cartoon } from './types/cartoon'
-
-import { ScrollView } from 'react-native'
 import {
     OnTopPositionChangeFn,
     wireScrollBarToDismiss,
 } from 'src/screens/article/helpers'
-import { useOtherSettingsValues } from 'src/hooks/use-settings'
+import { color } from 'src/theme/color'
+import { Article, ArticleTheme } from './types/article'
+import { Crossword } from './types/crossword'
+import { Gallery } from './types/gallery'
 
 /*
 This is the article view! For all of the articles.
