927c7b66b15d2a6c82e7ca7f331316ea7dfd593b
Laura buns <lawr@lauragonzalez.cc>
2019-10-18 12:36:01 +0100

    remove unused imports

--- projects/Mallard/src/components/article/index.tsx
+++ projects/Mallard/src/components/article/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
-import { ScrollView } from 'react-native'
 import { CAPIArticle } from 'src/common'
 import { FlexErrorMessage } from 'src/components/layout/ui/errors/flex-error-message'
-import {
-    OnTopPositionChangeFn,
-    wireScrollBarToDismiss,
-} from 'src/screens/article/helpers'
+import { OnTopPositionChangeFn } from 'src/screens/article/helpers'
 import { color } from 'src/theme/color'
-import { Article, ArticleTheme } from './types/article'
-import { Crossword } from './types/crossword'
-import { Gallery } from './types/gallery'
 import { ErrorBoundary } from '../layout/ui/errors/error-boundary'
+import { Article } from './types/article'
+import { Crossword } from './types/crossword'
 
 /*
 This is the article view! For all of the articles.
