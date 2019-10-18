f03345ce427aa7253a0511152b52d8037e85d8b4
Jean Lauliac <lauliacj@gmail.com>
2019-10-18 10:42:21 +0100

    add star ratings on text-only items

--- projects/Mallard/src/components/front/items/small-items.tsx
+++ projects/Mallard/src/components/front/items/small-items.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import { ItemTappable, PropTypes } from './helpers/item-tappable'
 import { TextBlock } from './helpers/text-block'
 import { SuperHeroImageItem } from './super-items'
+import { Stars } from 'src/components/stars/stars'
+import { View } from 'react-native'
 
 const SmallItem = ({ article, size, ...tappableProps }: PropTypes) => {
     return (
         <ItemTappable {...tappableProps} {...{ article }}>
+            <View style={{ flexDirection: 'row', marginBottom: 4 }}>
+                {article.starRating && (
+                    <Stars style={{ flex: 0 }} rating={article.starRating} />
+                )}
+            </View>
             <TextBlock
                 byline={article.byline}
                 kicker={article.kicker}
