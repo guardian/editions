7a56ffbe41280b260750fb544c9dee633b06ced5
Jean Lauliac <lauliacj@gmail.com>
2019-10-21 15:09:39 +0100

    fixes

--- projects/Mallard/src/components/front/items/small-items.tsx
+++ projects/Mallard/src/components/front/items/small-items.tsx
@@ -1,18 +1,35 @@
 import React from 'react'
+import { StyleSheet } from 'react-native'
 import { ItemTappable, PropTypes } from './helpers/item-tappable'
 import { TextBlock } from './helpers/text-block'
 import { SuperHeroImageItem } from './super-items'
 import { Stars } from 'src/components/stars/stars'
 import { View } from 'react-native'
+import { CAPIArticle } from 'src/common'
+
+const styles = StyleSheet.create({
+    starsWrapper: {
+        flexDirection: 'row',
+        marginBottom: 4,
+    },
+    stars: {
+        flex: 0,
+    },
+})
+
+const StarsWrapper = ({ article }: { article: CAPIArticle }) => {
+    if (article.type != 'article' || article.starRating == null) return null
+    return (
+        <View style={styles.starsWrapper}>
+            <Stars style={styles.stars} rating={article.starRating} />
+        </View>
+    )
+}
 
 const SmallItem = ({ article, size, ...tappableProps }: PropTypes) => {
     return (
         <ItemTappable {...tappableProps} {...{ article }}>
-            <View style={{ flexDirection: 'row', marginBottom: 4 }}>
-                {article.starRating && (
-                    <Stars style={{ flex: 0 }} rating={article.starRating} />
-                )}
-            </View>
+            <StarsWrapper article={article} />
             <TextBlock
                 byline={article.byline}
                 kicker={article.kicker}
