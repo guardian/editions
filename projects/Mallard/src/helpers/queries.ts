4ee7939dc81ad680be4d24cdc42af09ae04fb0c2
Jean Lauliac <lauliacj@gmail.com>
2019-11-21 17:17:58 +0000

    fixes

--- projects/Mallard/src/helpers/queries.ts
+++ projects/Mallard/src/helpers/queries.ts
@@ -1,30 +1,62 @@
 import { useEffect, useState, useContext, createContext } from 'react'
 
+/**
+ * Keeps track of the state of a specific query run with specific variables.
+ */
 type QueryNode = {
+    /* Identifies uniquely this node in the graph, built from the query ID and
+     * variables. */
     key: string
+    /* The query being run. */
     query: Query<unknown, unknown>
-    promise: Promise<unknown> | undefined
-    value: unknown
-    error: unknown
+    /* The variables used to run the query. */
     variables: unknown
+    /* If the query is running right now, this will be defined. Set to
+     * `undefined` once we got a result value (or an error). */
+    promise: Promise<unknown> | undefined
+    /* The value the query resolved to. `undefined` if we didn't
+     * get a result yet. */
+    value: unknown
+    /* Same as `value` but in case the query was rejected. */
+    error: unknown
+    /* Keeps track of the keys of other nodes this query run depend on. */
     deps: Set<string>
+    /* Reverse-linking of `deps` ("predecessors"). */
     preds: Set<string>
+    /* Functions that must be called when the value/error state changes. */
     listeners: (() => void)[]
 }
 
-type AllowedVariables = { [key: string]: number | string }
+/**
+ * Only plain JavaScript types are allowed in variables because it needs to
+ * be serializable with `JSON.stringify()`
+ */
+type AllowedVariableValues = number | string | boolean
+type AllowedVariables = {
+    [key: string]: AllowedVariableValues
+}
 
+/**
+ * Resolves a query which the currently running query depends on.
+ */
 export type LocalResolver = <Value, Variables>(
     query: Query<Value, Variables>,
     variables: Variables,
 ) => Promise<Value>
 
+/**
+ * Entry point for a query.
+ */
 export type QueryResolver<Value, Variables> = (
     variables: Variables,
     resolve: LocalResolver,
     prevValue: Value,
 ) => Promise<Value>
 
+/**
+ * Explicitly adding `undefined` fields makes refinement easier
+ * for consumer code.
+ */
 export type QueryResult<Value> =
     | { loading: true; value: undefined; error: undefined }
     | { loading: undefined; value: Value; error: undefined }
@@ -38,12 +70,24 @@ const formatResult = <Value>(node: QueryNode): QueryResult<Value> => {
 
 const EMPTY_STR_SET: Set<string> = new Set()
 
+class QueryAbortedError extends Error {
+    constructor() {
+        super(
+            'The current query has been aborted because one of its ' +
+                'dependencies got invalidated.',
+        )
+    }
+}
+
+/**
+ * Keeps track of query promises, results, errors and inter-dependencies.
+ */
 export class QueryEnvironment {
     private _graph = new Map<string, QueryNode>()
 
     /**
-     * Given the specific `query`, call `callback` everytime the value changes.
-     * If the query hasn't been resolved yet, this function will do so.
+     * Given the specific `query`, call `callback` every time its value changes.
+     * If the query hasn't been started yet, this function will do so.
      */
     watch<Value, Variables extends AllowedVariables>(
         query: Query<Value, Variables>,
@@ -61,6 +105,11 @@ export class QueryEnvironment {
         }
     }
 
+    /**
+     * Look at the current value of a particular `query` without having any
+     * side effect. If the query has never been run and is not running, return
+     * as if it was loading, but do not trigger an actual run.
+     */
     peek<Value, Variables extends AllowedVariables>(
         query: Query<Value, Variables>,
         variables: Variables,
@@ -71,6 +120,12 @@ export class QueryEnvironment {
         return formatResult(node)
     }
 
+    /**
+     * Mark a `query` with particular `variables` as now being stale and
+     * in need to be run again. If it never ran before, nothing happens.
+     * Otherwise it is run along all the queries depending on it, and listeners
+     * are notified of the new values.
+     */
     invalidate<Value, Variables extends AllowedVariables>(
         query: Query<Value, Variables>,
         variables: Variables,
@@ -112,10 +167,51 @@ export class QueryEnvironment {
         return node
     }
 
+    private _resolve(
+        parentNode: QueryNode,
+        run: { promise: Promise<unknown> | undefined },
+        query: Query<unknown, unknown>,
+        variables: unknown,
+    ) {
+        if (run.promise !== parentNode.promise) throw new QueryAbortedError()
+
+        const subNode = this._getNode(query, variables)
+        parentNode.deps.add(subNode.key)
+        subNode.preds.add(parentNode.key)
+        if (subNode.promise !== undefined) return subNode.promise
+        if (subNode.value !== undefined) return Promise.resolve(subNode.value)
+        if (subNode.error !== undefined) return Promise.reject(subNode.error)
+        throw new Error('inconsistent graph')
+    }
+
+    private async _handleNodePromise(
+        node: QueryNode,
+        promise: Promise<unknown>,
+    ) {
+        let value, error
+        try {
+            value = await promise
+        } catch (err) {
+            error = err
+        }
+        // if the node has been refreshed again in between, we ignore results
+        if (node.promise !== promise) return
+        node.promise = undefined
+        node.value = value
+        node.error = error
+
+        for (const listener of node.listeners) {
+            listener()
+        }
+    }
+
     private _refresh(node: QueryNode, trail: Set<string>) {
         if (trail.has(node.key))
             throw new Error('circular dependency between queries')
 
+        // We first remove all our dependencies. This is because the new run
+        // of the resolver might well depend on a different set of
+        // queries/variables, so we're going to collect them from scratch.
         for (const depKey of node.deps) {
             const dep = this._graph.get(depKey)
             if (dep === undefined || !dep.preds.has(node.key))
@@ -124,57 +220,31 @@ export class QueryEnvironment {
         }
         node.deps.clear()
 
-        const resolve = (
-            query: Query<unknown, unknown>,
-            variables: unknown,
-        ) => {
-            const subNode = this._getNode(query, variables)
-            node.deps.add(subNode.key)
-            subNode.preds.add(node.key)
-            if (subNode.promise !== undefined) return subNode.promise
-            if (subNode.value !== undefined)
-                return Promise.resolve(subNode.value)
-            if (subNode.error !== undefined)
-                return Promise.reject(subNode.error)
-            throw new Error('inconsistent graph')
-        }
-
         const prevValue = node.value
+        let run = { promise: undefined as (Promise<unknown> | undefined) }
+        const resolve: any = this._resolve.bind(this, node, run)
+
+        // Use `Promise.resolve()` to ensure that `resolve` is only ever called
+        // when this function is done running (ie. asynchronously).
         const promise = Promise.resolve().then(() =>
-            node.query.resolver(
-                node.variables,
-                (resolve as unknown) as LocalResolver,
-                prevValue,
-            ),
+            node.query.resolver(node.variables, resolve, prevValue),
         )
+        run.promise = promise
         node.promise = promise
-        promise
-            .then(
-                value => {
-                    if (node.promise !== promise) return
-                    node.promise = undefined
-                    node.value = value
-                },
-                error => {
-                    if (node.promise !== promise) return
-                    node.promise = undefined
-                    node.error = error
-                },
-            )
-            .then(() => {
-                for (const listener of node.listeners) {
-                    listener()
-                }
+        this._handleNodePromise(node, promise).catch(error => {
+            // "Escape" the Promise to throw in the global context so that
+            // this triggers the global "unhandled errors" handler.
+            setImmediate(() => {
+                throw error
             })
-            .catch(error => {
-                setImmediate(() => {
-                    throw error
-                })
-            })
+        })
 
         const innerTrail = new Set(trail)
         innerTrail.add(node.key)
 
+        // Now that the promise is set-up, trigger a refresh of all the nodes
+        // that depends on the one being refreshed. They'll likely try to
+        // resolve it again, and thus await on our fresh promise.
         for (const predKey of node.preds) {
             const pred = this._graph.get(predKey)
             if (pred === undefined || !pred.deps.has(node.key))
@@ -183,6 +253,10 @@ export class QueryEnvironment {
         }
     }
 
+    /**
+     * When there is no more listeners or predecessors on a node, we can
+     * forget about it and drop the data.
+     */
     private _releaseNode(node: QueryNode): void {
         if (node.listeners.length > 0 || node.preds.size > 0) return
         for (const depKey of node.deps) {
@@ -198,20 +272,12 @@ export class QueryEnvironment {
 export class Query<Value, Variables> {
     private static _nextId = 1
 
-    private _resolver: QueryResolver<Value, Variables>
-    private _id: number
+    public readonly resolver: QueryResolver<Value, Variables>
+    public readonly id: number
 
     private constructor(resolver: QueryResolver<Value, Variables>) {
-        this._id = Query._nextId++
-        this._resolver = resolver
-    }
-
-    get id() {
-        return this._id
-    }
-
-    get resolver() {
-        return this._resolver
+        this.id = Query._nextId++
+        this.resolver = resolver
     }
 
     static create<Value, Variables>(
@@ -231,6 +297,11 @@ export const useQueryEnvironment = (): QueryEnvironment => {
     return env
 }
 
+/**
+ * From a React component, give the current status of a `query` and re-render
+ * when any changes happens. For example if the query is invalidated and the
+ * result is different.
+ */
 export const useQuery = <Value, Variables extends AllowedVariables>(
     query: Query<Value, Variables>,
     variables: Variables,
@@ -238,12 +309,11 @@ export const useQuery = <Value, Variables extends AllowedVariables>(
     const env = useQueryEnvironment()
     const [result, setResult] = useState(env.peek(query, variables))
     const serializedVars = JSON.stringify(variables)
-    useEffect(
-        () =>
-            env.watch(query, JSON.parse(serializedVars), newResult => {
-                setResult(newResult)
-            }),
+    useEffect(() => env.watch(query, JSON.parse(serializedVars), setResult), [
+        // We don't want to depend on an object by referential equality, but
+        // instead rely on the object as a value. The easiest way to do this is
+        // to serialize that value.
         [env, query, serializedVars],
-    )
+    ])
     return result
 }
