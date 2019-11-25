8035be84537787377c12969e3c34fda04ecae478
James Miller <james.miller.freelancer@guardian.co.uk>
2019-11-25 16:29:02 +0000

    chore: Moving Crosswords to Monorepo

--- projects/Mallard/scripts/bundle-html.js
+++ projects/Mallard/scripts/bundle-html.js
@@ -1,22 +1,4 @@
-const { exec } = require('child_process')
-const bundles = require('../html/manifest')
-const chalk = require('chalk')
+const bundleHTML = require('../../Apps/editions-crossword-renderer-app/scripts/bundle-html')
+    .bundleHTML
 
-for (const { project, buildScript, buildPath, key } of Object.values(bundles)) {
-    exec(
-        [
-            `cd ../../projects/${project}`,
-            buildScript,
-            `rm -rf ../../projects/Mallard/html/${key}.bundle`,
-            `mv ./${buildPath} ../../projects/Mallard/html/${key}.bundle`,
-        ].join(' && '),
-        (err, stdout, stderr) => {
-            if (err || stderr) {
-                console.error('Failed building bundle ' + key)
-                console.error(stderr)
-                return
-            }
-            console.log(chalk.green(`Bundled ${key}`))
-        },
-    )
-}
+bundleHTML()
