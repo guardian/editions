375277eb4440ee1f4abdbf366a0f34137fa2b7d4
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2019-11-29 16:23:59 +0000

    Introduce new copy task to copy an issue from proof bucket to publish bucket

---
+++ projects/archiver/src/tasks/copier/index.ts
@@ -0,0 +1,42 @@
+import { Handler } from 'aws-lambda'
+import { handleAndNotify } from '../../services/task-handler'
+import { IssueTaskOutput } from '../issue'
+import { IndexTaskOutput } from '../indexer'
+import { copy, getBucket, list } from '../../utils/s3'
+import { issuePath } from '../../../../Apps/common/src'
+import { attempt, hasFailed } from '../../../../backend/utils/try'
+
+type CopyTaskInput = IndexTaskOutput
+export type CopyTaskOutput = Pick<
+    IssueTaskOutput,
+    'issuePublication' | 'message' | 'issue'
+>
+
+export const handler: Handler<CopyTaskInput, CopyTaskOutput> = handleAndNotify(
+    'copied',
+    async ({ issuePublication, issue }) => {
+        const inputBucket = getBucket('proof')
+        const outputBucket = getBucket('publish')
+        console.log(`Copying all files from ${inputBucket} to ${outputBucket}`)
+        // List all keys
+        const baseKey = issuePath(issue.key)
+        const keys = await list(inputBucket, baseKey)
+
+        // Loop over them creating copy promises
+        // Gather the promises into one and return
+        const copyPromises = await Promise.all(
+            keys.objects.map(key =>
+                attempt(copy(key.toString(), inputBucket, outputBucket)),
+            ),
+        )
+
+        if (copyPromises.filter(hasFailed).length)
+            throw new Error('Failed to copy some objects')
+
+        return {
+            issuePublication,
+            message: 'Issue copied successfully',
+            issue,
+        }
+    },
+)
