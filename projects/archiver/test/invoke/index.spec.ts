e9f4a8286659446310e119991677cb433e0eaf54
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2019-12-02 16:45:12 +0000

    Lint

--- projects/archiver/test/invoke/index.spec.ts
+++ projects/archiver/test/invoke/index.spec.ts
@@ -7,7 +7,7 @@ import {
 } from '../../common'
 
 describe('state machine invoker', () => {
-    const obejctsContentsInput =
+    const objectsContentsInput =
         '{"id":"ff8936b8-cc57-4572-9a50-bd779319f726","name":"american-edition","edition":"american-edition","issueDate":"2019-10-09","version":"2019-10-04T16:08:35.951Z","fronts":[{"id":"cde7a75d-5fdc-4468-aefe-aac676f4090e","name":"National","collections":[{"id":"3e16f025-690f-4ea3-9e58-7aa779b20df4","name":"Front Page","items":[{"internalPageCode":6668324,"furniture":{"showByline":false,"showQuotedHeadline":false,"mediaType":"useArticleTrail","overrideArticleMainMedia":false}}]},{"id":"47539d9a-bd1a-46d6-85d8-d6a69daada2e","name":"UK News","items":[{"internalPageCode":6668444,"furniture":{"showByline":false,"showQuotedHeadline":false,"mediaType":"useArticleTrail","overrideArticleMainMedia":false}},{"internalPageCode":6668479,"furniture":{"showByline":false,"showQuotedHeadline":false,"mediaType":"useArticleTrail","overrideArticleMainMedia":false}}]}],"swatch":"news"}]}'
 
     const recordsInput: Record[] = [
@@ -35,7 +35,7 @@ describe('state machine invoker', () => {
             stateMachineInvoke: invokeStateMachineAllwaysSuccess,
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             s3fetch: (params: GetS3ObjParams) => {
-                return Promise.resolve(obejctsContentsInput)
+                return Promise.resolve(objectsContentsInput)
             },
         }
 
@@ -67,7 +67,7 @@ describe('state machine invoker', () => {
             stateMachineInvoke: invokeStateMachineAllwaysFails,
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             s3fetch: (params: GetS3ObjParams) => {
-                return Promise.resolve(obejctsContentsInput)
+                return Promise.resolve(objectsContentsInput)
             },
         }
 
