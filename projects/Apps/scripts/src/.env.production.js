e79fefa8f6467ff1a48c030ec2c4fc2844d45d4d
James Miller <james.miller.freelancer@guardian.co.uk>
2019-12-18 10:55:20 +0000

    chore: Move Env script to monorepo

--- projects/Apps/scripts/src/.env.production.js
+++ projects/Apps/scripts/src/.env.production.js
@@ -1,9 +1,12 @@
 const path = require('path')
 const fs = require('fs')
+const argv = require('yargs').argv
 
 const NEWLINE = '\n'
 const removeWhiteSpace = s => s.trim()
 const isNonEmpty = s => s
+
+const appRelativePath = argv.appRelativePath || '../../../Mallard/'
 
 /**
  *
@@ -44,4 +47,23 @@ const writeEnvVarsToFiles = (...paths) => strings =>
             .join(NEWLINE),
     )
 
-module.exports = { writeEnvVarsToFiles }
+writeEnvVarsToFiles(`${appRelativePath}.env`)`
+ID_ACCESS_TOKEN
+ID_API_URL
+MEMBERS_DATA_API_URL
+SENTRY_DSN_URL
+ITUNES_CONNECT_SHARED_SECRET
+USE_SANDBOX_IAP
+ANDROID_RELEASE_STREAM
+`
+
+writeEnvVarsToFiles(
+    `${appRelativePath}android/sentry.properties`,
+    `${appRelativePath}ios/sentry.properties`,
+)`
+defaults.url=SENTRY_DEFAULTS_URL
+defaults.org=SENTRY_DEFAULTS_ORG
+defaults.project=SENTRY_DEFAULTS_PROJECT
+auth.token=SENTRY_AUTH_TOKEN
+cli.executable=SENTRY_CLI_EXECUTABLE
+`
