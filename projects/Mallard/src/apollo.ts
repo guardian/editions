b1c6a0776ca2b961a7ec9927f3574f351fe81bf3
Jean Lauliac <jean.lauliac@guardian.co.uk>
2019-12-20 11:21:22 +0000

    useScaledImage: cache the result (#952)

--- projects/Mallard/src/apollo.ts
+++ projects/Mallard/src/apollo.ts
@@ -7,29 +7,8 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { SETTINGS_RESOLVERS } from './helpers/settings/resolvers'
 import { resolveWeather } from './helpers/weather'
 import { resolveLocationPermissionStatus } from './helpers/location-permission'
+import { createScaledImageResolver } from './hooks/use-image-paths'
 import { initIssueSummary } from './hooks/use-issue-summary'
-
-/**
- * Resolvers is what Apollo uses to get the value of field that has never been
- * cached before. For example if you have a query `{ foo @client }`, Apollo
- * looks for the field `foo` at the root of the cache. If not available, it'll
- * look for the resolver `Query.foo` (`Query` is just the "namespace" where
- * all queries start).
- *  *
- * Resolvers are functions, which can be asynchonous. They can gather data from
- * local storage (ex. settings), from local APIs (ex. permissions, geolocation),
- * from remote APIs (ex. REST API).
- *
- * Once a resolver returns a value, Apollo will refresh automatically
- * any component that was requiring this in a query via the `useQuery` function.
- */
-const RESOLVERS = {
-    Query: {
-        ...SETTINGS_RESOLVERS,
-        weather: resolveWeather,
-        locationPermissionStatus: resolveLocationPermissionStatus,
-    },
-}
 
 /**
  * The `Link` is the interface Apollo uses to reach GraphQL servers. Since we
@@ -53,10 +32,33 @@ const link = {
 }
 
 export const createApolloClient = () => {
+    /**
+     * Resolvers is what Apollo uses to get the value of field that has never been
+     * cached before. For example if you have a query `{ foo @client }`, Apollo
+     * looks for the field `foo` at the root of the cache. If not available, it'll
+     * look for the resolver `Query.foo` (`Query` is just the "namespace" where
+     * all queries start).
+     *  *
+     * Resolvers are functions, which can be asynchonous. They can gather data from
+     * local storage (ex. settings), from local APIs (ex. permissions, geolocation),
+     * from remote APIs (ex. REST API).
+     *
+     * Once a resolver returns a value, Apollo will refresh automatically
+     * any component that was requiring this in a query via the `useQuery` function.
+     */
+    const resolvers = {
+        Query: {
+            ...SETTINGS_RESOLVERS,
+            weather: resolveWeather,
+            locationPermissionStatus: resolveLocationPermissionStatus,
+            scaledImage: createScaledImageResolver(),
+        },
+    }
+
     const client = new ApolloClient({
         cache: new InMemoryCache(),
         link,
-        resolvers: RESOLVERS,
+        resolvers,
     })
 
     initIssueSummary(client)
