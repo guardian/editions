98375467c4cff0e48257d78259f0eb88f84235ce
James Miller <james.miller.freelancer@guardian.co.uk>
2020-01-20 13:27:43 +0000

    chore: Fix crashing app?

---
+++ projects/Apps/index.js
@@ -0,0 +1,11 @@
+import { AppRegistry, YellowBox, Text } from 'react-native'
+import App from '@guardian/editions-src'
+
+// In lieu of a wrapper component (i.e. <UnscaledText />), this quickly opts us out of scaled text globally.
+Text.defaultProps = Text.defaultProps || {}
+Text.defaultProps.allowFontScaling = false
+
+// Supress Could Not Find Image warnings as a result of our approach to find the image locally
+YellowBox.ignoreWarnings([])
+
+AppRegistry.registerComponent('Mallard', () => App)
