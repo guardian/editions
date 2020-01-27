af1c8c6d5fe7f0808b339b428e461de7d71334a5
James Miller <james.miller.freelancer@guardian.co.uk>
2020-01-27 15:58:46 +0000

    chore: Config Provider

--- projects/Mallard/src/helpers/screen.ts
+++ projects/Mallard/src/helpers/screen.ts
@@ -6,7 +6,7 @@ import {
     imageSizes,
 } from '../../../Apps/common/src'
 import AsyncStorage from '@react-native-community/async-storage'
-import { largeDeviceMemory } from 'src/hooks/use-config'
+import { useLargeDeviceMemory } from 'src/hooks/use-config-provider'
 
 export const maxScreenSize = (): number => {
     const { width, height } = Dimensions.get('window')
@@ -58,7 +58,7 @@ export const imageForScreenSize = async (): Promise<ImageSize> => {
     }
 
     // Provide only phone images for low powered devices
-    const largeAmountOfMemory = await largeDeviceMemory()
+    const largeAmountOfMemory = useLargeDeviceMemory()
     if (!largeAmountOfMemory) {
         return 'phone'
     }
