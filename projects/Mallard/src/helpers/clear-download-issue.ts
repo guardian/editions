608a51fb4093f617bd6c6eacd22cabc0829e23ad
James Miller <james.miller.freelancer@guardian.co.uk>
2020-02-03 12:22:12 +0000

    chore: Only download on open on large Ram devices (#1017)

--- projects/Mallard/src/helpers/clear-download-issue.ts
+++ projects/Mallard/src/helpers/clear-download-issue.ts
@@ -1,7 +1,9 @@
-import { prepFileSystem, clearOldIssues, downloadTodaysIssue } from './files'
-import { fetchCacheClear } from './fetch'
 import ApolloClient from 'apollo-client'
+import { pushDownloadFailsafe } from 'src/helpers/push-download-failsafe'
+import { fetchCacheClear } from './fetch'
+import { clearOldIssues, downloadTodaysIssue, prepFileSystem } from './files'
 import { cleanPushTrackingByDays } from './push-tracking'
+import { largeDeviceMemory } from 'src/hooks/use-config-provider'
 
 const clearAndDownloadIssue = async (client: ApolloClient<object>) => {
     await prepFileSystem()
@@ -9,7 +11,13 @@ const clearAndDownloadIssue = async (client: ApolloClient<object>) => {
     await cleanPushTrackingByDays()
     const weOk = await fetchCacheClear()
     if (weOk) {
-        return await downloadTodaysIssue(client)
+        // Check to see if the device has a decent amount of memory before doing intensive tasks
+        const largeRAM = await largeDeviceMemory()
+        if (largeRAM) {
+            pushDownloadFailsafe(client)
+            return await downloadTodaysIssue(client)
+        }
+        return
     }
 }
 
