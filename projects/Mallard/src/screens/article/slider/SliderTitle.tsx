570b1ecd7c660ec631d334868c8e700ca3d3fbf7
James Miller <james.miller.freelancer@guardian.co.uk>
2020-02-14 16:34:18 +0000

    chore: Memoize the SliderTitle

--- projects/Mallard/src/screens/article/slider/SliderTitle.tsx
+++ projects/Mallard/src/screens/article/slider/SliderTitle.tsx
@@ -77,85 +77,87 @@ const styles = (color: string, location: string, isTablet: boolean) => {
     })
 }
 
-const SliderTitle = ({
-    title,
-    numOfItems,
-    color,
-    location = 'article',
-    subtitle,
-    position,
-    startIndex,
-}: SliderTitleProps) => {
-    const dots = []
-    const isTablet = DeviceInfo.isTablet()
-    const appliedStyle = styles(color, location, isTablet)
-    // takes a key e.g. O:Top Stories and provides the end part
-    const transformedSubtitle =
-        subtitle && subtitle.split(':')[subtitle.split(':').length - 1]
+const SliderTitle = React.memo(
+    ({
+        title,
+        numOfItems,
+        color,
+        location = 'article',
+        subtitle,
+        position,
+        startIndex,
+    }: SliderTitleProps) => {
+        const dots = []
+        const isTablet = DeviceInfo.isTablet()
+        const appliedStyle = styles(color, location, isTablet)
+        // takes a key e.g. O:Top Stories and provides the end part
+        const transformedSubtitle =
+            subtitle && subtitle.split(':')[subtitle.split(':').length - 1]
 
-    const newPos: any =
-        Platform.OS === 'android' && startIndex
-            ? Number(position) - startIndex
-            : startIndex
-            ? Animated.subtract(position, startIndex)
-            : position
+        const newPos: any =
+            Platform.OS === 'android' && startIndex
+                ? Number(position) - startIndex
+                : startIndex
+                ? Animated.subtract(position, startIndex)
+                : position
 
-    const largeDeviceMemory = useLargeDeviceMemory()
-    const range = (i: number) =>
-        largeDeviceMemory
-            ? {
-                  inputRange: [
-                      i - 0.50000000001,
-                      i - 0.5,
-                      i,
-                      i + 0.5,
-                      i + 0.50000000001,
-                  ],
-                  outputRange: ['#DCDCDC', color, color, color, '#DCDCDC'],
-              }
-            : {
-                  inputRange: [i - 1, i, i + 1],
-                  outputRange: ['#DCDCDC', color, '#DCDCDC'],
-              }
+        const largeDeviceMemory = useLargeDeviceMemory()
+        const range = (i: number) =>
+            largeDeviceMemory
+                ? {
+                      inputRange: [
+                          i - 0.50000000001,
+                          i - 0.5,
+                          i,
+                          i + 0.5,
+                          i + 0.50000000001,
+                      ],
+                      outputRange: ['#DCDCDC', color, color, color, '#DCDCDC'],
+                  }
+                : {
+                      inputRange: [i - 1, i, i + 1],
+                      outputRange: ['#DCDCDC', color, '#DCDCDC'],
+                  }
 
-    for (let i = 0; i < numOfItems; i++) {
-        const backgroundColor =
-            Platform.OS === 'android' && location === 'article'
-                ? i === newPos
-                    ? color
-                    : '#DCDCDC'
-                : newPos.interpolate({
-                      ...range(i),
-                      extrapolate: 'clamp',
-                  })
+        for (let i = 0; i < numOfItems; i++) {
+            const backgroundColor =
+                Platform.OS === 'android' && location === 'article'
+                    ? i === newPos
+                        ? color
+                        : '#DCDCDC'
+                    : newPos.interpolate({
+                          ...range(i),
+                          extrapolate: 'clamp',
+                      })
 
-        dots.push(
-            <Animated.View
-                key={`${title}${i}`}
-                style={[
-                    appliedStyle.dot,
-                    {
-                        backgroundColor,
-                    },
-                ]}
-            ></Animated.View>,
-        )
-    }
+            dots.push(
+                <Animated.View
+                    key={`${title}${i}`}
+                    style={[
+                        appliedStyle.dot,
+                        {
+                            backgroundColor,
+                        },
+                    ]}
+                ></Animated.View>,
+            )
+        }
 
-    return (
-        <View style={appliedStyle.container}>
-            <View style={appliedStyle.titleContainer}>
-                <Text style={appliedStyle.title}>{title}</Text>
-                {transformedSubtitle !== title && (
-                    <Text style={appliedStyle.subtitle}>
-                        {' '}
-                        {transformedSubtitle}
-                    </Text>
-                )}
+        return (
+            <View style={appliedStyle.container}>
+                <View style={appliedStyle.titleContainer}>
+                    <Text style={appliedStyle.title}>{title}</Text>
+                    {transformedSubtitle !== title && (
+                        <Text style={appliedStyle.subtitle}>
+                            {' '}
+                            {transformedSubtitle}
+                        </Text>
+                    )}
+                </View>
+                <View style={appliedStyle.dotsContainer}>{dots}</View>
             </View>
-            <View style={appliedStyle.dotsContainer}>{dots}</View>
-        </View>
-    )
-}
+        )
+    },
+)
 
 export { SliderTitle, SLIDER_FRONT_HEIGHT, SliderTitleProps }
