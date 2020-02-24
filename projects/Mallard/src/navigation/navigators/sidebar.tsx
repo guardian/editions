cd034c3d72a19c245b843db36823a1902ab4c684
James Miller <james.miller.freelancer@guardian.co.uk>
2020-02-24 16:50:05 +0000

    chore: Re-revert sidebar changes

--- projects/Mallard/src/navigation/navigators/sidebar.tsx
+++ projects/Mallard/src/navigation/navigators/sidebar.tsx
@@ -14,7 +14,13 @@ import {
     NavigationRouteConfig,
     NavigationTransitionProps,
 } from 'react-navigation'
+const createNativeStackNavigator = require('react-native-screens/createNativeStackNavigator')
+    .default
 import { ariaHidden } from 'src/helpers/a11y'
+import {
+    supportsTransparentCards,
+    supportsAnimation,
+} from 'src/helpers/features'
 import { safeInterpolation } from 'src/helpers/math'
 import { Breakpoints } from 'src/theme/breakpoints'
 import { color } from 'src/theme/color'
@@ -25,7 +31,6 @@ import {
 } from '../helpers/transition'
 import { sidebarWidth } from './sidebar/positions'
 import { screenInterpolator, mainLayerTransition } from './sidebar/transition'
-import { supportsTransparentCards } from 'src/helpers/features'
 
 const USE_SIDEBAR_ANIMATION =
     supportsTransparentCards() ||
@@ -133,9 +138,25 @@ export const createSidebarNavigator = (
             : mainRoute,
     }
     for (const [key, value] of Object.entries(sidebarRoute)) {
-        navigation[key] = USE_SIDEBAR_ANIMATION
-            ? addViewsForSidebarLayer(value, () => animatedValue)
-            : value
+        if (!supportsAnimation()) {
+            navigation[key] = value
+        } else {
+            navigation[key] = USE_SIDEBAR_ANIMATION
+                ? addViewsForSidebarLayer(value, () => animatedValue)
+                : value
+        }
+    }
+
+    // -iOS12 only use Native navigator
+    if (!supportsAnimation()) {
+        return createNativeStackNavigator(navigation, {
+            initialRouteName: '_',
+            defaultNavigationOptions: {
+                gesturesEnabled: false,
+            },
+            headerMode: 'none',
+            mode: 'card',
+        })
     }
 
     const transitionConfig = (transitionProps: NavigationTransitionProps) => {
