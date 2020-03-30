54f40c9a213ecdf3c855c8081855cd8e211da021
James Miller <james.miller.freelancer@guardian.co.uk>
2020-03-30 17:20:03 +0100

    feat: Reset the front scroll when the edition changes (#1104)
    * feat: Reset the front scroll when the edition changes
    
    * chore: validation
    
    * chore: validation

--- projects/Mallard/src/components/front/index.tsx
+++ projects/Mallard/src/components/front/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef, useState } from 'react'
+import React, { useMemo, useRef, useState, useEffect } from 'react'
 import { Animated, StyleSheet, View } from 'react-native'
 import {
     ArticlePillar,
@@ -117,6 +117,16 @@ export const Front = React.memo(
             Animated.AnimatedInterpolation
         >(new Animated.Value(0))
 
+        // Whenever we change edition, this resets us back to 0 scroll index for all fronts
+        useEffect(() => {
+            flatListRef &&
+                flatListRef.current &&
+                flatListRef.current._component.scrollToIndex({
+                    animated: false,
+                    index: 0,
+                })
+        }, [frontData])
+
         return (
             <FrontWrapper
                 scrubber={
@@ -148,7 +158,7 @@ export const Front = React.memo(
                     pagingEnabled={true}
                     decelerationRate="fast"
                     snapToInterval={card.width}
-                    ref={flatListRef}
+                    ref={(r: AnimatedFlatListRef) => (flatListRef.current = r)}
                     getItemLayout={(_: never, index: number) => ({
                         length: card.width,
                         offset: card.width * index,
