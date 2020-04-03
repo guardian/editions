246b572533cc56d956df1ac2e0f0866e3c6c7d2c
James Miller <james.miller.freelancer@guardian.co.uk>
2020-04-03 16:32:39 +0100

    chore: more cleanup after bad merge

--- projects/Mallard/src/components/spinner.stories.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react-native'
-import { Spinner } from './Spinner'
-
-storiesOf('Spinner', module).add('Standard Spinner', () => <Spinner />)
