6af2eafbd44df9d3b4cdb03a68d8e8ab8e03685b
James Miller <james.miller.freelancer@guardian.co.uk>
2020-04-03 09:13:28 +0100

    chore: Storybook work in progress

--- projects/Mallard/src/components/front/image-resource.tsx
+++ projects/Mallard/src/components/front/image-resource.tsx
@@ -10,7 +10,6 @@ import {
 import { useAspectRatio } from 'src/hooks/use-aspect-ratio'
 import { useImagePath, useScaledImage } from 'src/hooks/use-image-paths'
 import { Image as IImage, ImageUse } from '../../../../Apps/common/src'
-import DeviceInfo from 'react-native-device-info'
 
 /**
  * This component abstracts away the endpoint for images
@@ -61,7 +60,7 @@ const ImageResource = ({
     const aspectRatio = useAspectRatio(imagePath)
     const styles = [style, setAspectRatio && aspectRatio ? { aspectRatio } : {}]
 
-    if (DeviceInfo.isEmulatorSync()) {
+    if (__DEV__) {
         return (
             <Image
                 resizeMethod={'resize'}
