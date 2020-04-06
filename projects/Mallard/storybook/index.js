60ab193addbadb388cc52e9049e38cd24348f71f
James Miller <james.miller.freelancer@guardian.co.uk>
2020-04-06 15:54:25 +0100

    feat: Add knobs to storybook

--- projects/Mallard/storybook/index.js
+++ projects/Mallard/storybook/index.js
@@ -1,6 +1,8 @@
 import { getStorybookUI, configure } from '@storybook/react-native'
 import { loadStories } from './storyLoader'
 
+import './rn-addons'
+
 configure(() => {
     loadStories()
 }, module)
