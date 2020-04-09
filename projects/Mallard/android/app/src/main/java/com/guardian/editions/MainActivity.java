acab14300a556b42e286965a105cdac6edf6650f
Mohammad Haque <mohammad.haque@guardian.co.uk>
2020-04-09 10:40:12 +0100

    enabled rotation for tablet devices (#1117)

--- projects/Mallard/android/app/src/main/java/com/guardian/editions/MainActivity.java
+++ projects/Mallard/android/app/src/main/java/com/guardian/editions/MainActivity.java
@@ -1,19 +1,33 @@
 package com.guardian.editions;
 
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import com.facebook.react.ReactFragmentActivity;
+
 import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactFragmentActivity;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactFragmentActivity {
 
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this);
         super.onCreate(null);
+
+        if(!isTablet()) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
     }
+
+    private boolean isTablet() {
+        return getResources().getBoolean(R.bool.is_tablet);
+    }
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -23,13 +37,13 @@ public class MainActivity extends ReactFragmentActivity {
         return "Mallard";
     }
 
- @Override
- protected ReactActivityDelegate createReactActivityDelegate() {
-   return new ReactActivityDelegate(this, getMainComponentName()) {
-     @Override
-     protected ReactRootView createRootView() {
-      return new RNGestureHandlerEnabledRootView(MainActivity.this);
-     }
-   };
- }
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
 }
