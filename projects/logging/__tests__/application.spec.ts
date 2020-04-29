32f970a7234f03404b799883541008247db37bca
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-04-29 17:26:24 +0100

    Add dummy test for logging service.

--- projects/logging/__tests__/application.spec.ts
+++ projects/logging/__tests__/application.spec.ts
@@ -1,24 +1,18 @@
-// import { Request, Response } from 'express'
-// import { EditionsBackendControllers, createApp } from '../application'
-// import chai from 'chai'
-// import chaiHttp from 'chai-http'
+import { createApp } from '../application'
+import chai from 'chai'
+import chaiHttp from 'chai-http'
 
-// chai.use(chaiHttp)
-// chai.should()
+chai.use(chaiHttp)
+chai.should()
 
-// const stub = (req: Request, res: Response) => {
-//     console.log(`endpoint: ${req.path} called`)
-//     res.sendStatus(200)
-// }
-
-// describe('log endpoint returns 200', () => {
-//     const previewApp = createApp(testStubControllers, true)
-//     it('should return 200 for valid post requests', done => {
-//         chai.request(previewApp)
-//             .post('/log')
-//             .end((err, res) => {
-//                 expect(res.status).toBe(200)
-//                 done()
-//             })
-//     })
-// })
+describe('logging service', () => {
+    const testApp = createApp()
+    it('should return 200 from healthcheck', done => {
+        chai.request(testApp)
+            .get('/healthcheck')
+            .end((err, res) => {
+                expect(res.status).toBe(200)
+                done()
+            })
+    })
+})
