98bc49524d2a3403befa0b3f0308a9eb46f88269
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-04-30 10:51:53 +0100

    Sanitise data before logging

--- projects/logging/application.ts
+++ projects/logging/application.ts
@@ -1,11 +1,26 @@
 import express = require('express')
 import { Request, Response } from 'express'
 
+interface LogStashFormat {
+    '@timestamp': string
+    level: string
+    message: string
+    extraFields?: Map<string, any>
+}
+
+const prepareForLogStash = (logData: any): LogStashFormat => {
+    return {
+        '@timestamp': logData.timestamp || Date.now(),
+        level: logData.level || 'INFO',
+        message: logData.message || '',
+    }
+}
+
 export const createApp = (): express.Application => {
     const app: express.Application = express()
 
     app.use((req, res, next) => {
-        console.log(req.url)
+        // console.log(req.url) // uncomment this to enable request path logging
         next()
     })
 
@@ -15,8 +30,11 @@ export const createApp = (): express.Application => {
     })
 
     app.post('/log', express.json(), (req: Request, res: Response) => {
-        console.log('This is the log endpoint')
-        console.log(req.body)
+        const logData = prepareForLogStash(req.body)
+        console.log({
+            ...logData,
+            ...logData.extraFields,
+        })
         res.send('this is the log endpoint')
     })
 
