4adc954e1d5789654241e3fe2e1a975db2157043
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-05-01 16:48:38 +0100

    Support array of log data.

--- projects/logging/application.ts
+++ projects/logging/application.ts
@@ -17,6 +17,17 @@ const prepareForLogStash = (logData: any): LogStashFormat => {
     }
 }
 
+const processLog = (rawData: any[]) => {
+    rawData.forEach(rd => {
+        const logData = prepareForLogStash(rd)
+        logger.info({
+            ...logData,
+            // TODO: this is unsafe, extraFields should have a type imported from common lib shared with Mallard
+            ...rd.extraFields,
+        })
+    })
+}
+
 export const createApp = (): express.Application => {
     const app: express.Application = express()
 
@@ -28,14 +39,16 @@ export const createApp = (): express.Application => {
     app.post('/log', express.json(), (req: Request, res: Response) => {
         if (req.body.apiKey !== process.env.API_KEY) {
             res.status(403).send('Missing or invalid apiKey')
-        } else {
-            const logData = prepareForLogStash(req.body)
-            logger.info({
-                ...logData,
-                // TODO: this is unsafe, extraFields should have a type imported from common lib shared with Mallard
-                ...req.body.extraFields,
-            })
+        } else if (req.body.logData) {
+            const data = Array.isArray(req.body.logData)
+                ? req.body.logData
+                : [req.body.logData]
+            processLog(data)
             res.send('Log success')
+        } else {
+            res.status(400).send(
+                'apiKey or logData fields missing in request body',
+            )
         }
     })
 
