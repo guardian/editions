fdb6cb36afbfa2db27240de1b88907ad78793ed0
James Miller <james.miller.freelancer@guardian.co.uk>
2020-05-07 11:11:42 +0100

    chore: Update with Prod endpoint

--- projects/Mallard/src/helpers/settings/defaults.ts
+++ projects/Mallard/src/helpers/settings/defaults.ts
@@ -140,8 +140,7 @@ export const defaultSettings: Settings = {
     maxAvailableEditions: 7,
     logging: __DEV__
         ? 'https://editions-logging.code.dev-guardianapis.com/log/mallard'
-        : // WAITING FOR PROD ENDPOINT
-          'https://editions-logging.code.dev-guardianapis.com/log/mallard',
+        : 'https://editions-logging.guardianapis.com/log/mallard',
 }
 
 export const isPreview = (apiUrl: Settings['apiUrl']): boolean => {
