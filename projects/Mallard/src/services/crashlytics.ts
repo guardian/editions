2f96b06faa5fe4b5889481855acf0831fb7ae4eb
Mohammad Haque <mohammad.haque@guardian.co.uk>
2020-05-12 12:23:20 +0100

    Crashlytics is off by default

--- projects/Mallard/src/services/crashlytics.ts
+++ projects/Mallard/src/services/crashlytics.ts
@@ -2,14 +2,25 @@ import { Feature, loggingService, Level } from 'src/services/logging'
 import crashlytics from '@react-native-firebase/crashlytics'
 
 export const sendCrashlyticsAttributes = async () => {
+    enableCrashlytics()
+
     const data = await loggingService.basicLogInfo({
         level: Level.INFO,
         message: 'App Data',
     })
 
+    // send some sample key/value paires to crashlytics
     crashlytics().setAttributes({
         app: data.app,
         signedIn: String(data.signedIn),
         userId: data.userId,
     })
+}
+
+const enableCrashlytics = async () => {
+    // TODO This need to reflect user consent
+    if (__DEV__) {
+        await crashlytics().setCrashlyticsCollectionEnabled(true)
+        crashlytics().log('Crashlytics initialized')
+    }
 }
