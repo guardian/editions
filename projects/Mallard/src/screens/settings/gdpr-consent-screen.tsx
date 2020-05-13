caeeb84bb8389d9f6d1dbbc0e726b25d397609d4
James Miller <james.miller.freelancer@guardian.co.uk>
2020-05-13 10:01:19 +0100

    chore: Consent Wording Change (#1167)

--- projects/Mallard/src/screens/settings/gdpr-consent-screen.tsx
+++ projects/Mallard/src/screens/settings/gdpr-consent-screen.tsx
@@ -113,7 +113,7 @@ const GdprConsent = ({
             name: 'Performance',
             services: 'Sentry - Logging',
             description:
-                'Enabling these allow us to observe and measure how you use our services. We use this information to fix bugs more quickly so that users have a better experience. For example, we would be able to see the journey you have taken and where the error was encountered. Your data will only be stored in our servers for two weeks. If you disable this, we will not be able to observe and measure your use of our services, and we will have less information about their performance and event details of issues encountered.',
+                'Enabling these allow us to observe and measure how you use our services. We use this information to fix bugs more quickly so that users have a better experience. For example, we would be able to see the journey you have taken and where the error was encountered. Your data will only be stored in our servers for two weeks. If you disable this, we will not be able to observe and measure your use of our services, and we will have less information about their performance and details of any issues encountered.',
         },
         gdprAllowFunctionality: {
             key: gdprAllowFunctionalityKey,
