dc365764a66deaa48cf940d2bdf33cdd1a5a2cc9
Aoife McLaughlin <aoife.mclaughlin@guardian.co.uk>
2020-05-19 16:07:49 +0100

    revert lightbox storybook work

--- projects/Mallard/storybook/storyLoader.js
+++ projects/Mallard/storybook/storyLoader.js
@@ -4,20 +4,20 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-	require('../src/components/Lightbox/Lightbox.stories');
-	require('../src/components/Spinner/Spinner.stories');
-	require('../src/components/SportScore/SportScore.stories');
-	require('../src/components/Stars/Stars.stories');
+    require('../src/components/Lightbox/LightboxCaption.stories')
+    require('../src/components/Spinner/Spinner.stories')
+    require('../src/components/SportScore/SportScore.stories')
+    require('../src/components/Stars/Stars.stories')
 }
 
 const stories = [
-	'../src/components/Lightbox/Lightbox.stories',
-	'../src/components/Spinner/Spinner.stories',
-	'../src/components/SportScore/SportScore.stories',
-	'../src/components/Stars/Stars.stories'
-];
+    '../src/components/Lightbox/LightboxCaption.stories',
+    '../src/components/Spinner/Spinner.stories',
+    '../src/components/SportScore/SportScore.stories',
+    '../src/components/Stars/Stars.stories',
+]
 
 module.exports = {
-  loadStories,
-  stories,
-};
+    loadStories,
+    stories,
+}
