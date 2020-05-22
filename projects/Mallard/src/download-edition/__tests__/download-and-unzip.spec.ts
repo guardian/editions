8a393416c92ef61f97bc66be306ea5e32a28456c
James Miller <james.miller.freelancer@guardian.co.uk>
2020-05-22 15:04:32 +0100

    chore: Moved test

--- projects/Mallard/src/download-edition/__tests__/download-and-unzip.spec.ts
+++ projects/Mallard/src/download-edition/__tests__/download-and-unzip.spec.ts
@@ -22,20 +22,6 @@ const apolloClientMock: ApolloClient<object> = {
 } as any
 
 describe('download', () => {
-    // describe('matchSummmaryToKey', () => {
-    //     it('should return a matched IssueSummary if the key matches', () => {
-    //         const key = 'daily-edition/2019-09-18'
-    //         const isValidIssueSummary = matchSummmaryToKey(issueSummaries, key)
-    //         expect(isValidIssueSummary).toEqual(issueSummaries[0])
-    //     })
-
-    //     it('should return null if the key doesnt match', () => {
-    //         const key = 'daily-edition/2019-09-20'
-    //         const isValidIssueSummary = matchSummmaryToKey(issueSummaries, key)
-    //         expect(isValidIssueSummary).toEqual(null)
-    //     })
-    // })
-
     describe('downloadAndUnzipIssue', () => {
         it('should resolve the outer promise when the download runner resolves', async () => {
             const localId = '1'
@@ -92,28 +78,4 @@ describe('download', () => {
             await p3
         })
     })
-
-    // describe('issuesToDelete', () => {
-    //     MockDate.set('2019-08-21')
-    //     it('should return items outside of the 7 days that dont follow the issue naming, or the issue index', async () => {
-    //         const files = [
-    //             'daily-edition/issues',
-    //             'some-random-file',
-    //             'daily-edition/2019-08-15',
-    //             'daily-edition/2019-08-14',
-    //         ]
-    //         expect(await issuesToDelete(files)).toEqual([
-    //             'some-random-file',
-    //             'daily-edition/2019-08-14',
-    //         ])
-    //     })
-
-    //     it("should return an empty array if there isn't any to delete", async () => {
-    //         const files = [
-    //             'daily-edition/2019-08-15',
-    //             'daily-edition/2019-08-16',
-    //         ]
-    //         expect(await issuesToDelete(files)).toEqual([])
-    //     })
-    // })
 })
