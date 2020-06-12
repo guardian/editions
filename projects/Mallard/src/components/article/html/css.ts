8fe3918dd5572ddb22b56b33d14d019d08325222
Aoife McLaughlin <aoife.mclaughlin@guardian.co.uk>
2020-06-12 09:36:49 +0100

    add header padding

--- projects/Mallard/src/components/article/html/css.ts
+++ projects/Mallard/src/components/article/html/css.ts
@@ -156,6 +156,8 @@ const makeCss = ({ colors, theme }: CssProps, contentType: string) => css`
         margin: auto;
         position: relative;
         padding-top: ${px(metrics.vertical)};
+        padding-left:  ${px(metrics.vertical)};
+        padding-right:  ${px(metrics.vertical)};
     }
     .content-wrap .line {
         margin-right: ${px(metrics.article.sides * -1)};
