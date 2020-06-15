7cb0b68a4628987e290e4945434ee94cf8644801
James Miller <james.miller.freelancer@guardian.co.uk>
2020-06-15 12:10:57 +0100

    feat: Editions Menu Header (#1264)
    * feat: Editions Menu Header
    
    * chore: linting

--- projects/Mallard/storybook/storyLoader.js
+++ projects/Mallard/storybook/storyLoader.js
@@ -5,6 +5,7 @@
 
 function loadStories() {
 	require('../src/components/Button/Button.stories');
+	require('../src/components/EditionsMenu/Header/Header.stories');
 	require('../src/components/EditionsMenu/RegionButton/RegionButton.stories');
 	require('../src/components/Lightbox/LightboxCaption.stories');
 	require('../src/components/SignInFailedModalCard.stories');
@@ -16,6 +17,7 @@ function loadStories() {
 
 const stories = [
 	'../src/components/Button/Button.stories',
+	'../src/components/EditionsMenu/Header/Header.stories',
 	'../src/components/EditionsMenu/RegionButton/RegionButton.stories',
 	'../src/components/Lightbox/LightboxCaption.stories',
 	'../src/components/SignInFailedModalCard.stories',
