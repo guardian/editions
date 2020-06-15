13b6b32f737ec1b054248817254680589c695085
James Miller <james.miller.freelancer@guardian.co.uk>
2020-06-15 10:22:58 +0100

    chore: linting

--- projects/Mallard/src/components/EditionsMenu/Header/Header.stories.tsx
+++ projects/Mallard/src/components/EditionsMenu/Header/Header.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react-native'
-import { withKnobs, color, text } from '@storybook/addon-knobs'
+import { withKnobs, text } from '@storybook/addon-knobs'
 import { EditionsMenuHeader } from './Header'
 
 storiesOf('EditionsMenuHeader', module)
