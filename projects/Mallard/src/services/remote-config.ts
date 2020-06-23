ad84a93b6bfa70d3964cbd6ba7cee4c91a48384d
Mohammad Haque <mohammad.haque@guardian.co.uk>
2020-06-23 18:28:16 +0100

    WIP: firebase with multiple apps

--- projects/Mallard/src/services/remote-config.ts
+++ projects/Mallard/src/services/remote-config.ts
@@ -1,4 +1,9 @@
-import remoteConfig from '@react-native-firebase/remote-config'
+import remoteConfig, {
+    firebase,
+    FirebaseRemoteConfigTypes,
+} from '@react-native-firebase/remote-config'
+import { isInBeta } from 'src/helpers/release-stream'
+import { Platform } from 'react-native'
 
 // see https://rnfirebase.io/remote-config/usage for docs
 
@@ -25,9 +30,13 @@ const configValues = {
 
 class RemoteConfigService implements RemoteConfig {
     private initialized: boolean
+    private rmConfig: FirebaseRemoteConfigTypes.Module
 
     constructor() {
         this.initialized = false
+        this.rmConfig = isInBeta()
+            ? this.createSecondaryFbApp().remoteConfig()
+            : remoteConfig()
     }
 
     private setInitialized(isInitialized: boolean) {
@@ -35,14 +44,16 @@ class RemoteConfigService implements RemoteConfig {
     }
 
     init() {
-        remoteConfig()
+        console.log('Initializing Firebase Secondary app already created')
+
+        this.rmConfig
             .setDefaults(remoteConfigDefaults)
-            .then(() => remoteConfig().setConfigSettings(configValues))
+            .then(() => this.rmConfig.setConfigSettings(configValues))
             .then(() => {
-                remoteConfig().fetchAndActivate()
+                this.rmConfig.fetchAndActivate()
                 this.setInitialized(true)
                 console.log('Remote config fetched & activated!')
-                if (__DEV__) console.log(remoteConfig().getAll())
+                if (__DEV__) console.log(this.rmConfig.getAll())
             })
             .catch(() => {
                 this.setInitialized(false)
@@ -52,9 +63,36 @@ class RemoteConfigService implements RemoteConfig {
             })
     }
 
+    private async createSecondaryFbApp() {
+        // Secondary Firebase project credentials from https://console.firebase.google.com/project/guardian-daily-edition-code-2/settings/general/android:com.guardian.editions
+        const credentials = {
+            clientId: Platform.select({
+                ios:
+                    '385815722272-ov3324su0gb6nqrsu9f5o5gqortuh5th.apps.googleusercontent.com',
+                android: '',
+            }),
+            appId: Platform.select({
+                ios: '1:385815722272:ios:4a237b6066a85ba4f2bf45',
+                android: '1:385815722272:android:5aefffd4abea8974f2bf45',
+            }),
+            apiKey: Platform.select({
+                ios: 'AIzaSyD3qpzIH-qiNSgHb23wJAyH758gYvboJIQ',
+                android: 'AIzaSyDwnpZbUyNh7dtTTyM8IN0m_NH3P41LEs8',
+            }),
+            databaseURL: 'https://guardian-daily-edition-code-2.firebaseio.com',
+            storageBucket: '',
+            messagingSenderId: '',
+            projectId: 'guardian-daily-edition-code-2',
+        }
+
+        const app = await firebase.initializeApp(credentials, 'SECONDARY_APP')
+        console.log('NEW Firebase app created: ', app.remoteConfig)
+        return app
+    }
+
     getBoolean(key: string): boolean {
         if (this.initialized) {
-            return remoteConfig().getValue(key).value as boolean
+            return this.rmConfig.getValue(key).value as boolean
         }
 
         return false
@@ -62,7 +100,7 @@ class RemoteConfigService implements RemoteConfig {
 
     getString(key: string): RemoteStringValue {
         if (this.initialized) {
-            return remoteConfig().getValue(key).value as string
+            return this.rmConfig.getValue(key).value as string
         }
 
         return undefined
