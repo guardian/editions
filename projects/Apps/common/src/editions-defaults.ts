e927f1093c07883f3f2ca1b68aee914e7114f61b
James Miller <james.miller.freelancer@guardian.co.uk>
2020-06-26 09:19:46 +0100

    chore: Add Linebreak to Region Buttons

--- projects/Apps/common/src/editions-defaults.ts
+++ projects/Apps/common/src/editions-defaults.ts
@@ -3,17 +3,20 @@ import { RegionalEdition, editions } from './index'
 export const defaultRegionalEditions: RegionalEdition[] = [
     {
         title: 'The Daily',
-        subTitle: 'Published every day by 6am (GMT)',
+        subTitle: `Published every morning
+by 6am (GMT)`,
         edition: editions.daily,
     },
     {
         title: 'Australia Weekend',
-        subTitle: 'Published every Saturday by 6am (AEST)',
+        subTitle: `Published every Saturday morning 
+by 6am (AEST)`,
         edition: editions.ausWeekly,
     },
     {
         title: 'US Weekend',
-        subTitle: 'Published every Saturday by 6am (EST)',
+        subTitle: `Published every Saturday morning 
+by 6am (EST)`,
         edition: editions.usWeekly,
     },
 ]
