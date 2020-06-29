5e17feae06c7a8b39e3e8458a140f1d6393b3234
Justin Rowles <justin.rowles.freelance@guardian.co.uk>
2020-06-29 17:54:58 +0100

    Handle failed attempt to get issue

--- projects/archiver/src/tasks/copier/index.ts
+++ projects/archiver/src/tasks/copier/index.ts
@@ -3,7 +3,9 @@ import { handleAndNotify } from '../../services/task-handler'
 import { IssueTaskOutput } from '../issue'
 import { IndexTaskOutput } from '../indexer'
 import { getBucket, recursiveCopy } from '../../utils/s3'
-import { hasFailed } from '../../../../backend/utils/try'
+import { attempt, hasFailed } from '../../../../backend/utils/try'
+import { getPublishedId } from '../../utils/path-builder'
+import { getIssue } from '../../utils/backend-client'
 
 type CopyTaskInput = IndexTaskOutput
 export type CopyTaskOutput = Pick<
@@ -34,8 +36,14 @@ export const handler: Handler<CopyTaskInput, CopyTaskOutput> = handleAndNotify(
         if (zipCopyPromises.filter(hasFailed).length)
             throw new Error('Failed to copy some zips')
 
+        const publishedId = getPublishedId(issuePublication)
+        const issue = await attempt(getIssue(publishedId))
+        if (hasFailed(issue))
+            throw new Error('Failed to find issue after copying')
+
         return {
             issuePublication,
+            issue,
             message: 'Issue copied successfully',
         }
     },
