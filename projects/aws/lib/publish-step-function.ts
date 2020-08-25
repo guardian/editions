c5d9be70bb23d335dddebfdb751afa13dd8f0fd8
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-08-25 17:07:44 +0100

    Remove editions list step now that it is replaced by POST endpoint. (#1457)

--- projects/aws/lib/publish-step-function.ts
+++ projects/aws/lib/publish-step-function.ts
@@ -63,21 +63,11 @@ export const publishArchiverStepFunction = (
         environment,
     )
 
-    const editionsList = publishTask(
-        scope,
-        'editionsList',
-        'Generate editions list',
-        lambdaParams,
-        environment,
-    )
-
     copier.task.next(indexerPublish.task)
 
     indexerPublish.task.next(notification.task)
 
-    notification.task.next(editionsList.task)
-
-    editionsList.task.next(
+    notification.task.next(
         new sfn.Succeed(scope, 'publish-successfully-archived'),
     )
 
