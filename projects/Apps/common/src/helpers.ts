f369ce414305337ad8a4e6795aad9a071f99a6ec
Aoife McLaughlin <aoife.mclaughlin@guardian.co.uk>
2020-10-05 12:49:12 +0100

    fallback to default regional editions

--- projects/Apps/common/src/helpers.ts
+++ projects/Apps/common/src/helpers.ts
@@ -1,4 +1,5 @@
 import { EditionId, EditionsList, IssueSummary } from '.'
+import { defaultRegionalEditions } from './editions-defaults'
 
 export const issueSummaryComparator = (a: IssueSummary, b: IssueSummary) => {
     return a.date.localeCompare(b.date)
@@ -8,7 +9,9 @@ export const issueSummarySort = (issues: IssueSummary[]): IssueSummary[] => {
     return issues.sort(issueSummaryComparator).reverse()
 }
 
-export const getEditionIds = (editionList: EditionsList): EditionId[] =>
-    editionList.regionalEditions
-        .map(e => e.edition)
-        .concat(editionList.specialEditions.map(e => e.edition))
+export const getEditionIds = (editionList: EditionsList | null): EditionId[] =>
+    editionList
+        ? editionList.regionalEditions
+              .map(e => e.edition)
+              .concat(editionList.specialEditions.map(e => e.edition))
+        : defaultRegionalEditions.map(e => e.edition)
