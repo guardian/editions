78e69c7a5008a61625bf5327be74f85288affe64
Aoife McLaughlin <aoife.mclaughlin@guardian.co.uk>
2020-10-12 10:00:48 +0100

    use locale for date formatting and moment library for android

--- projects/Mallard/src/helpers/date.ts
+++ projects/Mallard/src/helpers/date.ts
@@ -1,12 +1,18 @@
 import moment from 'moment-timezone'
-import * as RNLocalize from 'react-native-localize'
+import { languageLocale } from './locale'
+import { Platform } from 'react-native'
 
 const londonTime = (time?: string | number) => {
     if (time != null) return moment.tz(time, 'Europe/London')
     return moment.tz('Europe/London')
 }
 
-const localDate = (date: Date): string =>
-    date.toLocaleDateString(RNLocalize.getLocales()[0].languageTag)
+const localDate = (date: Date): string => {
+    if (Platform.OS === 'ios') {
+        return date.toLocaleDateString(languageLocale)
+    } else {
+        return languageLocale === "en-US" ? moment(date).format('MM/DD/YYYY') : moment(date).format('DD/MM/YYYY')
+    }
+}
 
 export { londonTime, localDate }
