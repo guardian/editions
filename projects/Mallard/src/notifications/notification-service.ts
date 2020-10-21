2808860336ae5cb2b48f2b3c632ace155ed8f58b
Mohammad Haque <mohammad.haque@guardian.co.uk>
2020-10-21 15:04:48 +0100

    added more logging

--- projects/Mallard/src/notifications/notification-service.ts
+++ projects/Mallard/src/notifications/notification-service.ts
@@ -2,6 +2,8 @@ import { Platform } from 'react-native'
 import { getSetting } from 'src/helpers/settings'
 import { notificationEdition } from 'src/helpers/settings/defaults'
 import { errorService } from 'src/services/errors'
+import { isInBeta } from 'src/helpers/release-stream'
+import { getDefaultEdition } from 'src/hooks/use-edition-provider'
 
 export interface PushToken {
     name: 'uk' | 'us' | 'au'
@@ -22,7 +24,12 @@ const registerWithNotificationService = async (
         topics,
     }
 
-    console.log('*** NOTIFICATION REG PAYLOAD *** ' + JSON.stringify(options))
+    if (isInBeta()) {
+        const defaultEdition = await getDefaultEdition()
+        console.log('*** NOTIFICATION REG PAYLOAD *** ')
+        console.log('*** default edition *** ' + defaultEdition)
+        console.log('*** body *** ' + JSON.stringify(options))
+    }
 
     return fetch(registerDeviceUrl as string, {
         method: 'post',
