de07bee289fba642ba45fcead29069db678860d8
Mohammad Haque <mohammad.haque@guardian.co.uk>
2020-10-23 11:18:25 +0100

    Fixed the annotations imports

--- projects/Mallard/android/app/src/main/java/com/guardian/editions/ophan/InstallationIdHelper.java
+++ projects/Mallard/android/app/src/main/java/com/guardian/editions/ophan/InstallationIdHelper.java
@@ -1,9 +1,9 @@
 package com.guardian.editions.ophan;
 
 import android.content.Context;
+import android.util.Log;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import androidx.annotation.NonNull;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -11,13 +11,15 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.UUID;
 
+import javax.annotation.Nullable;
+
 public class InstallationIdHelper {
 
     @Nullable
     private String sID;
     private static final String INSTALLATION_FILE_NAME = "INSTALLATION";
 
-    public InstallationIdHelper(@NonNull Context context){
+    public InstallationIdHelper(@NonNull Context context) {
         sID = readId(context);
     }
 
@@ -37,7 +39,6 @@ public class InstallationIdHelper {
         return sID;
     }
 
-    @NotNull
     public synchronized String id() {
         if (sID == null) {
             throw new IllegalStateException("Installation.id has not been initialised");
