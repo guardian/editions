69e3ef2b23325486c6e8a545a629b332287f274e
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-10-28 17:10:26 +0000

    Include accept header in apps rendering request.

--- projects/backend/controllers/render.ts
+++ projects/backend/controllers/render.ts
@@ -4,8 +4,19 @@ import fetch from 'node-fetch'
 export const renderController = async (req: Request, res: Response) => {
     const path = req.params.path
     const renderingUrl = `${process.env.APPS_RENDERING_URL}${path}`
-    console.log(`fetching ${renderingUrl}`)
-    const response = await fetch(renderingUrl)
+    console.log(`Fetching ${renderingUrl} from apps rendering`)
+    const response = await fetch(renderingUrl, {
+        headers: {
+            Accept:
+                'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        },
+    })
     const text = await response.text()
-    res.send(text)
+    if (response.status === 200) {
+        res.send(text)
+    } else {
+        const message = `Failed to fetch story from ${renderingUrl}. Response: ${text}`
+        console.error(`${message}`)
+        res.status(response.status).send(message)
+    }
 }
