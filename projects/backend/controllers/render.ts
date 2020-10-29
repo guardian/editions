a474bc374787e3e4ab42cdd2f1e1d662fd0379c5
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-10-29 17:33:21 +0000

    Add image path replacement to render controller.

--- projects/backend/controllers/render.ts
+++ projects/backend/controllers/render.ts
@@ -1,21 +1,35 @@
 import { Request, Response } from 'express'
 import fetch from 'node-fetch'
 
+const replaceImageUrls = (html: string): string => {
+    return html.replace(/https:\/\/i.guim.co.uk\/img\//g, '../../')
+}
+
 export const renderController = async (req: Request, res: Response) => {
     const path = req.params.path
+    const replaceImagePaths = req.query.replaceImagePaths
     const renderingUrl = `${process.env.APPS_RENDERING_URL}${path}`
-    console.log(`Fetching ${renderingUrl} from apps rendering`)
+    console.log(
+        `Fetching ${renderingUrl} from apps rendering. replaceImagePaths: ${replaceImagePaths}`,
+    )
     const response = await fetch(renderingUrl, {
         headers: {
             Accept:
                 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         },
     })
-    const text = await response.text()
-    if (response.status === 200) {
-        res.send(text)
+    const responseBody = await response.text()
+
+    const editionsRenderedHtml =
+        replaceImagePaths === 'true'
+            ? replaceImageUrls(responseBody)
+            : responseBody
+
+    if (response.statusText == 'OK') {
+        res.setHeader('Content-Type', 'text/html')
+        res.send(editionsRenderedHtml)
     } else {
-        const message = `Failed to fetch story from ${renderingUrl}. Response: ${text}`
+        const message = `Failed to fetch story from ${renderingUrl}. Response: ${responseBody}`
         console.error(`${message}`)
         res.status(response.status).send(message)
     }
