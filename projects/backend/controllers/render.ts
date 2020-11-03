f63645d56da0c96cb9bc2cc1aaf13e2a7fd7239c
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-11-03 15:19:23 +0000

    Add functionality to return all image sizes of article.

--- projects/backend/controllers/render.ts
+++ projects/backend/controllers/render.ts
@@ -1,6 +1,21 @@
 import { Request, Response } from 'express'
 import fetch from 'node-fetch'
-import { ImageSize } from '../../Apps/common/src'
+import { ImageSize, imageSizes, RenderedContent } from '../../Apps/common/src'
+
+const fetchRenderedArticle = async (url: string) => {
+    const response = await fetch(url, {
+        headers: {
+            Accept:
+                'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        },
+    })
+    const responseBody = await response.text()
+    return {
+        success: response.statusText === 'OK',
+        status: response.status,
+        body: responseBody,
+    }
+}
 
 // TODO: this needs a test once we're happy with the correct format for the paths
 const replaceImageUrls = (html: string, imageSize: ImageSize): string => {
@@ -10,33 +25,48 @@ const replaceImageUrls = (html: string, imageSize: ImageSize): string => {
     )
 }
 
+const getAllImageSizesHtml = (renderedArticle: string): RenderedContent[] => {
+    return imageSizes.map(size => {
+        return {
+            size,
+            html: replaceImageUrls(renderedArticle, size),
+        }
+    })
+}
+
+const getRenderResponse = (renderedArticle: string, imageSize: string) => {
+    if (imageSize === 'all') {
+        return {
+            contentType: 'application/json',
+            responseBody: JSON.stringify(getAllImageSizesHtml(renderedArticle)),
+        }
+    } else {
+        const responseBody = (imageSizes as readonly string[]).includes(
+            imageSize,
+        )
+            ? replaceImageUrls(renderedArticle, imageSize as ImageSize)
+            : renderedArticle
+        return { contentType: 'text/html', responseBody }
+    }
+}
+
 export const renderController = async (req: Request, res: Response) => {
     const path = req.params.path
-    const replaceImagePaths = req.query.replaceImagePaths
-    const imageSize = req.query.imageSize as ImageSize
-    const renderingUrl = `${process.env.APPS_RENDERING_URL}${path}`
+    const imageSize = req.query.imageSize
+    const renderingUrl = `${process.env.APPS_RENDERING_URL}${path}?editions`
     console.log(
-        `Fetching ${renderingUrl} from apps rendering. replaceImagePaths: ${replaceImagePaths}`,
+        `Fetching ${renderingUrl} from apps rendering. imageSize: ${imageSize}`,
     )
-    const response = await fetch(renderingUrl, {
-        headers: {
-            Accept:
-                'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        },
-    })
-    const responseBody = await response.text()
+    const renderResponse = await fetchRenderedArticle(renderingUrl)
 
-    const editionsRenderedHtml =
-        replaceImagePaths === 'true'
-            ? replaceImageUrls(responseBody, imageSize)
-            : responseBody
+    const response = getRenderResponse(renderResponse.body, imageSize)
 
-    if (response.statusText == 'OK') {
-        res.setHeader('Content-Type', 'text/html')
-        res.send(editionsRenderedHtml)
+    if (renderResponse.success) {
+        res.setHeader('Content-Type', response.contentType)
+        res.send(response.responseBody)
     } else {
-        const message = `Failed to fetch story from ${renderingUrl}. Response: ${responseBody}`
+        const message = `Failed to fetch story from ${renderingUrl}. Response: ${renderResponse.body}`
         console.error(`${message}`)
-        res.status(response.status).send(message)
+        res.status(renderResponse.status).send(message)
     }
 }
