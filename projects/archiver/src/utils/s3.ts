5fac67f2602489d102bcf6a22925c0759f6bc0cb
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-11-23 11:58:20 +0000

    Use stage to determine credentials provider.

--- projects/archiver/src/utils/s3.ts
+++ projects/archiver/src/utils/s3.ts
@@ -37,7 +37,10 @@ const s3FrontendCredentials = new S3({
     credentials: new SharedIniFileCredentials({ profile: 'frontend' }),
 })
 
-export const s3 = process.env.arn ? s3DefaultCredentials : s3FrontendCredentials
+export const s3 =
+    process.env.stage === 'CODE' || process.env.stage === 'PROD'
+        ? s3DefaultCredentials
+        : s3FrontendCredentials
 
 export type Bucket = {
     name: string
