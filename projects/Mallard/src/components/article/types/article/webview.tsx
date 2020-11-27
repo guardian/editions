68c9559a7944111df728617b0fe71cc360c78dda
Mohammad Haque <mohammad.haque@guardian.co.uk>
2020-11-27 16:01:41 +0100

    added a fix for webview loading error

--- projects/Mallard/src/components/article/types/article/webview.tsx
+++ projects/Mallard/src/components/article/types/article/webview.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Platform } from 'react-native'
 import { WebView, WebViewProps } from 'react-native-webview'
 import {
@@ -26,6 +26,11 @@ const WebviewWithArticle = ({
 } & WebViewProps & { onScroll?: any }) => {
     const { localIssueId } = path
     const largeDeviceMemory = useLargeDeviceMemory()
+    const [isReady, setIsReady] = useState(false)
+
+    useEffect(() => {
+        setIsReady(true)
+    },[isReady])
 
     // Online rendering
     let uri = `${defaultSettings.appsRenderingService}${article.internalPageCode}?editions`
@@ -38,13 +43,17 @@ const WebviewWithArticle = ({
         uri = Platform.OS === 'android' ? 'file://' + htmlUri : htmlUri
     }
 
+    // set url only when component is ready
+    // https://github.com/react-native-webview/react-native-webview/issues/656#issuecomment-551312436
+    const finalUrl = isReady ? uri : ''
+
     return (
         <WebView
             {...webViewProps}
             bounces={largeDeviceMemory ? true : false}
             originWhitelist={['*']}
             scrollEnabled={true}
-            source={{ uri }}
+            source={{ uri: finalUrl }}
             ref={_ref}
             onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
             allowFileAccess={true}
