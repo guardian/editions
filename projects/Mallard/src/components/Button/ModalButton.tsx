b7f8838a2088a0cc40145a4b06e3c9d6e2dc88bd
Anna Beddow <anna.beddow@gmail.com>
2020-12-03 13:33:52 +0000

    Add props to update btn and txt style on modal btn

--- projects/Mallard/src/components/Button/ModalButton.tsx
+++ projects/Mallard/src/components/Button/ModalButton.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Button, ButtonAppearance } from './Button'
-import { StyleSheet } from 'react-native'
-
+import { StyleSheet, StyleProp, ViewStyle, TextStyle } from 'react-native'
 const styles = StyleSheet.create({
     button: {
         marginTop: 10,
@@ -13,12 +12,16 @@ const ModalButton = (props: {
     onPress: () => void
     children: string
     alt?: string
+    buttonStyles?: StyleProp<ViewStyle>
+    textStyles?: StyleProp<TextStyle>
     buttonAppearance?: ButtonAppearance
 }) => (
     <Button
         {...props}
         alt={props.alt || props.children}
         style={styles.button}
+        buttonStyles={props.buttonStyles}
+        textStyles={props.textStyles}
         appearance={props.buttonAppearance || ButtonAppearance.light}
     />
 )
