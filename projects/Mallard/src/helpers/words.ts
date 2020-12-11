a2191a0757fc37e83c7aacf7bca441083e1059ed
Rob Phillips <robert.phillips@guardian.co.uk>
2020-12-11 09:44:03 +0000

    Removing full stop for grammatical consistency

--- projects/Mallard/src/helpers/words.ts
+++ projects/Mallard/src/helpers/words.ts
@@ -168,7 +168,7 @@ export const ManageDownloads = {
 
 export const NewEditionWords = {
     title: `A new special edition is available`,
-    bodyText: `Tap on the edition icon above to access it.`,
+    bodyText: `Tap on the edition icon above to access it`,
     dismissButtonText: 'Got it',
 }
 
