0f66beddd1d8518e514489a9a241e3398dc9940b
Grahame Oakland <grahame.oakland@guardian.co.uk>
2021-01-21 21:09:18 +0000

    improve readability

--- projects/backend/controllers/appsRendering.ts
+++ projects/backend/controllers/appsRendering.ts
@@ -4,6 +4,7 @@ import fetch from 'node-fetch'
 export const appsRenderingController = async (req: Request, res: Response) => {
     const path = req.params.path
     const renderingUrl = `${process.env.APPS_RENDERING_URL}${path}?editions`
+    
     console.log(`Fetching HTML from AppsRendering URL: ${renderingUrl}`)
     const renderResponse = await fetch(renderingUrl, {
         headers: { Accept: 'text/html' },
