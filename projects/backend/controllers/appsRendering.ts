07a99d41e2dbe063f05bb35662a424f304cd1d9e
Grahame Oakland <grahame.oakland@guardian.co.uk>
2021-01-21 20:47:06 +0000

    pipe streams

--- projects/backend/controllers/appsRendering.ts
+++ projects/backend/controllers/appsRendering.ts
@@ -1,29 +1,19 @@
 import { Request, Response } from 'express'
 import fetch from 'node-fetch'
 
-const fetchRenderedArticle = async (url: string) => {
-    const response = await fetch(url, {
-        headers: {Accept: 'text/html',},
-    })
-    const responseBody = await response.text()
-    return {
-        success: response.ok,
-        status: response.status,
-        body: responseBody,
-    }
-}
-
 export const appsRenderingController = async (req: Request, res: Response) => {
     const path = req.params.path
     const renderingUrl = `${process.env.APPS_RENDERING_URL}${path}?editions`
     console.log(`Fetching HTML from AppsRendering URL: ${renderingUrl}`)
-    const renderResponse = await fetchRenderedArticle(renderingUrl)
+    const renderResponse = await fetch(renderingUrl, {
+        headers: { Accept: 'text/html' },
+    })
 
-    if (renderResponse.success) {
+    if (renderResponse.ok) {
         res.setHeader('Content-Type', 'text/html')
-        res.send(renderResponse.body)
+        renderResponse.body.pipe(res)
     } else {
-        const message = `Failed to fetch AppsRendered HTML from ${renderingUrl}. Response: ${renderResponse.body}`
+        const message = `Failed to fetch AppsRendered HTML from ${renderingUrl}. Response: ${renderResponse.statusText}`
         console.error(`${message}`)
         res.status(renderResponse.status).send(message)
     }
