79378a90c926cd49bc3bf67af68e1d6cde104e48
Mohammad Haque <mohammad.haque@guardian.co.uk>
2021-02-18 14:27:38 +0000

    fixed lint error

--- projects/backend/capi/encoders.ts
+++ projects/backend/capi/encoders.ts
@@ -3,8 +3,8 @@ import { TBufferedTransport, TCompactProtocol } from 'thrift'
 
 const encodeContent = async (data: Content): Promise<Buffer> => {
     try {
-        var buffer = Buffer.from([])
-        var transport = new TBufferedTransport(buffer, outBuffer => {
+        let buffer = Buffer.from([])
+        const transport = new TBufferedTransport(buffer, outBuffer => {
             if (outBuffer) {
                 buffer = Buffer.concat([buffer, outBuffer])
             } else {
@@ -12,7 +12,7 @@ const encodeContent = async (data: Content): Promise<Buffer> => {
             }
         })
 
-        var protocol = new TCompactProtocol(transport)
+        const protocol = new TCompactProtocol(transport)
         ContentSerde.write(protocol, data)
         protocol.flush()
         transport.flush()
