8cd030533473bba2654d28ecc45980299ff9cc85
Anna Beddow <anna.beddow@gmail.com>
2021-02-24 17:41:16 +0000

    Update title and subtitle in issue selector menu

--- projects/Mallard/src/screens/home-screen.tsx
+++ projects/Mallard/src/screens/home-screen.tsx
@@ -443,11 +443,16 @@ export const HomeScreen = () => {
     const { selectedEdition } = useEditions()
 
     const specialEditionProps = getSpecialEditionProps(selectedEdition)
+    const issueHeaderData =
+    selectedEdition.editionType === 'Special'
+        ? { title: '', subTitle: '' }
+        : { title: selectedEdition.title, subTitle: 'Recent Editions' }
+
     return (
         <WithAppAppearance value={'tertiary'}>
             <IssuePickerHeader
-                title={selectedEdition.header.title}
-                subTitle={selectedEdition.header.subTitle}
+                title={issueHeaderData.title}
+                subTitle={issueHeaderData.subTitle}
                 headerStyles={
                     specialEditionProps && specialEditionProps.headerStyle
                 }
