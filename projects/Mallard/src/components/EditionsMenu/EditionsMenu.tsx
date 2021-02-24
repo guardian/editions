52e16cb2afe4a167e53e3ec203c1b016dd92a0d7
Anna Beddow <anna.beddow@gmail.com>
2021-02-24 10:38:55 +0000

    Use button style background color for special title

--- projects/Mallard/src/components/EditionsMenu/EditionsMenu.tsx
+++ projects/Mallard/src/components/EditionsMenu/EditionsMenu.tsx
@@ -42,7 +42,6 @@ const EditionsMenu = ({
 
     const renderSpecialItem = ({ item }: { item: SpecialEdition }) => {
         const { buttonStyle, buttonImageUri, expiry, title, subTitle } = item
-
         const handlePress = () => {
             storeSelectedEdition(item)
             navigationPress()
@@ -63,7 +62,7 @@ const EditionsMenu = ({
             />
         )
     }
-
+ 
     return (
         <ScrollView
             style={{ paddingTop: 17, paddingHorizontal: metrics.horizontal }}
--- projects/Mallard/src/components/ScreenHeader/EditionMenuScreenHeader.tsx
+++ projects/Mallard/src/components/ScreenHeader/EditionMenuScreenHeader.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { Header } from '../layout/header/header'
 import { EditionsMenuButton } from 'src/components/EditionsMenu/EditionsMenuButton/EditionsMenuButton'
 import { IssueTitle } from 'src/components/issue/issue-title'
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    title: { paddingLeft: 110, alignSelf: 'flex-start' },
+})
 
 export const EditionsMenuScreenHeader = ({
     leftActionPress,
@@ -12,6 +17,6 @@ export const EditionsMenuScreenHeader = ({
         leftAction={<EditionsMenuButton selected onPress={leftActionPress} />}
         layout={'center'}
     >
-        <IssueTitle title={`Editions`} />
+        <IssueTitle title={`Editions`} style={styles.title} />
     </Header>
 )
