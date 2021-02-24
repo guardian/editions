91d2903a930620ea0b8e61722ebd1a7f30b89e84
Anna Beddow <anna.beddow@gmail.com>
2021-02-24 18:27:17 +0000

    Update Text styles in issue headers

--- projects/Mallard/src/components/ScreenHeader/IssueScreenHeader/IssueScreenHeader.tsx
+++ projects/Mallard/src/components/ScreenHeader/IssueScreenHeader/IssueScreenHeader.tsx
@@ -8,8 +8,17 @@ import {
 } from 'src/navigation/helpers/base'
 import { IssueMenuButton } from '../../Button/IssueMenuButton'
 import { EditionsMenuButton } from '../../EditionsMenu/EditionsMenuButton/EditionsMenuButton'
-import { ScreenHeader } from '../ScreenHeader'
 import { useEditions } from 'src/hooks/use-edition-provider'
+import { styles } from 'src/components/styled-text'
+import { IssueTitle } from 'src/components/issue/issue-title'
+import { Header } from 'src/components/layout/header/header'
+
+interface Titles {
+    title: string
+    subTitle: string
+    titleStyle: any
+}
+
 const IssueScreenHeader = withNavigation(
     ({
         headerStyles,
@@ -20,28 +29,63 @@ const IssueScreenHeader = withNavigation(
         issue?: IssueWithFronts
     } & NavigationInjectedProps) => {
         const { date, weekday } = useIssueDate(issue)
+        const { setNewEditionSeen, selectedEdition } = useEditions()
+
+        const getDateString = () => {
+            const abbreviatedDay = weekday.substring(0, 3)
+            return `${abbreviatedDay} ${date}`
+        }
 
         const goToIssueList = () => {
             navigateToIssueList(navigation)
         }
-        const { setNewEditionSeen } = useEditions()
 
         const handleEditionMenuPress = () => {
             setNewEditionSeen()
             navigateToEditionMenu(navigation)
         }
 
+        const isSpecialEdition = (editionType: string) => {
+            return editionType === 'Special'
+        }
+
+        const getTitles = (): Titles => {
+            if (isSpecialEdition(selectedEdition.editionType)) {
+                const splitTitle = selectedEdition.title.split('\n')
+                return {
+                    title: splitTitle[0],
+                    subTitle: splitTitle[1],
+                    titleStyle: styles.issueHeavyText,
+                }
+            }
+            const dateString = getDateString()
+            return {
+                title: selectedEdition.title,
+                subTitle: dateString,
+                titleStyle: styles.issueLightText,
+            }
+        }
+
+        const { title, subTitle, titleStyle } = getTitles()
+
         return (
-            <ScreenHeader
-                title={weekday}
-                subTitle={date}
+            <Header
                 onPress={goToIssueList}
-                rightAction={<IssueMenuButton onPress={goToIssueList} />}
+                action={<IssueMenuButton onPress={goToIssueList} />}
                 leftAction={
                     <EditionsMenuButton onPress={handleEditionMenuPress} />
                 }
                 headerStyles={headerStyles}
-            />
+            >
+                {title ? (
+                    <IssueTitle
+                        title={title}
+                        subtitle={subTitle}
+                        titleStyle={titleStyle}
+                        overwriteStyles={headerStyles}
+                    />
+                ) : null}
+            </Header>
         )
     },
 )
