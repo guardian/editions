c98b5f70f0ab2d1ddb5111eb521266902b515257
Joe Cowton <joe.cowton@guardian.co.uk>
2021-03-31 16:51:58 +0100

    LIVE-2087: fix animation speed and sign in bug

--- projects/Mallard/src/screens/identity-login-screen.tsx
+++ projects/Mallard/src/screens/identity-login-screen.tsx
@@ -17,6 +17,8 @@ import { SubFoundModalCard } from 'src/components/sub-found-modal-card';
 import { withConsent } from 'src/helpers/settings';
 import { Copy } from 'src/helpers/words';
 import { useFormField } from 'src/hooks/use-form-field';
+import type { SettingsOverlayInterface } from 'src/hooks/use-settings-overlay';
+import { SettingsOverlayContext } from 'src/hooks/use-settings-overlay';
 import type { CompositeNavigationStackProps } from 'src/navigation/NavigationModels';
 import { RouteNames } from 'src/navigation/NavigationModels';
 import isEmail from 'validator/lib/isEmail';
@@ -50,6 +52,9 @@ const AuthSwitcherScreen = () => {
 
 	const { authIdentity } = useContext(AccessContext);
 	const { open } = useModal();
+	const { setSettingsModalOpen } = useContext(
+		SettingsOverlayContext,
+	) as SettingsOverlayInterface;
 
 	const handleAuthClick = async (
 		runGetIdentityAuthParams: () => Promise<AuthParams>,
@@ -77,7 +82,10 @@ const AuthSwitcherScreen = () => {
 											attempt.data.userDetails
 												.primaryEmailAddress
 										}
-										onDismiss={() => navigation.popToTop()}
+										onDismiss={() => {
+											setSettingsModalOpen(false);
+											navigation.popToTop();
+										}}
 										onOpenCASLogin={() =>
 											navigation.navigate(
 												RouteNames.CasSignIn,
