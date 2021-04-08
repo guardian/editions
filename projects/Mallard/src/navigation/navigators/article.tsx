84b91fc543dc613ab33c8a041a5d3077c123bec9
Anna Beddow <anna.beddow@gmail.com>
2021-04-08 12:37:37 +0100

    Add full screen article component

--- projects/Mallard/src/navigation/navigators/article.tsx
+++ projects/Mallard/src/navigation/navigators/article.tsx
@@ -1,59 +1,42 @@
+import { useNavigation } from '@react-navigation/core';
 import type { RouteProp } from '@react-navigation/native';
 import { useRoute } from '@react-navigation/native';
+import type { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import { Animated, StyleSheet } from 'react-native';
-import { safeInterpolation } from 'src/helpers/math';
+import { Button, ButtonAppearance } from 'src/components/Button/Button';
+import { Header } from 'src/components/layout/header/header';
 import { ArticleScreen } from 'src/screens/article-screen';
-import { color } from 'src/theme/color';
-import { metrics } from 'src/theme/spacing';
 import { SlideCard } from '../../components/layout/slide-card/index';
 import type { MainStackParamList } from '../NavigationModels';
 
-const styles = StyleSheet.create({
-	root: {
-		...StyleSheet.absoluteFillObject,
-	},
-	inner: {
-		...StyleSheet.absoluteFillObject,
-		backgroundColor: color.background,
-		height: '100%',
-		flexGrow: 1,
-		overflow: 'hidden',
-		marginBottom: metrics.slideCardSpacing,
-	},
-	basicCard: { backgroundColor: color.background, overflow: 'hidden' },
-});
+const FullScreenArticle = () => {
+	const navigation = useNavigation<StackNavigationProp<MainStackParamList>>();
+	return (
+		<>
+			<Header
+				theme="light"
+				leftAction={
+					<Button
+						appearance={ButtonAppearance.Skeleton}
+						icon={'\uE00A'}
+						alt="Back"
+						onPress={() => navigation.goBack()}
+					></Button>
+				}
+				layout="center"
+			>
+				{null}
+			</Header>
+			<ArticleScreen />
+		</>
+	);
+};
 
 export const ArticleWrapper = () => {
 	const route = useRoute<RouteProp<MainStackParamList, 'Article'>>();
-	const position = new Animated.Value(0);
-	if (route.params.prefersFullScreen) {
-		return (
-			<Animated.View
-				style={[
-					StyleSheet.absoluteFillObject,
-					styles.basicCard,
-					{
-						transform: [
-							{
-								translateY: position.interpolate({
-									inputRange: safeInterpolation([0, 1]),
-									outputRange: safeInterpolation([200, 0]),
-								}),
-							},
-						],
-					},
-					{
-						opacity: position.interpolate({
-							inputRange: safeInterpolation([0, 0.5]),
-							outputRange: safeInterpolation([0, 1]),
-						}),
-					},
-				]}
-			></Animated.View>
-		);
-	}
-	return (
+	return route.params.prefersFullScreen ? (
+		<FullScreenArticle />
+	) : (
 		<SlideCard>
 			<ArticleScreen />
 		</SlideCard>
