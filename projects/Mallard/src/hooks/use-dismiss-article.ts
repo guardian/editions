2b9c592a8961f383481f0b9d04b7303e08e58440
Anna Beddow <anna.beddow@gmail.com>
2021-04-27 15:35:07 +0100

    Explicitly specify useNativeDriver

--- projects/Mallard/src/hooks/use-dismiss-article.ts
+++ projects/Mallard/src/hooks/use-dismiss-article.ts
@@ -34,12 +34,15 @@ export const useDismissArticle = () => {
 					return false;
 				},
 				onStartShouldSetPanResponder: () => false,
-				onPanResponderMove: Animated.event([
-					null,
-					{
-						dy: scrollY,
-					},
-				]),
+				onPanResponderMove: Animated.event(
+					[
+						null,
+						{
+							dy: scrollY,
+						},
+					],
+					{ useNativeDriver: true },
+				),
 				onPanResponderEnd: (ev, gestureState) => {
 					if (gestureState.dy > 50) {
 						onDismiss();
