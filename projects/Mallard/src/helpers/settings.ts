aed07efce3a18916e81269af2204a26b246182ab
Mohammad Haque <mohammad.haque@guardian.co.uk>
2021-05-07 11:18:20 +0100

    added a fix for image loading issue with ios14 build

--- projects/Mallard/src/helpers/settings.ts
+++ projects/Mallard/src/helpers/settings.ts
@@ -121,13 +121,17 @@ export const getSetting = <S extends keyof Settings>(
 	setting: S,
 ): Promise<Settings[S]> => {
 	return AsyncStorage.getItem(SETTINGS_KEY_PREFIX + setting).then((item) => {
+		// TODO - to test our new edition stack we are forcing beta users to use
+		// the new stack. Need to remove this once new stack is fully in operation
+		// and fastly config change has been made so it points to the new mobile stack
+		if (isInBeta() && setting == 'apiUrl') {
+			return newMobileProdStack as Settings[S];
+		}
+
 		if (!item) {
 			return defaultSettings[setting];
 		}
 
-		if (isInBeta() && setting == 'apiUrl') {
-			return newMobileProdStack as Settings[S];
-		}
 		return unsanitize(item) as Settings[S];
 	});
 };
