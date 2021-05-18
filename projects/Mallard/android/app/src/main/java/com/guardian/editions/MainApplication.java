924ce15c13d22529704aba6868eaa0048bff55af
Mohammad Haque <mohammad.haque@guardian.co.uk>
2021-05-18 15:16:12 +0100

    updated react-native-push-notification lib to make it compatible with updated firebase; fixed a sentry import

--- projects/Mallard/android/app/src/main/java/com/guardian/editions/MainApplication.java
+++ projects/Mallard/android/app/src/main/java/com/guardian/editions/MainApplication.java
@@ -9,7 +9,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import io.sentry.RNSentryPackage;
+import io.sentry.react.RNSentryPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
