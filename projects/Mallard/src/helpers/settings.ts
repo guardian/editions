f99d4a6bef30faeb63e403520122661d4b72fd80
Mohammad Haque <mohammad.haque@guardian.co.uk>
2021-05-19 10:53:13 +0100

    Reordered force beta users to use new stack

--- projects/Mallard/src/helpers/settings.ts
+++ projects/Mallard/src/helpers/settings.ts
@@ -121,12 +121,14 @@ export const getSetting = <S extends keyof Settings>(
 	setting: S,
 ): Promise<Settings[S]> => {
 	return AsyncStorage.getItem(SETTINGS_KEY_PREFIX + setting).then((item) => {
-		if (!item) {
-			return defaultSettings[setting];
-		}
-
+		// TODO - to test our new edition stack we are forcing beta users to use
+		// the new stack. Need to remove this once new stack is fully in operation
+		// and fastly config change has been made so it points to the new mobile stack
 		if (isInBeta() && setting == 'apiUrl') {
 			return newMobileProdStack as Settings[S];
+		}
+		if (!item) {
+			return defaultSettings[setting];
 		}
 		return unsanitize(item) as Settings[S];
 	});
