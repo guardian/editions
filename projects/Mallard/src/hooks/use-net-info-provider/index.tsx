207d958a7e60d3a7bfbeaaf2b2d754a859606d29
James Miller <james.miller.freelancer@guardian.co.uk>
2021-09-29 17:18:38 +0100

    feat: useNetInfoProvider (#1900)
    * feat: useNetInfoProvider
    
    * chore: Reverting a change
    
    * chore: Additional State types

---
+++ projects/Mallard/src/hooks/use-net-info-provider/index.tsx
@@ -0,0 +1,117 @@
+import { NetInfoStateType, useNetInfo } from '@react-native-community/netinfo';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import type { NetInfoState } from './types';
+import { DownloadBlockedStatus } from './types';
+import { isDisconnectedState, stateResolver } from './utils';
+
+const defaultState: NetInfoState = {
+	type: NetInfoStateType.unknown,
+	isConnected: false,
+	isPoorConnection: false,
+	isInternetReachable: false,
+	downloadBlocked: DownloadBlockedStatus.NotBlocked,
+	isDevButtonShown: false,
+	setIsDevButtonShown: () => {},
+	overrideIsConnected: false,
+	setOverrideIsConnected: () => {},
+	overrideNetworkType: NetInfoStateType.unknown,
+	setOverrideNetworkType: () => {},
+	overrideIsInternetReachable: false,
+	setOverrideIsInternetReachable: () => {},
+};
+
+const NetInfoContext = createContext(defaultState);
+
+export const NetInfoProvider = ({
+	children,
+}: {
+	children: React.ReactNode;
+}) => {
+	/* Variables to track against the actual Net Info state */
+	const [localType, setLocalType] = useState<NetInfoState['type']>(
+		defaultState.type,
+	);
+	const [localIsConnected, setLocalIsConnected] = useState<
+		NetInfoState['isConnected']
+	>(defaultState.isConnected);
+	const [localIsInternetReachable, setLocalIsInternetReachable] = useState<
+		NetInfoState['isInternetReachable']
+	>(defaultState.isInternetReachable);
+
+	/* Calculated values */
+	const [localIsPoorConnection, setLocalIsPoorConnection] = useState<
+		NetInfoState['isPoorConnection']
+	>(defaultState.isPoorConnection);
+	const [localDownloadBlocked, setLocalDownloadBlocked] = useState<
+		NetInfoState['downloadBlocked']
+	>(defaultState.downloadBlocked);
+
+	/* Values associated with overrides */
+	const [isDevButtonShown, setIsDevButtonShown] = useState<
+		NetInfoState['isDevButtonShown']
+	>(defaultState.isDevButtonShown);
+	const [overrideIsConnected, setOverrideIsConnected] = useState<
+		NetInfoState['overrideIsConnected']
+	>(defaultState.overrideIsConnected);
+	const [overrideNetworkType, setOverrideNetworkType] = useState<
+		NetInfoState['overrideNetworkType']
+	>(defaultState.overrideNetworkType);
+	const [overrideIsInternetReachable, setOverrideIsInternetReachable] =
+		useState<NetInfoState['overrideIsInternetReachable']>(
+			defaultState.overrideIsInternetReachable,
+		);
+
+	const { type, isConnected, isInternetReachable } = useNetInfo();
+
+	// Update the state whenever core netinfo values change or overrides are in play
+	useEffect(() => {
+		const resolvedState = stateResolver({
+			type,
+			isConnected,
+			isInternetReachable,
+			isDevButtonShown,
+			overrideIsConnected,
+			overrideNetworkType,
+			overrideIsInternetReachable,
+		});
+
+		setLocalType(resolvedState.type);
+		setLocalIsConnected(resolvedState.isConnected);
+		setLocalIsInternetReachable(resolvedState.isInternetReachable);
+		setLocalIsPoorConnection(resolvedState.isPoorConnection);
+		setLocalDownloadBlocked(resolvedState.downloadBlocked);
+	}, [
+		type,
+		isConnected,
+		isInternetReachable,
+		isDevButtonShown,
+		overrideIsConnected,
+		overrideNetworkType,
+		overrideIsInternetReachable,
+	]);
+
+	return (
+		<NetInfoContext.Provider
+			value={{
+				type: localType,
+				isConnected: localIsConnected,
+				isInternetReachable: localIsInternetReachable,
+				isPoorConnection: localIsPoorConnection,
+				downloadBlocked: localDownloadBlocked,
+				isDevButtonShown,
+				setIsDevButtonShown,
+				overrideIsConnected,
+				setOverrideIsConnected,
+				overrideNetworkType,
+				setOverrideNetworkType,
+				overrideIsInternetReachable,
+				setOverrideIsInternetReachable,
+			}}
+		>
+			{children}
+		</NetInfoContext.Provider>
+	);
+};
+
+export const useNetInfoProvider = () => useContext(NetInfoContext);
+export { NetInfoStateType, isDisconnectedState };
---
+++ projects/Mallard/src/hooks/use-net-info-provider/utils.ts
@@ -0,0 +1,90 @@
+import { NetInfoStateType } from '@react-native-community/netinfo';
+import { DownloadBlockedStatus } from './types';
+import type { NetInfoCore, NetInfoInputs, NetInfoValues } from './types';
+
+export const getDownloadBlockedStatus = (
+	netInfo: {
+		type: NetInfoCore['type'];
+		isConnected: NetInfoCore['isConnected'];
+	},
+	wifiOnlyDownloads: boolean,
+): DownloadBlockedStatus => {
+	return !netInfo.isConnected
+		? DownloadBlockedStatus.Offline
+		: wifiOnlyDownloads && netInfo.type !== 'wifi'
+		? DownloadBlockedStatus.WifiOnly
+		: DownloadBlockedStatus.NotBlocked;
+};
+
+export const isDisconnectedState = (type: NetInfoStateType) =>
+	type === NetInfoStateType.none || type === NetInfoStateType.unknown;
+
+export const isTruelyConnected = ({
+	isConnected,
+	isInternetReachable,
+}: {
+	isConnected: NetInfoCore['isConnected'];
+	isInternetReachable: NetInfoCore['isInternetReachable'];
+}) => {
+	const internetUnreachable = isInternetReachable === false;
+	return isConnected && !internetUnreachable;
+};
+
+export const isPoorConnection = (type: NetInfoStateType) =>
+	type === NetInfoStateType.cellular;
+
+export const stateResolver = (netInfo: NetInfoInputs): NetInfoValues => {
+	const {
+		type,
+		isConnected,
+		isInternetReachable,
+		isDevButtonShown,
+		overrideIsConnected,
+		overrideNetworkType,
+		overrideIsInternetReachable,
+	} = netInfo;
+
+	const checkPoorConnection = isPoorConnection(
+		isDevButtonShown ? overrideNetworkType : type,
+	);
+	const isFullyConnected = isTruelyConnected({
+		isConnected: isDevButtonShown ? overrideIsConnected : isConnected,
+		isInternetReachable: isDevButtonShown
+			? overrideIsInternetReachable
+			: isInternetReachable,
+	});
+
+	if (isDevButtonShown) {
+		return {
+			...netInfo,
+			type: overrideNetworkType,
+			isConnected: isDisconnectedState(overrideNetworkType)
+				? false
+				: isFullyConnected,
+			isInternetReachable: isDisconnectedState(overrideNetworkType)
+				? false
+				: overrideIsInternetReachable,
+			isPoorConnection: checkPoorConnection,
+			downloadBlocked: getDownloadBlockedStatus(
+				{
+					type: overrideNetworkType,
+					isConnected: isFullyConnected,
+				},
+				true,
+			),
+		};
+	}
+
+	return {
+		...netInfo,
+		isConnected: isFullyConnected,
+		isPoorConnection: checkPoorConnection,
+		downloadBlocked: getDownloadBlockedStatus(
+			{
+				type,
+				isConnected: isFullyConnected,
+			},
+			true,
+		),
+	};
+};
