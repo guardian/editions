c8e674d3092ba691041282ee2191a2747899d13a
James Miller <james@james-miller.co.uk>
2021-10-06 10:55:44 +0100

    feat: Issue Summary Context Provider

---
+++ projects/Mallard/src/hooks/use-issue-summary-provider.tsx
@@ -0,0 +1,158 @@
+import React, {
+	createContext,
+	useCallback,
+	useContext,
+	useEffect,
+	useState,
+} from 'react';
+import type { Dispatch } from 'react';
+import type { IssueSummary } from 'src/common';
+import { fetchAndStoreIssueSummary, readIssueSummary } from 'src/helpers/files';
+import type { PathToIssue } from 'src/paths';
+import { errorService } from 'src/services/errors';
+import { useAppState } from './use-app-state-provider';
+import {
+	getMaxAvailableEditions,
+	useMaxAvailableEditions,
+} from './use-config-provider';
+import { useEditions } from './use-edition-provider';
+import { useNetInfoProvider } from './use-net-info-provider';
+
+interface IssueSummaryState {
+	issueSummary: IssueSummary[] | null;
+	issueId: PathToIssue | null;
+	error: string;
+	initialFrontKey: string | null;
+	setIssueId: Dispatch<PathToIssue>;
+}
+
+const defaultState: IssueSummaryState = {
+	issueSummary: null,
+	issueId: null,
+	error: '',
+	initialFrontKey: null,
+	setIssueId: () => {},
+};
+
+const IssueSummaryContext = createContext(defaultState);
+
+export const getIssueSummary = async (
+	isConnected = true,
+	isPoorConnection = false,
+): Promise<IssueSummary[]> => {
+	const issueSummary =
+		isConnected && !isPoorConnection
+			? await fetchAndStoreIssueSummary()
+			: await readIssueSummary();
+	const maxAvailableEditions = await getMaxAvailableEditions();
+	try {
+		const trimmedSummary = issueSummary.slice(0, maxAvailableEditions);
+		return trimmedSummary;
+	} catch (e) {
+		e.message = `getIssueSummary error: maxAvailableEditions: ${maxAvailableEditions} & issueSummary: ${JSON.stringify(
+			issueSummary,
+		)}`;
+		errorService.captureException(e);
+		throw Error(e);
+	}
+};
+
+export const issueSummaryToLatestPath = (
+	issueSummary: IssueSummary[],
+): PathToIssue => ({
+	localIssueId: issueSummary[0].localId,
+	publishedIssueId: issueSummary[0].publishedId,
+});
+
+export const IssueSummaryProvider = ({
+	children,
+}: {
+	children: React.ReactNode;
+}) => {
+	const [issueSummary, setIssueSummary] = useState<
+		IssueSummaryState['issueSummary']
+	>(defaultState.issueSummary);
+	const [issueId, setLocalIssueId] = useState<IssueSummaryState['issueId']>(
+		defaultState.issueId,
+	);
+	const [initialFrontKey, setInitialFrontKey] = useState<
+		IssueSummaryState['initialFrontKey']
+	>(defaultState.initialFrontKey);
+	const [error, setError] = useState<IssueSummaryState['error']>(
+		defaultState.error,
+	);
+
+	const { isConnected, isPoorConnection } = useNetInfoProvider();
+	const { selectedEdition } = useEditions();
+	const { maxAvailableEditions } = useMaxAvailableEditions();
+	const { isActive } = useAppState();
+
+	const setIssueId = (issueId: PathToIssue, frontKey?: string) => {
+		frontKey && setInitialFrontKey(frontKey);
+		setLocalIssueId(issueId);
+	};
+
+	// Use Callback used so the function isnt recreated on each rerender
+	const getLatestIssueSummary = useCallback(() => {
+		getIssueSummary(isConnected, isPoorConnection)
+			.then((retrievedIssueSummary) => {
+				setIssueSummary(retrievedIssueSummary);
+				// Clear the initial front key if it is a new issue id and set it
+				const latestIssueId = issueSummaryToLatestPath(
+					retrievedIssueSummary,
+				);
+				issueId !== latestIssueId && setInitialFrontKey(null);
+				setLocalIssueId(latestIssueId);
+				setError('');
+			})
+			.catch((e) => {
+				setError(e.message);
+				// In the case of error, attempt to get the last stored issue summary
+				getIssueSummary(false)
+					.then((backupIssueSummary) => {
+						setIssueSummary(backupIssueSummary);
+						const backupIssueIds =
+							issueSummaryToLatestPath(backupIssueSummary);
+						setIssueId(backupIssueIds);
+					})
+					.catch((e) => {
+						e.message = `Unable to get backup issue summary: ${e.message}`;
+						errorService.captureException(e);
+					});
+			});
+	}, [isConnected, isPoorConnection, selectedEdition, maxAvailableEditions]);
+
+	// On load, get the latest issue summary
+	useEffect(() => {
+		getLatestIssueSummary();
+	}, []);
+
+	// When the network status, edition , max available editions or app state changes
+	useEffect(() => {
+		if (isActive) {
+			getLatestIssueSummary();
+		}
+	}, [
+		isConnected,
+		isPoorConnection,
+		selectedEdition,
+		maxAvailableEditions,
+		isActive,
+	]);
+
+	return (
+		<IssueSummaryContext.Provider
+			value={{
+				issueSummary,
+				issueId,
+				setIssueId,
+				initialFrontKey,
+				error,
+			}}
+		>
+			{children}
+		</IssueSummaryContext.Provider>
+	);
+};
+
+export const useIssueSummary = () => useContext(IssueSummaryContext);
