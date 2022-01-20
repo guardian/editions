16ba04ab7376de5e250a5790383c0ae4b5441072
James Miller <james.miller.freelancer@guardian.co.uk>
2022-01-20 08:39:06 +0000

    chore: Attempt to disable IAP for Android (#1980)

---
+++ projects/Mallard/react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dependencies: {
+      "react-native-iap": {
+        platforms: {
+          android: null // disable Android platform, other platforms will still autolink if provided
+        }
+      }
+    }
+  }
