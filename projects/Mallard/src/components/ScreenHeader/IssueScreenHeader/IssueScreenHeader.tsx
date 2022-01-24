ccae35d029a0777b70e433589ca65736dbc5d9ba
James Miller <james@james-miller.co.uk>
2022-01-24 17:41:07 +0000

    chore: Improve the header mildly

--- projects/Mallard/src/components/ScreenHeader/IssueScreenHeader/IssueScreenHeader.tsx
+++ projects/Mallard/src/components/ScreenHeader/IssueScreenHeader/IssueScreenHeader.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useCallback } from 'react';
 import type { IssueWithFronts, SpecialEditionHeaderStyles } from 'src/common';
 import { IssueTitle } from 'src/components/issue/issue-title';
 import { Header } from 'src/components/layout/header/header';
@@ -24,29 +24,29 @@ const IssueScreenHeader = React.memo(
 		headerStyles?: SpecialEditionHeaderStyles;
 		issue?: IssueWithFronts;
 	}) => {
-		const navigation = useNavigation();
+		const { navigate } = useNavigation();
 		const { date, weekday } = useIssueDate(issue);
 		const { setNewEditionSeen, selectedEdition } = useEditions();
 
-		const getDateString = () => {
+		const getDateString = useCallback(() => {
 			const abbreviatedDay = weekday.substring(0, 3);
 			return `${abbreviatedDay} ${date}`;
-		};
+		}, [date, weekday]);
 
-		const goToIssueList = () => {
-			navigation.navigate(RouteNames.IssueList);
-		};
+		const goToIssueList = useCallback(() => {
+			navigate(RouteNames.IssueList);
+		}, [navigate]);
 
-		const handleEditionMenuPress = () => {
+		const handleEditionMenuPress = useCallback(() => {
 			setNewEditionSeen();
-			navigation.navigate(RouteNames.EditionsMenu);
-		};
+			navigate(RouteNames.EditionsMenu);
+		}, [setNewEditionSeen, navigate]);
 
 		const isSpecialEdition = (editionType: string) => {
 			return editionType === 'Special';
 		};
 
-		const getTitles = (): Titles => {
+		const getTitles = useCallback((): Titles => {
 			if (isSpecialEdition(selectedEdition.editionType)) {
 				const splitTitle = selectedEdition.title.split('\n');
 				return {
@@ -61,7 +61,7 @@ const IssueScreenHeader = React.memo(
 				subTitle: dateString,
 				titleStyle: styles.issueLightText,
 			};
-		};
+		}, [selectedEdition]);
 
 		const { title, subTitle, titleStyle } = getTitles();
 
