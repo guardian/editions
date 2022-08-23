c8cb611e832a650c860f03c38e516b2be790aaa1
James Miller <935975+jimhunty@users.noreply.github.com>
2022-08-23 08:32:45 +0100

    chore: Updated typo

--- projects/Mallard/src/components/icons/__tests__/EditionsMenu.spec.tsx
+++ projects/Mallard/src/components/icons/__tests__/EditionsMenu.spec.tsx
@@ -3,7 +3,7 @@ import TestRenderer from 'react-test-renderer';
 import { EditionsMenu } from '../EditionsMenu';
 
 describe('EditionsMenu', () => {
-	it('should display a EditionsMenu icon in SVG', () => {
+	it('should display an EditionsMenu icon in SVG', () => {
 		const component = TestRenderer.create(<EditionsMenu />).toJSON();
 		expect(component).toMatchSnapshot();
 	});
