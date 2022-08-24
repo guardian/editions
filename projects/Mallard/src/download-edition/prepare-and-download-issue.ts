74ee39ef5e18e297a39254ed150dfb35761afd09
James Miller <935975+jimhunty@users.noreply.github.com>
2022-08-24 07:54:03 +0100

    chore: Remove Cache Clear (#2100)

--- projects/Mallard/src/download-edition/prepare-and-download-issue.ts
+++ projects/Mallard/src/download-edition/prepare-and-download-issue.ts
@@ -1,7 +1,6 @@
 import { downloadTodaysIssue } from 'src/download-edition/download-todays-issue';
 import { largeDeviceMemory } from 'src/hooks/use-config-provider';
 import type { NetInfoState } from 'src/hooks/use-net-info-provider';
-import { fetchCacheClear } from '../helpers/fetch';
 import { prepFileSystem } from '../helpers/files';
 import { cleanPushTrackingByDays } from '../notifications/push-tracking';
 import {
@@ -18,15 +17,12 @@ const prepareAndDownloadTodaysIssue = async (
 	await clearDownloadsDirectory();
 	await cleanOldEditions();
 	await cleanPushTrackingByDays();
-	const weOk = await fetchCacheClear();
-	if (weOk) {
-		// Check to see if the device has a decent amount of memory before doing intensive tasks
-		const largeRAM = await largeDeviceMemory();
-		if (largeRAM) {
-			return await downloadTodaysIssue(downloadBlocked);
-		}
-		return;
+	// Check to see if the device has a decent amount of memory before doing intensive tasks
+	const largeRAM = await largeDeviceMemory();
+	if (largeRAM) {
+		return await downloadTodaysIssue(downloadBlocked);
 	}
+	return;
 };
 
 export { prepareAndDownloadTodaysIssue };
