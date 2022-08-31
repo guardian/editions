1b7c97bab9313daf4b40f605c0b3ac68dd5fd1fc
James Miller <935975+jimhunty@users.noreply.github.com>
2022-08-31 08:11:42 +0100

    Revert "chore: Dont auto download in preview (#2076)"
    This reverts commit b78ce2fa6cf44035e047265328a97fc36fda05e5.

--- projects/Mallard/src/hooks/use-issue-downloads.tsx
+++ projects/Mallard/src/hooks/use-issue-downloads.tsx
@@ -5,30 +5,27 @@ import { prepareAndDownloadTodaysIssue } from 'src/download-edition/prepare-and-
 import { pushDownloadFailsafe } from 'src/helpers/push-download-failsafe';
 import { pushNotificationRegistration } from 'src/notifications/push-notifications';
 import { useAppState } from './use-app-state-provider';
-import { useApiUrl, useLargeDeviceMemory } from './use-config-provider';
+import { useLargeDeviceMemory } from './use-config-provider';
 import { useNetInfo } from './use-net-info-provider';
 
 export const useIssueDownloads = () => {
-	const { isPreview } = useApiUrl();
 	const { downloadBlocked } = useNetInfo();
 	const { isActive } = useAppState();
 	const largeRAM = useLargeDeviceMemory();
 
 	useEffect(() => {
-		if (!isPreview) {
-			prepareAndDownloadTodaysIssue(downloadBlocked);
-			// On mount, if there is plenty of memory then setup the failsafe for push downloads
-			if (largeRAM) {
-				pushDownloadFailsafe(downloadBlocked);
-			}
-			// Initial app registration for Push Notifications
-			pushNotificationRegistration(downloadBlocked);
+		prepareAndDownloadTodaysIssue(downloadBlocked);
+		// On mount, if there is plenty of memory then setup the failsafe for push downloads
+		if (largeRAM) {
+			pushDownloadFailsafe(downloadBlocked);
 		}
+		// Initial app registration for Push Notifications
+		pushNotificationRegistration(downloadBlocked);
 	}, []);
 
 	useEffect(() => {
 		// we check the value so that we dont run the function if it changes from true to false.
-		if (isActive && !isPreview) {
+		if (isActive) {
 			prepareAndDownloadTodaysIssue(downloadBlocked);
 		}
 	}, [isActive]);
