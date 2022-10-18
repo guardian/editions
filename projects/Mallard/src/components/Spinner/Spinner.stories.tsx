5fcf497847b419154c322b4a767edbdbfdb597af
James Miller <935975+jimhunty@users.noreply.github.com>
2022-10-18 09:53:54 +0100

    chore: Remove Storybook (#2119)
    * chore: Remove Storybook
    
    * chore: Put GQL back

--- projects/Mallard/src/components/Spinner/Spinner.stories.tsx
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from '@storybook/react-native';
-import React from 'react';
-import { Spinner } from './Spinner';
-
-storiesOf('Spinner', module).add('Standard Spinner', () => <Spinner />);
