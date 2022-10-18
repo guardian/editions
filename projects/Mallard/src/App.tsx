10e9dcb2289f651e1baf4663b3b788243bc50a89
James Miller <935975+jimhunty@users.noreply.github.com>
2022-10-18 08:16:59 +0100

    chore: Remove Storybook

--- projects/Mallard/src/App.tsx
+++ projects/Mallard/src/App.tsx
@@ -1,7 +1,3 @@
-// Welcome to the main entry point of the app.
-//
-// In this file, we'll be kicking off our app or storybook.
-
 import React, { useEffect } from 'react';
 import { StatusBar, StyleSheet, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
