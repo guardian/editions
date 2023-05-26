19085ffcfbf0534f1fff2bcd369b457bd17af130
James Miller <935975+jimhunty@users.noreply.github.com>
2023-05-26 09:01:16 +0100

    chore: Inspect release webview (#2211)
    * chore: Inspect release webview
    
    * chore: Pop sentry back

--- projects/Mallard/android/app/src/main/java/com/guardian/editions/MainApplication.java
+++ projects/Mallard/android/app/src/main/java/com/guardian/editions/MainApplication.java
@@ -18,6 +18,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.guardian.editions.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
+import android.webkit.WebView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -80,6 +81,7 @@ public class MainApplication extends Application implements ReactApplication {
         ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
         SoLoader.init(this, /* native exopackage */ false);
         initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+        WebView.setWebContentsDebuggingEnabled(true);
     }
 
   /**
