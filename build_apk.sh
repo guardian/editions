ad488c4f9e76c4584df78785d0a33eae6a3689d0
groakland <grahame.oakland@guardian.co.uk>
2023-06-10 14:19:08 +0100

    Create external shell script for the build APK TeamCity Configuration step (Android)

---
+++ build_apk.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# CI script for building and deploying Editions android APK
+# to either BETA (beta-android) or RELEASE (release-android)
+
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: build_apk.sh [BETA|RELEASE]"
+  exit 1
+fi
+
+case $1 in
+         "BETA")
+             TARGET="beta-android"
+             ;;
+         "RELEASE")
+             TARGET="release-android"
+             ;;
+         *)
+             echo "Invalid option '$1' must be either 'BETA' or 'RELEASE'"
+             exit 1
+             ;;
+     esac
+
+npm cache clean -f
+npm install -g n
+n 16.16
+PATH="$PATH"
+echo "NEW NODE VERSION"
+node --version
+
+npm install -g yarn npx --force
+cd projects/Mallard
+yarn prestorybook
+echo "building APK with command 'make $TARGET'"
+make "$TARGET"
+
+exit 0
