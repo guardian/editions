cc8cae1d84eef5ff7668b2a444362e701dffb92d
James Miller <935975+jimhunty@users.noreply.github.com>
2023-07-28 08:41:49 +0100

    fix: Android Cover Card Images

--- projects/Mallard/src/components/front/image-resource.tsx
+++ projects/Mallard/src/components/front/image-resource.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import type { StyleProp } from 'react-native';
-import { View } from 'react-native';
+import type { ImageStyle as RNImageStyle, StyleProp } from 'react-native';
+import { Platform, Image as RNImage, View } from 'react-native';
 import type { ImageStyle } from 'react-native-fast-image';
 import FastImage from 'react-native-fast-image';
 import { useAspectRatio } from 'src/hooks/use-aspect-ratio';
@@ -20,6 +20,7 @@ type ImageResourceProps = {
 	style?: StyleProp<ImageStyle>;
 	setAspectRatio?: boolean;
 	accessibilityLabel?: string;
+	isCoverCard?: boolean;
 };
 
 const ImageResource = ({
@@ -27,6 +28,7 @@ const ImageResource = ({
 	style,
 	setAspectRatio = false,
 	use,
+	isCoverCard = false,
 	...props
 }: ImageResourceProps) => {
 	const imagePath = useImagePath(image, use);
@@ -36,17 +38,30 @@ const ImageResource = ({
 		setAspectRatio && aspectRatio ? { aspectRatio } : {},
 	];
 
-	return imagePath ? (
-		<FastImage
-			key={imagePath}
-			{...props}
-			resizeMode={FastImage.resizeMode.cover}
-			style={[styles, style] as StyleProp<ImageStyle>}
-			source={{ uri: imagePath }}
-		/>
-	) : (
-		<View style={styles}></View>
-	);
+	// Cover Cards are not rendering properly for Android using FastImage, so this falls back to React Native Image in this case
+	if (imagePath && Platform.OS === 'android' && isCoverCard) {
+		return (
+			<RNImage
+				key={imagePath}
+				{...props}
+				resizeMode={FastImage.resizeMode.cover}
+				style={[styles, style] as StyleProp<RNImageStyle>}
+				source={{ uri: imagePath }}
+			/>
+		);
+	} else if (imagePath) {
+		return (
+			<FastImage
+				key={imagePath}
+				{...props}
+				resizeMode={FastImage.resizeMode.cover}
+				style={[styles, style] as StyleProp<ImageStyle>}
+				source={{ uri: imagePath }}
+			/>
+		);
+	} else {
+		return <View style={styles}></View>;
+	}
 };
 
 export { ImageResource };
