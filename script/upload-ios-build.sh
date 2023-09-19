4f5d30728929ffa5b741d407c39ddf0709eb54f5
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2023-09-19 16:30:24 +0100

    Update upload-ios-build.sh

--- script/upload-ios-build.sh
+++ script/upload-ios-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST -d '{"ref":"master","environment":"internal-beta"}' https://api.github.com/repos/guardian/editions/deployments --header "Content-Type:application/json" --header "Authorization:token $GITHUB_TOKEN"
+curl -X POST -d '{"ref":"main","environment":"internal-beta"}' https://api.github.com/repos/guardian/editions/deployments --header "Content-Type:application/json" --header "Authorization:token $GITHUB_TOKEN"
