48ad13e4af4261006fd29f678bb6ab571075dd2e
Jorge Azevedo <jorge.azevedo@guardian.co.uk>
2023-09-20 07:12:07 +0100

    Migrate server side build to TC (#2283)
    * First too build steps from teamcity for build to test
    
    Co-authored-by: Jorge <Jorge.Azevedo@guardian.co.uk>
    Co-authored-by: Michael <Michael.Clapham@guardian.co.uk>
    
    * Exclude Mallard from build by setting the environment variable exclude
    
    Co-authored-by: Jorge <Jorge.Azevedo@guardian.co.uk>
    Co-authored-by: Michael <Michael.Clapham@guardian.co.uk>
    
    * Add two more build step and save yarn upload for next commit
    
    Co-authored-by: Jorge <Jorge.Azevedo@guardian.co.uk>
    Co-authored-by: Michael <Michael.Clapham@guardian.co.uk>
    
    * Upload to riffraff using actions-riff-raff
    
    Co-authored-by: Jorge <Jorge.Azevedo@guardian.co.uk>
    Co-authored-by: Michael <Michael.Clapham@guardian.co.uk>
    
    * Seperate ci steps for easier debugging
    
    * Made ci.sh executable
    
    * Renamed ci to ci.sh
    
    * Add credentials for riff-raff upload
    
    * Add missing permissions for riff-raff upload
    
    * Add build number offset to sync with TC build numbers
    
    * Switch to using .nvmrc
    
    * Remove redundant call to yarn
    
    ---------
    
    Co-authored-by: Julia Branke <Julia.Branke@guardian.co.uk>
    Co-authored-by: Michael <Michael.Clapham@guardian.co.uk>

---
+++ script/ci.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "start ci script"
+set -e
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT_DIR="${DIR}/.."
+
+echo "create directory zipfiles"
+mkdir $ROOT_DIR/zipfiles
+
+# Create zip files of the different dist directories
+(
+  echo "create editions-backend.zip"
+  cd "$ROOT_DIR/projects/backend/dist/"
+  zip -r "$ROOT_DIR/zipfiles/editions-backend.zip" .
+  echo "create editions-archiver.zip"
+  cd "$ROOT_DIR/projects/archiver/dist/"
+  zip -r "$ROOT_DIR/zipfiles/editions-archiver.zip" .
+  echo "create editions-logging.zip"
+  cd "$ROOT_DIR/projects/logging/dist/"
+  zip -r "$ROOT_DIR/zipfiles/editions-logging.zip" .
+  echo "finished zipping"
+)
