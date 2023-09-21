16ce4195a72524b24a7cbe955a9a5947a357f8a1
James Miller <935975+jimhunty@users.noreply.github.com>
2023-09-21 10:39:59 +0100

    chore: Remove logging from the AWS project (#2224)

--- projects/aws/bin/logging.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register'
-import cdk = require('@aws-cdk/core')
-import { LoggingStack } from '../lib/logging-stack'
-
-const app = new cdk.App()
-new LoggingStack(app, 'EditionsLoggingStack')
