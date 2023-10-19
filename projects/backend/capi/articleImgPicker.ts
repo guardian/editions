1342327897f28676e58b6325d497cc166d0dbd97
Rebecca Thompson <33927854+rebecca-thompson@users.noreply.github.com>
2023-10-19 10:26:49 +0100

    skip populating main media for cartoon content

--- projects/backend/capi/articleImgPicker.ts
+++ projects/backend/capi/articleImgPicker.ts
@@ -1,22 +1,24 @@
 import { Content } from '@guardian/content-api-models/v1/content'
 import {
+    ArticleType,
     CreditedImage,
-    TrailImage,
     ImageRole,
     imageRoles,
+    TrailImage,
 } from '../../Apps/common/src'
 import { oc } from 'ts-optchain'
-import { getImage, getCreditedImage } from './assets'
-import { ArticleType } from '../../Apps/common/src'
+import { getCreditedImage, getImage } from './assets'
 import { ContentType } from '@guardian/content-api-models/v1/contentType'
+import { ElementType } from '@guardian/content-api-models/v1/elementType'
 
 /**
- * This function exploits the 'role'field that is passed to the backend when generating image urls
+ * This function exploits the 'role' field that is passed to the backend when generating image urls
  * to add some image quality overrides in certain scenarios. Any content with displayhint or articleType 'immersive'
- * gets it's images bumped to 'immersive' quality. The same happens to 'picture' content
+ * gets its images bumped to 'immersive' quality. The same happens to 'picture' content
+ * @param articleType
  * @param displayHint
  * @param capiRole the image role specified in the content API (if any)
- * @param contetnType e.g. gallery/picture/article - we want big pictures for the picture type
+ * @param contentType e.g. gallery/picture/article - we want big pictures for the picture type
  */
 export const getImageRole = (
     articleType: ArticleType,
@@ -39,6 +41,12 @@ const getMainImage = (
     articleType: ArticleType,
 ): CreditedImage | undefined => {
     const maybeMainElement = oc(result).blocks.main.elements[0]()
+
+    // we populate the body with the cartoon, so we don't want it to render in main media
+    if (maybeMainElement && maybeMainElement.type === ElementType.CARTOON) {
+        return undefined
+    }
+
     const maybeCreditedMainImage =
         maybeMainElement && getCreditedImage(maybeMainElement)
 
