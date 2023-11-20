ffaa10c9f36e43c3baa4113a386cb852165a8301
James Miller <935975+jimhunty@users.noreply.github.com>
2023-11-20 08:11:09 +0000

    fix: Crosswords Prod

---
+++ projects/Apps/crosswords/scripts/html-update.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const { resolve } = require('path')
+
+const filePath = resolve(__dirname, '..', 'dist', 'index.html')
+
+// Read the content of the file
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error(`Error reading file ${filePath}: ${err}`)
+        return
+    }
+
+    // Replace "type='module'" with "type='text/javascript'"
+    const updatedContent = data.replace(
+        'type="module"',
+        'type="text/javascript"',
+    )
+
+    // Overwrite the file with the updated content
+    fs.writeFile(filePath, updatedContent, 'utf8', (err) => {
+        if (err) {
+            console.error(`Error writing file ${filePath}: ${err}`)
+            return
+        }
+
+        console.log(`File ${filePath} has been updated successfully.`)
+    })
+})
