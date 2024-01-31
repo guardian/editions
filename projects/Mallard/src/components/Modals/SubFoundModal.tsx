e5aa389d5da7f2e2b051fd6c0be6d1b9211c1d60
James Miller <935975+jimhunty@users.noreply.github.com>
2024-01-31 14:36:47 +0000

    feat: UX improvement when sub is found in CAS login (#2370)
    * chore: RN 72 Upgrade
    
    * chore: Android and iOS working nut with Nav issues
    
    * chore: Style and type fixes
    
    * chore: Upgrade to 0.72.10
    
    * chore: Updated dependencies
    
    * chore: Fix linting
    
    * chore: Apple Silicon fixes
    
    * chore: Attempt to fix clock issue
    
    * feat: CAS Sub Found nav improvement

--- projects/Mallard/src/components/Modals/SubFoundModal.tsx
+++ projects/Mallard/src/components/Modals/SubFoundModal.tsx
@@ -1,16 +1,27 @@
 import { useNavigation } from '@react-navigation/native';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
 import { Copy } from 'src/helpers/words';
+import type { MainStackParamList } from 'src/navigation/NavigationModels';
 import { CenterWrapper } from '../CenterWrapper/CenterWrapper';
 import { CardAppearance, OnboardingCard } from '../onboarding/onboarding-card';
 
-const SubFoundModalCard = () => {
+type SubFoundModalCardProps = NativeStackScreenProps<
+	MainStackParamList,
+	'SubFoundModal',
+	'Main'
+>;
+
+const SubFoundModalCard = ({ route }: SubFoundModalCardProps) => {
 	const { goBack } = useNavigation();
 	return (
 		<CenterWrapper>
 			<OnboardingCard
 				title={Copy.subFound.title}
-				onDismissThisCard={goBack}
+				onDismissThisCard={() => {
+					goBack();
+					route.params?.closeAction?.();
+				}}
 				subtitle={Copy.subFound.subtitle}
 				appearance={CardAppearance.Blue}
 				size="small"
