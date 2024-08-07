f6eaaf151b619cfc3fe9ff0b3c363c5f1ab74cfc
James Miller <935975+jimhunty@users.noreply.github.com>
2024-08-07 08:48:38 +0100

    chore: RN74 upgrade

--- projects/Mallard/src/services/__tests__/content-auth-service.spec.ts
+++ projects/Mallard/src/services/__tests__/content-auth-service.spec.ts
@@ -3,6 +3,7 @@ import { fetchCasSubscription } from '../content-auth-service';
 describe('content-auth-service', () => {
 	describe('fetchCasSubscription', () => {
 		it('throws an error on non-200 responses', async () => {
+			// @ts-ignore
 			global.fetch = jest.fn().mockReturnValue(
 				Promise.resolve({
 					status: 419,
