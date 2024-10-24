0a6d4169498c5aa66a4531f885531d1fda1ba917
James Miller <935975+jimhunty@users.noreply.github.com>
2024-10-24 07:53:25 +0100

    chore: Updated date

--- projects/Mallard/src/helpers/words.ts
+++ projects/Mallard/src/helpers/words.ts
@@ -252,7 +252,7 @@ const andContinue = 'and continue';
 
 const iAPMigration = {
 	title: 'The app is changing...',
-	body: 'On XXXXXXXX, we will be switching to a new app. Your subscription will be transferred automatically, so you don’t need to do anything. The new app will feel more like a digital version of our newspaper, and we hope it delivers an intuitive reading experience you continue to enjoy.\n\n Thank you for your ongoing support.',
+	body: 'On 16/12/2024, we will be switching to a new app. Your subscription will be transferred automatically, so you don’t need to do anything. The new app will feel more like a digital version of our newspaper, and we hope it delivers an intuitive reading experience you continue to enjoy.\n\n Thank you for your ongoing support.',
 };
 
 export const copy = {
